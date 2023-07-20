35ac088495c499a54abfd32917b0f14f83df8b4f
Sehbeom <j3beom@gmail.com>
2023-07-20 22:21:32 +0900

    :heavy_plus_sign: Feat(user/country): 국가 목록 조회 controller 구현 (#214)
    - 국가 목록 조회 controller 구현
    - readAllCountries()

--- backend/src/main/java/com/pi/stepup/domain/user/api/UserApiController.java
+++ backend/src/main/java/com/pi/stepup/domain/user/api/UserApiController.java
@@ -1,4 +1,29 @@
 package com.pi.stepup.domain.user.api;
 
+import static com.pi.stepup.domain.user.constant.UserResponseMessage.READ_ALL_COUNTRIES_SUCCESS;
+
+import com.pi.stepup.domain.user.service.UserService;
+import com.pi.stepup.global.dto.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserApiController {
+
+    private final UserService userService;
+
+    @GetMapping("/country")
+    public ResponseEntity<?> readAllCountries() {
+        return ResponseEntity.ok(
+            ResponseDto.create(
+                READ_ALL_COUNTRIES_SUCCESS.getMessage(),
+                userService.readAllCountries()
+            )
+        );
+    }
 }
