a89191ad4cf1c72bc8161455aba45fda8b6f43f7
Sehbeom <j3beom@gmail.com>
2023-07-20 23:19:50 +0900

    :heavy_plus_sign: Feat(user/dup-email): 이메일 중복 검사 dao 구현 (#215)
    - 이메일 중복 검사 dao 계층 구현
    - findByEmail()

--- backend/src/main/java/com/pi/stepup/domain/user/dao/UserRepository.java
+++ backend/src/main/java/com/pi/stepup/domain/user/dao/UserRepository.java
@@ -1,10 +1,13 @@
 package com.pi.stepup.domain.user.dao;
 
 import com.pi.stepup.domain.user.domain.Country;
+import com.pi.stepup.domain.user.domain.User;
 import java.util.List;
 
 public interface UserRepository {
 
     List<Country> findAllCountries();
+
+    User findByEmail(String email);
 
 }
--- backend/src/main/java/com/pi/stepup/domain/user/dao/UserRepositoryImpl.java
+++ backend/src/main/java/com/pi/stepup/domain/user/dao/UserRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.pi.stepup.domain.user.dao;
 
 import com.pi.stepup.domain.user.domain.Country;
+import com.pi.stepup.domain.user.domain.User;
 import java.util.List;
 import javax.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
@@ -16,5 +17,14 @@ public class UserRepositoryImpl implements UserRepository {
     public List<Country> findAllCountries() {
         return em.createQuery("SELECT c FROM Country c", Country.class)
             .getResultList();
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return em.createQuery(
+                "SELECT u FROM User u "
+                    + "WHERE u.email = :email", User.class)
+            .setParameter("email", email)
+            .getSingleResult();
     }
 }
