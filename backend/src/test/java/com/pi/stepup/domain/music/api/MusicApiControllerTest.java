5dd96bd847e637bf075b93b761faa73a43140b5e
lkk <rabbit1999k@naver.com>
2023-07-20 16:14:40 +0900

    :white_check_mark: Test(music/addMusic): 노래 추가 테스트 코드 수정 (#207)
    - service null pointer 오류 : injectmocks -> autowired

--- backend/src/test/java/com/pi/stepup/domain/music/api/MusicApiControllerTest.java
+++ backend/src/test/java/com/pi/stepup/domain/music/api/MusicApiControllerTest.java
@@ -1,7 +1,6 @@
 package com.pi.stepup.domain.music.api;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.google.gson.Gson;
@@ -10,19 +9,22 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-//@SpringBootTest
-@ExtendWith(MockitoExtension.class)
+    @ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class MusicApiControllerTest {
 
-    @InjectMocks
+    @Autowired
     private MusicApiController musicApiController;
 
     private MockMvc mockMvc;
@@ -38,7 +40,7 @@ class MusicApiControllerTest {
     @Test
     @DisplayName("musicApiController가 null이 아님을 테스트")
     public void setMusicApiControllerNotNullTest() {
-        assertNotNull(musicApiController);
+        assertThat(musicApiController).isNotNull();
     }
 
     @Test
@@ -53,13 +55,16 @@ class MusicApiControllerTest {
             .URL("url")
             .build();
 
+//        String json = new ObjectMapper().writeValueAsString(music);
+//        String json = "";
+
         final ResultActions actions = mockMvc.perform(
             MockMvcRequestBuilders.post(url)
                 .content(gson.toJson(music))
                 .contentType(MediaType.APPLICATION_JSON)
         );
 
-        actions.andExpect(status().isOk()).andDo(print());
+        actions.andExpect(status().isCreated());
     }
 
 }
