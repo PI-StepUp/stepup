860171f71bd51163713e4d8d6b55d61a873fc9da
lkk <rabbit1999k@naver.com>
2023-07-20 16:09:45 +0900

    :heavy_plus_sign: Feat(music/addMusic): 노래 추가 controller 구현 (#208)

--- backend/src/main/java/com/pi/stepup/domain/music/api/MusicApiController.java
+++ backend/src/main/java/com/pi/stepup/domain/music/api/MusicApiController.java
@@ -1,5 +1,28 @@
 package com.pi.stepup.domain.music.api;
 
-public class MusicApiController {
+import com.pi.stepup.domain.music.domain.Music;
+import com.pi.stepup.domain.music.service.MusicService;
+import com.pi.stepup.global.dto.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/music")
+@RequiredArgsConstructor
+public class MusicApiController {
+    private final MusicService musicService;
+
+    @PostMapping
+    public ResponseEntity<?> createMusic(@RequestBody Music music){
+        Long musicId = musicService.create(music).getMusicId();
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseDto.create(
+            "노래 등록 완료",
+            musicId
+        ));
+    }
 }
