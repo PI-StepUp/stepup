5eea57cc13d3f341da312837b1729fee3800673e
Sehbeom <j3beom@gmail.com>
2023-07-21 01:13:34 +0900

    :heavy_plus_sign: Feat(user/dup-email): 이메일 중복 검사 service 구현 (#215)
    - 이메일 중복 검사 service 계층 구현
    - checkEmailDuplicated()

--- backend/src/main/java/com/pi/stepup/domain/user/dto/UserRequestDto.java
+++ backend/src/main/java/com/pi/stepup/domain/user/dto/UserRequestDto.java
@@ -1,5 +1,14 @@
 package com.pi.stepup.domain.user.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class UserRequestDto {
 
+    @Getter
+    @Setter
+    public static class CheckEmailRequestDto {
+
+        private String email;
+    }
 }
