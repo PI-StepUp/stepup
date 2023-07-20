a8a3059dfe02a3bebd9253e27fdaae76c3ea8788
Sehbeom <j3beom@gmail.com>
2023-07-20 17:53:39 +0900

    :heavy_plus_sign: Feat(user/country): 국가 목록 조회 controller 구현 (#214)
    - 국가 목록 조회 api controller 구현
    - readAllCountries()

--- backend/src/main/java/com/pi/stepup/domain/user/constant/UserResponseMessage.java
+++ backend/src/main/java/com/pi/stepup/domain/user/constant/UserResponseMessage.java
@@ -1,4 +1,15 @@
 package com.pi.stepup.domain.user.constant;
 
 public enum UserResponseMessage {
+    READ_ALL_COUNTRIES_SUCCESS("국가 코드 목록 조회 완료");
+
+    private final String message;
+
+    UserResponseMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
