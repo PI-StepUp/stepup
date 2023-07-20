102019e16d55a98d439016b49be98074da6e2160
rabbit0216 <rabbit1999k@gmail.com>
2023-07-20 22:56:31 +0900

    :zap: !CHANGE(music): music dto 생성자 수정

--- backend/src/main/java/com/pi/stepup/domain/music/dto/MusicRequestDto.java
+++ backend/src/main/java/com/pi/stepup/domain/music/dto/MusicRequestDto.java
@@ -1,16 +1,37 @@
 package com.pi.stepup.domain.music.dto;
 
+import com.pi.stepup.domain.music.domain.Music;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 public class MusicRequestDto {
 
     @Getter
-    @RequiredArgsConstructor
+    @NoArgsConstructor
     public static class MusicSaveRequestDto {
-        private final String title;
-        private final String artist;
-        private final String answer;
-        private final String URL;
+        private String title;
+        private String artist;
+        private String answer;
+        private String URL;
+
+        @Builder
+        public MusicSaveRequestDto(String title, String artist, String answer, String URL) {
+            this.title = title;
+            this.artist = artist;
+            this.answer = answer;
+            this.URL = URL;
+        }
+
+        public Music toEntity(){
+            return Music.builder()
+                    .title(this.title)
+                    .artist(this.artist)
+                    .answer(this.answer)
+                    .URL(this.URL)
+                    .build();
+        }
     }
+
+
 }
--- backend/src/main/java/com/pi/stepup/domain/music/dto/MusicResponseDto.java
+++ backend/src/main/java/com/pi/stepup/domain/music/dto/MusicResponseDto.java
@@ -1,18 +1,19 @@
 package com.pi.stepup.domain.music.dto;
 
 import com.pi.stepup.domain.music.domain.Music;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 public class MusicResponseDto {
 
     @Getter
-    private static class MusicFindResponseDto {
+    public static class MusicFindResponseDto {
         private final String title;
         private final String artist;
         private final String answer;
         private final String URL;
 
+        @Builder
         public MusicFindResponseDto(Music music) {
             this.title = music.getTitle();
             this.artist = music.getArtist();
