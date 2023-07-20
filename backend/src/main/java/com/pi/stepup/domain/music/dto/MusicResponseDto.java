5c9c234d82f0ea24a83e7a8d5e0054eeb0cffd0a
lkk <rabbit1999k@naver.com>
2023-07-20 17:50:14 +0900

    :heavy_plus_sign: Feat(music): response dto 구현
    - pk 제외 모든 파라미터 포함

--- backend/src/main/java/com/pi/stepup/domain/music/dto/MusicResponseDto.java
+++ backend/src/main/java/com/pi/stepup/domain/music/dto/MusicResponseDto.java
@@ -1,5 +1,23 @@
 package com.pi.stepup.domain.music.dto;
 
+import com.pi.stepup.domain.music.domain.Music;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 public class MusicResponseDto {
 
+    @Getter
+    private static class MusicFindResponseDto {
+        private final String title;
+        private final String artist;
+        private final String answer;
+        private final String URL;
+
+        public MusicFindResponseDto(Music music) {
+            this.title = music.getTitle();
+            this.artist = music.getArtist();
+            this.answer = music.getAnswer();
+            this.URL = music.getURL();
+        }
+    }
 }
