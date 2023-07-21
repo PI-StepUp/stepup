8e5cef15824c3fcb3bdefe5a80bb014466b76005
lkk <rabbit1999k@naver.com>
2023-07-21 16:50:18 +0900

    :hammer: Refactor(music): MusicRepositoryTest 공통되는 속성 init 함수로 빼기
    - music entity

--- backend/src/test/java/com/pi/stepup/domain/music/dao/MusicRepositoryTest.java
+++ backend/src/test/java/com/pi/stepup/domain/music/dao/MusicRepositoryTest.java
@@ -3,6 +3,7 @@ package com.pi.stepup.domain.music.dao;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.pi.stepup.domain.music.domain.Music;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,18 @@ class MusicRepositoryTest {
 
     @Autowired
     private MusicRepository musicRepository;
+    private Music music;
+
+    @Test
+    @BeforeEach
+    public void init() {
+        music = Music.builder()
+            .title("spicy")
+            .artist("aespa")
+            .answer("")
+            .URL("url")
+            .build();
+    }
 
     @Test
     @DisplayName("MusicRepository가 null이 아님을 테스트")
@@ -25,12 +38,7 @@ class MusicRepositoryTest {
     @DisplayName("노래 추가 테스트")
     @Transactional
     public void insertMusicRepoTest() {
-        Music music = Music.builder()
-            .title("spicy")
-            .artist("aespa")
-            .answer("")
-            .URL("url")
-            .build();
+
         Music result = musicRepository.insert(music);
         assertThat(music).isEqualTo(result);
     }
@@ -50,5 +58,19 @@ class MusicRepositoryTest {
         Long musicId = music.getMusicId();
         Music result = musicRepository.findOne(musicId);
         assertThat(music.getTitle()).isEqualTo(result.getTitle());
+    }
+
+    @Test
+    @DisplayName("노래 전체 목록 조회 테스트")
+    @Transactional
+    public void findAllMusicRepoTest(){
+
+    }
+
+    @Test
+    @DisplayName("노래 전체 목록 키워드 조회 테스트")
+    @Transactional
+    public void findAllMusicByKeywordRepoTest(){
+
     }
 }
