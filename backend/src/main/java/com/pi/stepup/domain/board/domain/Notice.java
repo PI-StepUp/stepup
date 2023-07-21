fc1d92a7d9ae5d76b4ae8cef7153413d3c1db977
이주희 <joo9837@naver.com>
2023-07-21 17:06:09 +0900

    :hammer: Refactor(board): Notice 리팩토링
    - 어노테이션 변경
    - Builder 추가

--- backend/src/main/java/com/pi/stepup/domain/board/domain/Notice.java
+++ backend/src/main/java/com/pi/stepup/domain/board/domain/Notice.java
@@ -1,24 +1,30 @@
 package com.pi.stepup.domain.board.domain;
 
 import com.pi.stepup.domain.dance.domain.RandomDance;
-import lombok.Getter;
-import lombok.Setter;
+import com.pi.stepup.domain.user.domain.User;
+import lombok.*;
 
-import javax.persistence.CascadeType;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import java.util.List;
 
 @Entity
 @Getter
-@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @DiscriminatorValue("NOTICE")
-public class Notice extends Board{
+public class Notice extends Board {
 
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "RANDOM_DANCE_ID")
     private RandomDance randomDance;
+
+    @Builder
+    public Notice(Long boardId, User writer, String title, String content, List<Comment> comments, String fileURL, RandomDance randomDance) {
+        super(boardId, writer, title, content, comments, fileURL);
+        this.randomDance = randomDance;
+    }
 
 }
