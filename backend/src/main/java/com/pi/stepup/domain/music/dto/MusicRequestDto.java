5b97762a2bc712381fb7b6b55011809649688e34
lkk <rabbit1999k@naver.com>
2023-07-21 16:50:42 +0900

    :hammer: Refactor(music): MusicRequestDto 어노테이션 변경

--- backend/src/main/java/com/pi/stepup/domain/music/dto/MusicRequestDto.java
+++ backend/src/main/java/com/pi/stepup/domain/music/dto/MusicRequestDto.java
@@ -1,27 +1,23 @@
 package com.pi.stepup.domain.music.dto;
 
 import com.pi.stepup.domain.music.domain.Music;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 public class MusicRequestDto {
 
-    @Getter
+    @Data
+    @Builder
     @NoArgsConstructor
+    @AllArgsConstructor
     public static class MusicSaveRequestDto {
         private String title;
         private String artist;
         private String answer;
         private String URL;
-
-        @Builder
-        public MusicSaveRequestDto(String title, String artist, String answer, String URL) {
-            this.title = title;
-            this.artist = artist;
-            this.answer = answer;
-            this.URL = URL;
-        }
 
         public Music toEntity(){
             return Music.builder()
