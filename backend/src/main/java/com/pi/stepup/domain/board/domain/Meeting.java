4662f21c03f47c088ef13339df1df69ba81217b5
이주희 <joo9837@naver.com>
2023-07-21 17:05:59 +0900

    :hammer: Refactor(board): Meeting 리팩토링
    - 어노테이션 변경
    - Builder 추가

--- backend/src/main/java/com/pi/stepup/domain/board/domain/Meeting.java
+++ backend/src/main/java/com/pi/stepup/domain/board/domain/Meeting.java
@@ -1,22 +1,30 @@
 package com.pi.stepup.domain.board.domain;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import com.pi.stepup.domain.user.domain.User;
+import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
 @Getter
-@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @DiscriminatorValue("MEETING")
-public class Meeting extends Board{
+public class Meeting extends Board {
     private String region;
     private LocalDateTime startAt;
     private LocalDateTime endAt;
     private int commentCnt;
 
+    @Builder
+    public Meeting(Long boardId, User writer, String title, String content, List<Comment> comments, String fileURL, String region, LocalDateTime startAt, LocalDateTime endAt, int commentCnt) {
+        super(boardId, writer, title, content, comments, fileURL);
+        this.region = region;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.commentCnt = commentCnt;
+    }
 
 }
