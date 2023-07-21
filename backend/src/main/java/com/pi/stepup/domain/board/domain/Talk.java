ebe4a0d21839da145e086f21b8a3d39a73962984
이주희 <joo9837@naver.com>
2023-07-21 17:06:38 +0900

    :hammer: Refactor(board): Talk 리팩토링
    - 어노테이션 변경
    - Builder 추가

--- backend/src/main/java/com/pi/stepup/domain/board/domain/Talk.java
+++ backend/src/main/java/com/pi/stepup/domain/board/domain/Talk.java
@@ -1,21 +1,23 @@
 package com.pi.stepup.domain.board.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.pi.stepup.domain.user.domain.User;
+import lombok.*;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import java.util.List;
 
 @Entity
 @Getter
-@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @DiscriminatorValue("TALK")
 public class Talk extends Board {
     private int commentCnt;
-    public int getCommentCnt() {
-        return commentCnt;
-    }
-    public void setCommentCnt(int commentCnt) {
+
+    @Builder
+    public Talk(Long boardId, User writer, String title, String content, List<Comment> comments, String fileURL, int commentCnt) {
+        super(boardId, writer, title, content, comments, fileURL);
         this.commentCnt = commentCnt;
     }
+
 }
