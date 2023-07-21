ec07ea057eba7a11bf9f72a79d1f7e5aca27157f
livinoid98 <ccy9803@gmail.com>
2023-07-21 09:36:14 +0900

    :art: Design(publish/practiceroom): 개인연습실 방 퍼블리싱 (#182)
    - 개인연습실 방 페이지 DOM 구조 작성 완료
    - 개인연습실 방 페이지 scss 스타일 코드 작성 완료

--- front/src/pages/practiceroom.tsx
+++ front/src/pages/practiceroom.tsx
@@ -1,6 +1,5 @@
 import SideMenu from "components/SideMenu";
 
-import Image from "next/image";
 import LeftArrowIcon from "/public/images/icon-left-arrow.svg"
 import GroupIcon from "/public/images/icon-group.svg"
 import PlusIcon from "/public/images/icon-plus.svg"
@@ -8,8 +7,8 @@ import ReflectIcon from "/public/images/icon-reflect.svg"
 import CameraIcon from "/public/images/icon-camera.svg"
 import MicIcon from "/public/images/icon-mic.svg"
 import MoreIcon from "/public/images/icon-more-dot.svg"
-import ChatDefaultImg from "/public/images/chat-default-profile-img.svg"
-import sendImg from "/public/images/send-img.svg"
+
+import Image from "next/image"
 
 const PracticeRoom = () => {
     return(
@@ -22,19 +21,9 @@ const PracticeRoom = () => {
                             <Image src={LeftArrowIcon} alt=""/>
                         </div>
                         <div className="room-title">
-                            <h3>랜덤플레이 댄스 방 제목</h3>
+                            <h3>보이넥스트도어 - One and Only</h3>
                             <span>2013년 7월 3일</span>
                         </div>
-                    </div>
-                    <div className="participle-wrap">
-                        <div className="participle-icon">
-                            <Image src={GroupIcon} alt="" />
-                            <span>30명</span>
-                        </div>
-                        <button>
-                            <Image src={PlusIcon} alt="" />
-                            <span>ADD PARTICIPLE</span>
-                        </button>
                     </div>
 
                     <div className="video-content">
@@ -69,87 +58,10 @@ const PracticeRoom = () => {
                     </div>
                     
                 </div>
-                <div className="participant-wrap">
-                    <div className="participant-list">
-                        <div className="participant-list-title">
-                            <h3>참여자 목록</h3>
-                            <span>5</span>
-                        </div>
-                        <div className="participant-list-content">
-                            <ul>
-                                <li className="on">
-                                        <Image src={ChatDefaultImg} alt=""/>
-                                        <span>ME</span>
-                                </li>
-                                <li>
-                                        <Image src={ChatDefaultImg} alt=""/>
-                                        <span>이싸피</span>
-                                </li>
-                                <li>
-                                        <Image src={ChatDefaultImg} alt=""/>
-                                        <span>정싸피</span>
-                                </li>
-                                <li>
-                                        <Image src={ChatDefaultImg} alt=""/>
-                                        <span>윤싸피</span>
-                                </li>
-                                <li>
-                                        <Image src={ChatDefaultImg} alt=""/>
-                                        <span>최싸피</span>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="chat-wrap">
-                        <div className="chat-title">
-                            <h3>채팅하기</h3>
-                        </div>
-                        <div className="chat-content-wrap">
-                            <div className="chat-read">
-                                <ul>
-                                    <li>
-                                        <div className="chat-user-img">
-                                            <Image src={ChatDefaultImg} alt=""/>
-                                        </div>
-                                        <div className="chat-user-msg">
-                                            <span>김싸피</span>
-                                            <p>웹기술 채팅내용입니다.</p>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div className="chat-user-img">
-                                            <Image src={ChatDefaultImg} alt=""/>
-                                        </div>
-                                        <div className="chat-user-msg">
-                                            <span>김싸피</span>
-                                            <p>웹기술 채팅내용입니다. 웹기술 채팅내용입니다. 웹기술 채팅내용입니다. 웹기술 채팅내용입니다.</p>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div className="chat-user-img">
-                                            <Image src={ChatDefaultImg} alt=""/>
-                                        </div>
-                                        <div className="chat-user-msg">
-                                            <span>김싸피</span>
-                                            <p>웹기술 채팅내용입니다.</p>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div className="chat-user-img">
-                                            <Image src={ChatDefaultImg} alt=""/>
-                                        </div>
-                                        <div className="chat-user-msg">
-                                            <span>김싸피</span>
-                                            <p>웹기술 채팅내용입니다.</p>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div className="chat-write">
-                                <input type="text" placeholder="모두에게 전송"/>
-                                <button><Image src={sendImg} alt=""/></button>
-                            </div>
-                        </div>
+                <div className="musiclist">
+                    <div className="musiclist-title">
+                        <h3>연습실 목록</h3>
+                        <span>5</span>
                     </div>
                 </div>
             </div>
