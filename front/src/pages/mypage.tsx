5cd7fdf2010a8a09bf1a62432e322ab39adcca28
송채은 <sce9842@hanmail.net>
2023-07-21 17:43:26 +0900

    :art: Design(design/mypage): 마이페이지 화면 퍼블리싱 (#82)
    - 마이페이지 css 수정
    - 회원정보 수정 클릭이벤트 추가 중

--- front/src/pages/mypage.tsx
+++ front/src/pages/mypage.tsx
@@ -9,224 +9,224 @@ import img_requestsong from "../public/images/img_requestsong.png"
 import img_board from "../public/images/img_board.png"
 
 const MyPage = () => {
-	return (
-		<>
-			<Header />
-			<div className="background_color">
-				<div>
-					<ul className="mypage_profile">
-						<li>
-							<div className="info">
-								<div className="cnt">3</div>
-								<div className="cnt_title">예약된 랜플댄</div>
-								<div className="btn_info">
-									<a href="">랜플댄 시간표 보기</a>
-								</div>
-							</div>
-						</li>
-						<li>
-							<div className="info">
-								<div className="img_profile">
-									<Image className="img" src={img_profile} alt="profile"></Image>
-								</div>
-								<div>
-									<progress value="985" max="2000"></progress>
-									<div className="progress_text">
-										<p>다음 랭킹까지 1,015</p>
-										<p>985/2,000</p>
-									</div>
-									<div className="info_user">
-										<p className="ranking">골드</p>
-										<p className="name">김싸피</p>
-									</div>
-								</div>
-							</div>
-						</li>
-						<li>
-							<div className="info">
-								<div className="cnt">3</div>
-								<div className="cnt_title">작성한 글 수</div>
-								<div className="btn_info">
-									<a href="">게시글 확인하기</a>
-								</div>
-							</div>
-						</li>
-					</ul>
-				</div>
-				{/* end - profile information */}
-				<div>
-					<ul className="list">
-						<div>내 예약</div>
-						<li className="contents">
-							<div className="img_box">
-								<Image className="img" src={img_rpdance} alt="reserved"></Image>
-							</div>
-							<div className="description">
-								<div className="time">Korea 5:00PM ~ 6:20PM (KST)</div>
-								<div className="title">여기서요? 4세대 남돌 곡 모음</div>
-								<div className="learner">Learner</div>
-							</div>
-							<div className="btn_contents">
-								<a href="">예약 취소</a>
-								<a href="">방 생성 취소</a>
-							</div>
-						</li>
-						{/* end - one reservation*/}
-						<li className="contents">
-							<div className="img_box">
-								<Image className="img" src={img_rpdance} alt="reserved"></Image>
-							</div>
-							<div className="description">
-								<div className="time">Korea 5:00PM ~ 6:20PM (KST)</div>
-								<div className="title">여기서요? 4세대 남돌-여돌 곡 모음 제목이 길면 다음줄로. 최대 2줄</div>
-								<div className="learner">Learner</div>
-							</div>
-							<div className="btn_contents">
-								<a href="">예약 취소</a>
-								<a href="">방 생성 취소</a>
-							</div>
-						</li>
-						<li className="contents">
-							<div className="img_box">
-								<Image className="img" src={img_rpdance} alt="reserved"></Image>
-							</div>
-							<div className="description">
-								<div className="time">Korea 5:00PM ~ 6:20PM (KST)</div>
-								<div className="title">여기서요? 4세대 남돌-여돌 곡 모음 제목이 길면 다음줄로. 최대 2줄</div>
-								<div className="learner">Learner</div>
-							</div>
-							<div className="btn_contents">
-								<a href="">예약 취소</a>
-								<a href="">방 생성 취소</a>
-							</div>
-						</li>
-					</ul>
-				</div>
-				{/* end - my reservation */}
-				<div className="settings">
-					<details>
-						<summary>
-							<div className="img_setting"></div>
-							<p className="settings_title">회원 정보 수정</p>
-							<div className="img_vector"></div>
-						</summary>
-						<div className="enter_password">
-							<p>현재 비밀번호</p>
-							<input className="pw" type="password" />
-							<input className="btn_submit" type="submit" />
-						</div>
-					</details>
-				</div>
-				{/* end - settings */}
-				<div>
-					<ul className="list">
-						<div>참여 이력</div>
-						<li className="contents">
-							<div className="img_box">
-								<Image className="img" src={img_rpdance} alt="reserved"></Image>
-							</div>
-							<div className="description">
-								<div className="time">Korea 5:00PM ~ 6:20PM (KST)</div>
-								<div className="title_past">여기서요? 4세대 남돌 곡 모음</div>
-								<div className="learner">Learner</div>
-							</div>
-							<div className="rank">
-								<p>-</p>
-							</div>
-						</li>
-						{/* end - one past random play dance*/}
-						<li className="contents">
-							<div className="img_box">
-								<Image className="img" src={img_rpdance} alt="reserved"></Image>
-							</div>
-							<div className="description">
-								<div className="time">Korea 5:00PM ~ 6:20PM (KST)</div>
-								<div className="title_past">여기서요? 4세대 남돌-여돌 곡 모음 제목이 길면 다음줄로. 최대 2줄</div>
-								<div className="learner">Learner</div>
-							</div>
-							<div className="rank">
-								<p>1위</p>
-							</div>
-						</li>
-						<li className="contents">
-							<div className="img_box">
-								<Image className="img" src={img_rpdance} alt="reserved"></Image>
-							</div>
-							<div className="description">
-								<div className="time">Korea 5:00PM ~ 6:20PM (KST)</div>
-								<div className="title_past">여기서요? 4세대 남돌-여돌 곡 모음 제목이 길면 다음줄로. 최대 2줄</div>
-								<div className="learner">Learner</div>
-							</div>
-							<div className="rank">
-								<p>3위</p>
-							</div>
-						</li>
-						<a href="" className="btn_more">더보기</a>
-					</ul>
-				</div>
-				{/* end - past random play dance */}
-				<div>
-					<ul className="list">
-						<div>작성한 글</div>
-						<li className="contents">
-							<div className="img_box">
-								<Image className="img" src={img_notice} alt="notice"></Image>
-							</div>
-							<div className="description">
-								<div className="time">공지사항</div>
-								<div className="title_past">작성한 공지사항 제목</div>
-								<div className="learner">2023.07.15</div>
-							</div>
-							<div className="detail">
-								<a href="">글 보기</a>
-							</div>
-						</li>
-						{/* end - one board*/}
-						<li className="contents">
-							<div className="img_box">
-								<Image className="img" src={img_requestsong} alt="song"></Image>
-							</div>
-							<div className="description">
-								<div className="time">플레이리스트</div>
-								<div className="title_past">작성한 노래 신청글 제목 / 제목이 길면 다음줄로. 최대 2줄</div>
-								<div className="learner">2023.07.15</div>
-							</div>
-							<div className="detail">
-								<a href="">글 보기</a>
-							</div>
-						</li>
-						<li className="contents">
-							<div className="img_box">
-								<Image className="img" src={img_offline} alt="reserved"></Image>
-							</div>
-							<div className="description">
-								<div className="time">오프라인 정모</div>
-								<div className="title_past">작성한 오프라인 정모 게시글 제목 / 제목이 길면 다음줄로. 최대 2줄</div>
-								<div className="learner">2023.07.15</div>
-							</div>
-							<div className="detail">
-								<a href="">글 보기</a>
-							</div>
-						</li>
-						<a href="" className="btn_more">더보기</a>
-					</ul>
-					{/* end - board */}
-				</div>
-				<div className="ad_enjoy">
-					<div className="ad_title">
-						<span>STEPUP의 </span>
-						<span>더 많은 즐길거리 즐기기</span>
-					</div>
-					<div className="ad_btn">
-						<a id="ad_whitebox" href="">랜플댄 예약하기</a>
-						<a id="ad_bluebox" href="">개인연습실 이용하기</a>
-					</div>
-				</div>
-			</div>
-			{/* end - advertisement */}
-			<Footer />
-		</>
-	)
+  return (
+    <>
+      <Header />
+      <div className="background-color">
+        <div>
+          <ul className="mypage-profile">
+            <li>
+              <div className="info">
+                <div className="cnt">3</div>
+                <div className="cnt-title">예약된 랜플댄</div>
+                <div className="btn-info">
+                  <a href="">랜플댄 시간표 보기</a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="info">
+                <div className="img-profile">
+                  <Image className="img" src={img_profile} alt="profile"></Image>
+                </div>
+                <div>
+                  <progress value="985" max="2000"></progress>
+                  <div className="progress-text">
+                    <p>다음 랭킹까지 1,015</p>
+                    <p>985/2,000</p>
+                  </div>
+                  <div className="info-user">
+                    <p className="ranking">골드</p>
+                    <p className="name">김싸피</p>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="info">
+                <div className="cnt">3</div>
+                <div className="cnt-title">작성한 글 수</div>
+                <div className="btn-info">
+                  <a href="">게시글 확인하기</a>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        {/* end - profile information */}
+        <div>
+          <ul className="list">
+            <div>내 예약</div>
+            <li className="contents">
+              <div className="img-box">
+                <Image className="img" src={img_rpdance} alt="reserved"></Image>
+              </div>
+              <div className="description">
+                <div className="time">Korea 5:00PM ~ 6:20PM (KST)</div>
+                <div className="title">여기서요? 4세대 남돌 곡 모음</div>
+                <div className="learner">Learner</div>
+              </div>
+              <div className="btn-contents">
+                <a href="">예약 취소</a>
+                <a href="">방 생성 취소</a>
+              </div>
+            </li>
+            {/* end - one reservation*/}
+            <li className="contents">
+              <div className="img-box">
+                <Image className="img" src={img_rpdance} alt="reserved"></Image>
+              </div>
+              <div className="description">
+                <div className="time">Korea 5:00PM ~ 6:20PM (KST)</div>
+                <div className="title">여기서요? 4세대 남돌-여돌 곡 모음 제목이 길면 다음줄로. 최대 2줄</div>
+                <div className="learner">Learner</div>
+              </div>
+              <div className="btn-contents">
+                <a href="">예약 취소</a>
+                <a href="">방 생성 취소</a>
+              </div>
+            </li>
+            <li className="contents">
+              <div className="img-box">
+                <Image className="img" src={img_rpdance} alt="reserved"></Image>
+              </div>
+              <div className="description">
+                <div className="time">Korea 5:00PM ~ 6:20PM (KST)</div>
+                <div className="title">여기서요? 4세대 남돌-여돌 곡 모음 제목이 길면 다음줄로. 최대 2줄</div>
+                <div className="learner">Learner</div>
+              </div>
+              <div className="btn-contents">
+                <a href="">예약 취소</a>
+                <a href="">방 생성 취소</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+        {/* end - my reservation */}
+        <div className="settings">
+          <details>
+            <summary>
+              <div className="img-setting"></div>
+              <p className="settings-title">회원 정보 수정</p>
+              <div className="img-vector"></div>
+            </summary>
+            <div className="enter-password">
+              <p>현재 비밀번호</p>
+              <input className="pw" type="password" />
+              <input className="btn-submit" type="submit" />
+            </div>
+          </details>
+        </div>
+        {/* end - settings */}
+        <div>
+          <ul className="list">
+            <div>참여 이력</div>
+            <li className="contents">
+              <div className="img-box">
+                <Image className="img" src={img_rpdance} alt="reserved"></Image>
+              </div>
+              <div className="description">
+                <div className="time">Korea 5:00PM ~ 6:20PM (KST)</div>
+                <div className="title-past">여기서요? 4세대 남돌 곡 모음</div>
+                <div className="learner">Learner</div>
+              </div>
+              <div className="rank">
+                <p>-</p>
+              </div>
+            </li>
+            {/* end - one past random play dance*/}
+            <li className="contents">
+              <div className="img-box">
+                <Image className="img" src={img_rpdance} alt="reserved"></Image>
+              </div>
+              <div className="description">
+                <div className="time">Korea 5:00PM ~ 6:20PM (KST)</div>
+                <div className="title-past">여기서요? 4세대 남돌-여돌 곡 모음 제목이 길면 다음줄로. 최대 2줄</div>
+                <div className="learner">Learner</div>
+              </div>
+              <div className="rank">
+                <p>1위</p>
+              </div>
+            </li>
+            <li className="contents">
+              <div className="img-box">
+                <Image className="img" src={img_rpdance} alt="reserved"></Image>
+              </div>
+              <div className="description">
+                <div className="time">Korea 5:00PM ~ 6:20PM (KST)</div>
+                <div className="title-past">여기서요? 4세대 남돌-여돌 곡 모음 제목이 길면 다음줄로. 최대 2줄</div>
+                <div className="learner">Learner</div>
+              </div>
+              <div className="rank">
+                <p>3위</p>
+              </div>
+            </li>
+            <a href="" className="btn-more">더보기</a>
+          </ul>
+        </div>
+        {/* end - past random play dance */}
+        <div>
+          <ul className="list">
+            <div>작성한 글</div>
+            <li className="contents">
+              <div className="img-box">
+                <Image className="img" src={img_notice} alt="notice"></Image>
+              </div>
+              <div className="description">
+                <div className="time">공지사항</div>
+                <div className="title-past">작성한 공지사항 제목</div>
+                <div className="learner">2023.07.15</div>
+              </div>
+              <div className="detail">
+                <a href="">글 보기</a>
+              </div>
+            </li>
+            {/* end - one board*/}
+            <li className="contents">
+              <div className="img-box">
+                <Image className="img" src={img_requestsong} alt="song"></Image>
+              </div>
+              <div className="description">
+                <div className="time">플레이리스트</div>
+                <div className="title-past">작성한 노래 신청글 제목 / 제목이 길면 다음줄로. 최대 2줄</div>
+                <div className="learner">2023.07.15</div>
+              </div>
+              <div className="detail">
+                <a href="">글 보기</a>
+              </div>
+            </li>
+            <li className="contents">
+              <div className="img-box">
+                <Image className="img" src={img_offline} alt="reserved"></Image>
+              </div>
+              <div className="description">
+                <div className="time">오프라인 정모</div>
+                <div className="title-past">작성한 오프라인 정모 게시글 제목 / 제목이 길면 다음줄로. 최대 2줄</div>
+                <div className="learner">2023.07.15</div>
+              </div>
+              <div className="detail">
+                <a href="">글 보기</a>
+              </div>
+            </li>
+            <a href="" className="btn-more">더보기</a>
+          </ul>
+          {/* end - board */}
+        </div>
+        <div className="ad-enjoy">
+          <div className="ad-title">
+            <span>STEPUP의 </span>
+            <span>더 많은 즐길거리 즐기기</span>
+          </div>
+          <div className="ad-btn">
+            <a id="ad-whitebox" href="">랜플댄 예약하기</a>
+            <a id="ad-bluebox" href="">개인연습실 이용하기</a>
+          </div>
+        </div>
+      </div>
+      {/* end - advertisement */}
+      <Footer />
+    </>
+  )
 }
 
 export default MyPage;
--- front/src/pages/mypageedit.tsx
+++ front/src/pages/mypageedit.tsx
@@ -3,38 +3,56 @@ import Footer from "../components/Footer"
 import Image from "next/image"
 import img_profile from "../public/images/img_profile_default.png"
 
+const componentDidMount = () =>{
+	// 중복 확인 버튼 클릭 이벤트
+	const displayWarning = document.getElementById("display-overlapping");
+	const clickWarning = (ev: Event) => {
+		if(ev.target instanceof HTMLInputElement){
+			displayWarning!.textContent = '중복된 닉네임입니다';
+			ev.target;
+		}
+	}
+	const btnElements = document.getElementById("btn-overlapping");
+	btnElements?.addEventListener("click", clickWarning);
+
+}
+
 const MyPageEdit = () => {
+
+	
 	return (
 		<>
 			<Header />
-			<div className="background_color">
+			<div className="background-color">
 				<div>
 					<ul className="myinfo">
 						<li>
-							<div className="list_title mt-70">프로필 이미지</div>
+							<div className="list-title mt-70">프로필 이미지</div>
 							<div className="profile">
-								<div className="img_box">
+								<div className="img-box">
 									<Image className="img" src={img_profile} alt="profile"></Image>
 								</div>
-								<div className="upload_img">
-									<label htmlFor="input-file" className="btn_upload">변경</label>
+								<div className="upload-img">
+									<label htmlFor="input-file" className="btn btn-profile">변경</label>
 									<input type="file" id="input-file" accept=".jpg, .png" />
-									<span className="btn_remove">삭제</span>
+									<span className="btn btn-profile btn-profile-remove">삭제</span>
 								</div>
-								<p>이미지는 5MB 이내의 jpg, png 파일만 등록 가능합니다</p>
+								<p className="warning">이미지는 5MB 이내의 jpg, png 파일만 등록 가능합니다</p>
 							</div>
 						</li>
 						{/* end - profile image */}
 						<li>
 							<div className="nickname">
-								<div className="list_title mt-40">닉네임</div>
-								<input type="text" />
+								<div className="list-title nickname-title mt-40">닉네임</div>
+								<input type="text" className="input-nickname" />
+								<input type="button" className="btn overlapping" id="btn-overlapping" value={"중복 확인"} />
 							</div>
+							<p className="warning" id="display-overlapping">사용 가능한 닉네임입니다</p>
 						</li>
 					</ul>
 				</div>
 				{/* end - my information */}
-				<div className="change_pw">
+				<div className="change-pw">
 
 				</div>
 				{/* end - change password */}
