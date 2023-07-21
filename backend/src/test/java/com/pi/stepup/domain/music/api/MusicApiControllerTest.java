ec4ef8106f59c42e391e7732db310d8431e451d8
lkk <rabbit1999k@naver.com>
2023-07-21 09:32:39 +0900

    :hammer: Refactor(music/readOneMusic): MusicApiControllerTest 안쓰는 어노테이션 제거

--- backend/src/test/java/com/pi/stepup/domain/music/api/MusicApiControllerTest.java
+++ backend/src/test/java/com/pi/stepup/domain/music/api/MusicApiControllerTest.java
@@ -9,10 +9,7 @@ import com.pi.stepup.domain.music.dto.MusicRequestDto.MusicSaveRequestDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,9 +18,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
-@ExtendWith(MockitoExtension.class)
 @SpringBootTest
-@AutoConfigureMockMvc
 class MusicApiControllerTest {
 
     @Autowired
