370dc48b956cf6bc9bc704f7e4f3b4c8ec2c3bef
rabbit0216 <rabbit1999k@gmail.com>
2023-07-22 23:52:20 +0900

    ✅ Test(music/findAllMusic): 모든 노래 조회 service 테스트 코드 구현 (#244)

--- backend/src/test/java/com/pi/stepup/domain/music/service/MusicServiceTest.java
+++ backend/src/test/java/com/pi/stepup/domain/music/service/MusicServiceTest.java
@@ -4,6 +4,7 @@ package com.pi.stepup.domain.music.service;
 import com.pi.stepup.domain.music.dao.MusicRepository;
 import com.pi.stepup.domain.music.domain.Music;
 import com.pi.stepup.domain.music.dto.MusicRequestDto.MusicSaveRequestDto;
+import com.pi.stepup.domain.music.dto.MusicResponseDto.MusicFindResponseDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,10 +14,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 
@@ -60,9 +64,32 @@ class MusicServiceTest {
 
         Optional<Music> result = musicService.readOne(music.getMusicId());
         Music foundMusic = null;
-        if(result.isPresent()) {
+        if (result.isPresent()) {
             foundMusic = result.get();
         }
         assertThat(music).isEqualTo(foundMusic);
     }
+
+    @Test
+    @DisplayName("노래 전체 목록 조회 테스트")
+    public void readAllMusicServiceTest() {
+        List<Music> makedMusic = makeMusic();
+        doReturn(makedMusic)
+                .when(musicRepository)
+                .findAll();
+
+        List<MusicFindResponseDto> foundMusic = musicService.readAll();
+
+        assertThat(makedMusic.size()).isEqualTo(foundMusic.size());
+    }
+
+    private List<Music> makeMusic() {
+        List<Music> music = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Music tmp = Music.builder().title("title" + i).artist("artist" + (i + 1)).build();
+            music.add(tmp);
+        }
+        return music;
+    }
+
 }
