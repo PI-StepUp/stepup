2ba71c058a4577671f219a702962291b23438a1d
rabbit0216 <rabbit1999k@gmail.com>
2023-07-23 13:53:52 +0900

    ✅ Test(music/findMusic): 모든 노래 조회 api 테스트 코드 구현 (#245)

--- backend/src/test/java/com/pi/stepup/domain/music/api/MusicApiControllerTest.java
+++ backend/src/test/java/com/pi/stepup/domain/music/api/MusicApiControllerTest.java
@@ -3,6 +3,7 @@ package com.pi.stepup.domain.music.api;
 import com.google.gson.Gson;
 import com.pi.stepup.domain.music.domain.Music;
 import com.pi.stepup.domain.music.dto.MusicRequestDto.MusicSaveRequestDto;
+import com.pi.stepup.domain.music.dto.MusicResponseDto.MusicFindResponseDto;
 import com.pi.stepup.domain.music.service.MusicService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,6 +16,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -77,4 +80,24 @@ class MusicApiControllerTest {
         getAction.andExpect(status().isOk()).andDo(print());
     }
 
+    @Test
+    @DisplayName("노래 목록 조회 테스트")
+    public void readAllMusicControllerTest() throws Exception {
+        when(musicService.readAll()).thenReturn(makeMusic());
+
+        final ResultActions getAction = mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/music")
+        );
+
+        getAction.andExpect(status().isOk()).andDo(print());
+    }
+
+    private List<MusicFindResponseDto> makeMusic() {
+        List<MusicFindResponseDto> music = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Music tmp = Music.builder().title("title" + i).artist("artist" + (i + 1)).build();
+            music.add(new MusicFindResponseDto(tmp));
+        }
+        return music;
+    }
 }
