df6cbfd6799564fc47ceb72a7f8f58d7a84f3c38
rabbit0216 <rabbit1999k@gmail.com>
2023-07-23 14:20:45 +0900

    ➕ Feat(music/findAllMusic): 모든 노래 조회 api 구현 (#245)

--- backend/src/main/java/com/pi/stepup/domain/music/api/MusicApiController.java
+++ backend/src/main/java/com/pi/stepup/domain/music/api/MusicApiController.java
@@ -1,13 +1,15 @@
 package com.pi.stepup.domain.music.api;
 
-import com.pi.stepup.domain.music.domain.Music;
 import com.pi.stepup.domain.music.dto.MusicRequestDto.MusicSaveRequestDto;
+import com.pi.stepup.domain.music.dto.MusicResponseDto.MusicFindResponseDto;
 import com.pi.stepup.domain.music.service.MusicService;
 import com.pi.stepup.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/music")
@@ -16,19 +18,34 @@ public class MusicApiController {
     private final MusicService musicService;
 
     @PostMapping
-    public ResponseEntity<?> createMusic(@RequestBody MusicSaveRequestDto musicSaveRequestDto){
+    public ResponseEntity<?> createMusic(@RequestBody MusicSaveRequestDto musicSaveRequestDto) {
         Long musicId = musicService.create(musicSaveRequestDto).getMusicId();
         return ResponseEntity.status(HttpStatus.CREATED).body(ResponseDto.create(
-            "노래 등록 완료",
-            musicService.readOne(musicId)
+                "노래 등록 완료",
+                musicService.readOne(musicId)
         ));
     }
 
     @GetMapping("/{musicId}")
-    public ResponseEntity<?> readOneMusic(@PathVariable("musicId") Long musicId){
+    public ResponseEntity<?> readOneMusic(@PathVariable("musicId") Long musicId) {
         return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(
                 "노래 조회 완료",
                 musicService.readOne(musicId)
+        ));
+    }
+
+    @GetMapping(params = "keyword")
+    public ResponseEntity<?> readAllMusic(
+            @RequestParam(required = false, name = "keyword") String keyword) {
+        List<MusicFindResponseDto> result;
+        if (keyword == null) {
+            result = musicService.readAll();
+        } else {
+            result = musicService.readAllByKeyword(keyword);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(
+                "노래 목록 조회 완료",
+                result
         ));
     }
 }
