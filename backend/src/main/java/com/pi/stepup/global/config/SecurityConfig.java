d3ca26d7b4c9ef17c69615ed349459ce3d86fc7c
Sehbeom <j3beom@gmail.com>
2023-07-23 20:26:59 +0900

    :heavy_plus_sign: Feat(user/auth): Config, Filter 구현 (#264)
    - Spring Security Configuration 설정 : SecurityConfig
    - Filter 계층 검증 기능 구현 : JwtAuthenticationFilter

---
+++ backend/src/main/java/com/pi/stepup/global/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.pi.stepup.global.config;
+
+import com.pi.stepup.global.util.jwt.JwtAuthenticationFilter;
+import com.pi.stepup.global.util.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .httpBasic().disable()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests()
+            .antMatchers("/api/user/{id}").authenticated()
+            .antMatchers(HttpMethod.PUT, "/api/user").authenticated()
+            .antMatchers(HttpMethod.POST, "/api/user/checkpw").authenticated()
+            .anyRequest().permitAll()
+            .and()
+            .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+
+}
---
+++ backend/src/main/java/com/pi/stepup/global/util/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,45 @@
+package com.pi.stepup.global.util.jwt;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends GenericFilterBean {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
+        // 1. Request Header 로부터 JWT 토큰 받아옴
+        String token = resolveToken((HttpServletRequest) request);
+
+        // 2. JwtTokenProvider.validateToken() 으로 유효성 검사 진행
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            // 토큰 유효할 경우, 토큰으로부터 Authentication 객체를 받아와 SecurityContext에 저장
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        chain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+            int tokenStartIndex = 7;
+            return bearerToken.substring(tokenStartIndex);
+        }
+        return null;
+    }
+}
