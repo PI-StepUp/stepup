445e7a7176d5a48e27432e0eadeece29937f19e9
Seulgi <urina842@gmail.com>
2023-07-23 20:25:51 +0900

    :white_check_mark: Test(dance/test): DanceRepo 테스트 코드 구현
    - insert, findOne, update, delete, findAllHeldDance 메서드 테스트

--- backend/src/test/java/com/pi/stepup/domain/dance/dao/DanceRepositoryTest.java
+++ backend/src/test/java/com/pi/stepup/domain/dance/dao/DanceRepositoryTest.java
@@ -1,106 +1,176 @@
 package com.pi.stepup.domain.dance.dao;
 
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.pi.stepup.domain.dance.constant.DanceType;
-import com.pi.stepup.domain.dance.domain.Dance;
+import com.pi.stepup.domain.dance.domain.RandomDance;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
-import java.util.Optional;
-import org.junit.jupiter.api.Assertions;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 public class DanceRepositoryTest {
+
+    @PersistenceContext
+    private EntityManager em;
 
     @Autowired
     private DanceRepository danceRepository;
 
+    private final String title = "랜덤 플레이 댄스";
+    private final String content = "함께 합시다";
+    private final String userCountry = "Asia/Seoul";
+    private final LocalDateTime time = LocalDateTime.now(ZoneId.of(userCountry));
+    private final DanceType type = DanceType.BASIC;
+    private final int max = 30;
+    private final String url = "url";
+
     @Test
-    @DisplayName("랜플댄 개최 테스트")
-    @Transactional
+    @DisplayName("랜덤 플레이 댄스 개최 테스트")
     public void insertDanceTest() {
-        //개최할 랜플댄
-        Dance insertDance = Dance.builder()
-            .title("제1회 랜덤 플레이 댄스")
-            .content("같이 놀아요")
-            //유저의 국가 코드 전달
-            .startAt(LocalDateTime.now(ZoneId.of("Asia/Seoul")))
-            .endAt(LocalDateTime.now(ZoneId.of("Asia/Seoul")))
-            .danceType(DanceType.BASIC)
-            .maxUser(30)
-            .thumbnail("url")
+        RandomDance insertDance = RandomDance.builder()
+            .title(title)
+            .content(content)
+            .startAt(time)
+            .endAt(time)
+            .danceType(type)
+            .maxUser(max)
+            .thumbnail(url)
             .build();
 
-        Dance saveDance = danceRepository.save(insertDance);
+        RandomDance saveDance = danceRepository.insert(insertDance);
 
         assertThat(saveDance.getId()).isNotNull();
-        assertThat(insertDance).isEqualTo(saveDance);
+        assertThat(saveDance.getTitle()).isEqualTo(title);
+        assertThat(saveDance.getContent()).isEqualTo(content);
+
+        System.out.println(">>> insertDanceTest 통과");
+        System.out.println(saveDance.getId());
     }
 
     @Test
-    @DisplayName("랜플댄 하나 조회 테스트")
-    @Transactional
+    @DisplayName("개최 랜덤 플레이 댄스 하나 조회 테스트")
     public void findDanceTest() {
-        //조회할 번호
-        Long id = 1L;
-        Optional<Dance> findDance = danceRepository.findById(id);
-
-        assertThat(findDance).isNotNull();
-    }
-
-    //테스트는 통과되는데, 쿼리문이 나오지 않는다
-    @Test
-    @DisplayName("랜플댄 수정 테스트")
-    @Transactional
-    public void updateDanceTest() {
-        //수정할 랜플댄 조회
-//        Long id = 1L;
-//        Optional<Dance> findDance = danceRepository.findById(id);
-//
-//        findDance.ifPresent(selectDance -> {
-//            selectDance.setTitle("수정");
-//            Dance updateDance = danceRepository.save(selectDance);
-//            assertThat(updateDance.getTitle()).isEqualTo("수정");
-//        });
-
-        //수정할 랜플댄
-        Dance updateDance = Dance.builder()
-            .id(1L)
-            .title("수정")
-            .content("테스트")
+        RandomDance insertDance = RandomDance.builder()
+            .title(title)
+            .content(content)
+            .startAt(time)
+            .endAt(time)
+            .danceType(type)
+            .maxUser(max)
+            .thumbnail(url)
             .build();
 
-        Dance newDance = danceRepository.save(updateDance);
-        assertThat(newDance.getTitle()).isEqualTo("수정");
+        em.persist(insertDance);
+        RandomDance findDance1 = em.find(RandomDance.class, insertDance.getId());
+        RandomDance findDance2 = danceRepository.findOne(insertDance.getId());
+
+        assertThat(findDance2).isNotNull();
+        assertThat(findDance2.getTitle()).isEqualTo(findDance1.getTitle());
+        assertThat(findDance2.getContent()).isEqualTo(findDance1.getContent());
+
+        System.out.println(">>> findRandomDanceTest 통과");
+        System.out.println(findDance2.getId());
     }
 
-    //테스트는 통과되는데, 쿼리문이 나오지 않는다
     @Test
-    @DisplayName("랜플댄 삭제 테스트")
+    @DisplayName("개최 랜덤 플레이 댄스 수정 테스트")
+    public void updateDanceTest() {
+        RandomDance insertDance = RandomDance.builder()
+            .title(title)
+            .content(content)
+            .startAt(time)
+            .endAt(time)
+            .danceType(type)
+            .maxUser(max)
+            .thumbnail(url)
+            .build();
+        em.persist(insertDance);
+
+        RandomDance findDance = em.find(RandomDance.class, insertDance.getId());
+
+        String updatedTitle = "제목 업데이트";
+        String updatedContent = "내용 업데이트";
+
+        RandomDance updatedDance = RandomDance.builder()
+            .id(findDance.getId())
+            .title(updatedTitle)
+            .content(updatedContent)
+            .startAt(findDance.getStartAt())
+            .endAt(findDance.getEndAt())
+            .danceType(findDance.getDanceType())
+            .maxUser(findDance.getMaxUser())
+            .thumbnail(findDance.getThumbnail())
+            .build();
+
+        danceRepository.update(updatedDance);
+
+        assertThat(findDance.getTitle()).isEqualTo(updatedTitle);
+        assertThat(findDance.getContent()).isEqualTo(updatedContent);
+
+        System.out.println(">>> updateDanceTest 통과");
+        System.out.println(findDance.getId());
+        System.out.println(findDance.getTitle());
+    }
+
+    @Test
+    @DisplayName("개최 랜덤 플레이 댄스 삭제 테스트")
     @Transactional
     public void deleteDanceTest() {
-        try {
-            //삭제할 랜플댄 번호
-            Long id = 1L;
-            danceRepository.deleteById(id);
+        RandomDance insertDance = RandomDance.builder()
+            .title(title)
+            .content(content)
+            .startAt(time)
+            .endAt(time)
+            .danceType(type)
+            .maxUser(max)
+            .thumbnail(url)
+            .build();
+        em.persist(insertDance);
 
-            assertThat(danceRepository.getById(1L)).isNull();
-        } catch (EmptyResultDataAccessException e) {
-            System.out.println("EmptyResultDataAccessException 발생");
-        }
+        Long randomDanceId = insertDance.getId();
+        RandomDance findDance1 = em.find(RandomDance.class, randomDanceId);
+
+        danceRepository.delete(randomDanceId);
+
+        RandomDance findDance2 = em.find(RandomDance.class, randomDanceId);
+        assertThat(findDance2).isNull();
+
+        System.out.println(">>> deleteDanceTest 통과");
+        System.out.println(findDance2);
     }
 
-//    @Test
-//    @DisplayName("랜플댄 전체 목록 테스트")
-//    public void findAllDanceTest() {
-//        List<Dance> danceList = danceRepository.findAll();
-//    }
+    @Test
+    @DisplayName("개최 랜덤 플레이 댄스 전체 목록 조회 테스트")
+    public void findAllHeldDanceTest() {
+        for (int i = 0; i < 10; i++) {
+            RandomDance insertDance = RandomDance.builder()
+                .title(title + i)
+                .content(content)
+                .startAt(time)
+                .endAt(time)
+                .danceType(type)
+                .maxUser(max)
+                .thumbnail(url)
+                .build();
+            em.persist(insertDance);
+        }
+
+        List<RandomDance> randomDanceList = danceRepository.findAllHeldDance();
+
+        assertThat(randomDanceList.size()).isEqualTo(10);
+        assertThat(randomDanceList).isNotNull();
+
+        System.out.println(">>> findAllRandomDanceTest 통과");
+        System.out.println(randomDanceList.size());
+//        danceRepository.findAllRandomDance().forEach(System.out::println);
+    }
 }
