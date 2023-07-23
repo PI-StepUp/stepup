6aa2818f6893d14cb30d6012e6b333648e3c5eda
Seulgi <urina842@gmail.com>
2023-07-23 21:09:53 +0900

    :heavy_plus_sign: Feat(dance/dto): DanceRequestDto 구현

--- backend/src/main/java/com/pi/stepup/domain/dance/dto/DanceRequestDto.java
+++ backend/src/main/java/com/pi/stepup/domain/dance/dto/DanceRequestDto.java
@@ -1,5 +1,29 @@
 package com.pi.stepup.domain.dance.dto;
 
+import com.pi.stepup.domain.dance.constant.DanceType;
+import com.pi.stepup.domain.dance.domain.RandomDance;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 public class DanceRequestDto {
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class DanceSaveRequestDto {
+
+        private Long randomDanceId;
+        private String title;
+        private String content;
+        private LocalDateTime startAt;
+        private LocalDateTime endAt;
+        private DanceType danceType;
+        private int maxUser;
+        private String thumbnail;
+    }
 
 }
