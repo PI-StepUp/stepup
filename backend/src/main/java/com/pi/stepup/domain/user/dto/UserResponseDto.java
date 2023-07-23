5e46f9cf87873a935081653e4155bc104b468d5d
Sehbeom <j3beom@gmail.com>
2023-07-23 20:40:14 +0900

    :heavy_plus_sign: Feat(user/auth): 로그인,회원가입,회원정보조회 service 구현 (#266)
    - 로그인 login(), 회원가입 signUp(), 회원정보조회 readOne() 구현
    - 로그인 request dto 추가 : LoginRequestDto
    - 회원가입 request dto 추가 : SignUpRequestDto
    - 회원정보조회 response dto 추가 : UserInfoResponseDto

--- backend/src/main/java/com/pi/stepup/domain/user/dto/UserResponseDto.java
+++ backend/src/main/java/com/pi/stepup/domain/user/dto/UserResponseDto.java
@@ -1,6 +1,8 @@
 package com.pi.stepup.domain.user.dto;
 
 import com.pi.stepup.domain.user.domain.Country;
+import com.pi.stepup.domain.user.domain.User;
+import java.time.LocalDate;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -16,6 +18,31 @@ public class UserResponseDto {
         private CountryResponseDto(Country country) {
             this.countryId = country.getCountryId();
             this.countryCode = country.getCode();
+        }
+    }
+
+    @Getter
+    public static class UserInfoResponseDto {
+        private final String email;
+        private final Integer emailAlert;
+        private final Long countryId;
+        private final String countryCode;
+        private final String nickname;
+        private final LocalDate birth;
+        private final String profileImg;
+        private final Integer point;
+        // TODO : Rank 정보 추가
+
+        @Builder
+        public UserInfoResponseDto(User user) {
+            this.email = user.getEmail();
+            this.emailAlert = user.getEmailAlert();
+            this.countryId = user.getCountry().getCountryId();
+            this.countryCode = user.getCountry().getCode();
+            this.nickname = user.getNickname();
+            this.birth = user.getBirth();
+            this.profileImg = user.getProfileImg();
+            this.point = user.getPoint();
         }
     }
 }
