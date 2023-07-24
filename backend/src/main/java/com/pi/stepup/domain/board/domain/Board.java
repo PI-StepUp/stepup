bc451279bab897b98df718038a4ea8ae0c68b348
이주희 <joo9837@naver.com>
2023-07-24 17:01:19 +0900

    :hammer: Refactor(board): Board 엔티티 수정

--- backend/src/main/java/com/pi/stepup/domain/board/domain/Board.java
+++ backend/src/main/java/com/pi/stepup/domain/board/domain/Board.java
@@ -3,12 +3,14 @@ package com.pi.stepup.domain.board.domain;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.pi.stepup.domain.user.domain.User;
 import com.pi.stepup.global.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.*;
-
-import lombok.*;
 
 
 @Entity
@@ -22,39 +24,25 @@ public abstract class Board extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "BOARD_ID")
-    private Long boardId;
+    Long boardId;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "WRITER")
-    private User writer;
+    User writer;
 
     @Column(nullable = false)
-    private String title;
+    String title;
 
     @Column(columnDefinition = "TEXT")
-    private String content;
+    String content;
 
     //댓글 정렬 최신순
     @OrderBy("commentId desc")
     @JsonIgnoreProperties({"board"})
     @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Comment> comments = new ArrayList<>();
+    List<Comment> comments = new ArrayList<>();
 
     @Column(name = "FILE_URL")
-    private String fileURL;
+    String fileURL;
 
-    @Builder
-    public Board(String title, String content, List<Comment> comments, String fileURL) {
-        this.writer = writer;
-        this.title = title;
-        this.content = content;
-        this.comments = comments;
-        this.fileURL = fileURL;
-    }
-
-    public void update(String title, String content, String fileURL) {
-        this.title = title;
-        this.content = content;
-        this.fileURL = fileURL;
-    }
 }
