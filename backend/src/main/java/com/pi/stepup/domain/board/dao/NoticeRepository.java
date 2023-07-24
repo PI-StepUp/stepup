1c7aa3f00a373e55bc6b224a84d3f43a3a66fef1
이주희 <joo9837@naver.com>
2023-07-24 17:05:14 +0900

    :hammer: Refactor(board/notice): rep 메서드명 수정

--- backend/src/main/java/com/pi/stepup/domain/board/dao/NoticeRepository.java
+++ backend/src/main/java/com/pi/stepup/domain/board/dao/NoticeRepository.java
@@ -1,16 +1,15 @@
 package com.pi.stepup.domain.board.dao;
 
 import com.pi.stepup.domain.board.domain.Notice;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface NoticeRepository  {
+public interface NoticeRepository {
 
     Notice insert(Notice notice);
 
-    Optional<Notice> findById(Long boardId);
+    Optional<Notice> findOne(Long boardId);
 
     List<Notice> findAll();
 
