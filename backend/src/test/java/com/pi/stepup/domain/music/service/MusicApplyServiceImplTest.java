2932b9af7c93198619e25b461c5a036e153d3227
rabbit0216 <rabbit1999k@gmail.com>
2023-07-24 18:41:04 +0900

    ✅ Test(music/addMusicApply): 노래 신청 등록 service 테스트 코드 구현 (#259)

---
+++ backend/src/test/java/com/pi/stepup/domain/music/service/MusicApplyServiceImplTest.java
@@ -0,0 +1,71 @@
+package com.pi.stepup.domain.music.service;
+
+import com.pi.stepup.domain.music.dao.MusicApplyRepository;
+import com.pi.stepup.domain.music.domain.MusicApply;
+import com.pi.stepup.domain.music.dto.MusicRequestDto.MusicApplySaveRequestDto;
+import com.pi.stepup.domain.user.dao.UserRepository;
+import com.pi.stepup.domain.user.domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class MusicApplyServiceImplTest {
+    @InjectMocks
+    MusicApplyServiceImpl musicApplyService;
+
+    @Mock
+    MusicApplyRepository musicApplyRepository;
+
+    @Mock
+    UserRepository userRepository;
+
+    private MusicApplySaveRequestDto musicApplySaveRequestDto;
+    private MusicApply musicApply;
+    private User user;
+
+    @Test
+    @BeforeEach
+    public void init() {
+        // TODO: musicApplySaveRequestDto 생성
+        musicApplySaveRequestDto = MusicApplySaveRequestDto.builder()
+                .artist("artist")
+                .title("title")
+                .content("content")
+                .writerId("writer")
+                .build();
+
+        user = User.builder()
+                .id("writer")
+                .build();
+
+        musicApply = MusicApply.builder()
+                .title(musicApplySaveRequestDto.getTitle())
+                .artist(musicApplySaveRequestDto.getArtist())
+                .content(musicApplySaveRequestDto.getContent())
+                .writer(user)
+                .build();
+    }
+
+    @Test
+    @DisplayName("노래 신청 등록 테스트")
+    public void createMusicApplyServiceTest() {
+        when(musicApplyRepository.insert(any(MusicApply.class))).thenReturn(musicApply);
+        when(userRepository.findById(any(String.class))).thenReturn(Optional.of(user));
+
+        MusicApply result = musicApplyService.create(musicApplySaveRequestDto);
+        assertThat(result.getTitle()).isEqualTo(musicApply.getTitle());
+    }
+
+
+}
