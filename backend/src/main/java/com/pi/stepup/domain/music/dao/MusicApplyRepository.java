8b598cb0e91520ee11483cddde07cb25c4b6c274
rabbit0216 <rabbit1999k@gmail.com>
2023-07-24 22:02:53 +0900

    ➕ Feat(music/findMusicApply): 노래 신청 목록 조회 repo 구현 (#441)

--- backend/src/main/java/com/pi/stepup/domain/music/dao/MusicApplyRepository.java
+++ backend/src/main/java/com/pi/stepup/domain/music/dao/MusicApplyRepository.java
@@ -2,6 +2,10 @@ package com.pi.stepup.domain.music.dao;
 
 import com.pi.stepup.domain.music.domain.MusicApply;
 
+import java.util.List;
+
 public interface MusicApplyRepository {
     MusicApply insert(MusicApply musicApply);
+
+    List<MusicApply> findAll(String keyword);
 }
--- backend/src/main/java/com/pi/stepup/domain/music/dao/MusicApplyRepositoryImpl.java
+++ backend/src/main/java/com/pi/stepup/domain/music/dao/MusicApplyRepositoryImpl.java
@@ -3,18 +3,31 @@ package com.pi.stepup.domain.music.dao;
 import com.pi.stepup.domain.music.domain.MusicApply;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class MusicApplyRepositoryImpl implements MusicApplyRepository {
     private final EntityManager em;
 
-
     @Override
     public MusicApply insert(MusicApply musicApply) {
         em.persist(musicApply);
         return musicApply;
+    }
+
+    @Override
+    public List<MusicApply> findAll(String keyword) {
+        String sql = "SELECT ma FROM MusicApply ma";
+
+        if (StringUtils.hasText(keyword) && !keyword.equals("")) {
+            sql += "WHERE m.title LIKE concat('%', " + keyword + ", '%') OR " +
+                    "m.artist LIKE concat('%', " + keyword + ", '%')";
+        }
+
+        return em.createQuery(sql, MusicApply.class).getResultList();
     }
 }
