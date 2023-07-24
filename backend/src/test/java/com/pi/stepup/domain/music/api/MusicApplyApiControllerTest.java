1f73c3d2acab5b451cb1ebe0e62a411ed000ed3a
rabbit0216 <rabbit1999k@gmail.com>
2023-07-24 22:41:43 +0900

    ✅ Test(music/findMusicApply): 노래 신청 목록 조회 api 테스트 코드 구현 (#443)

--- backend/src/test/java/com/pi/stepup/domain/music/api/MusicApplyApiControllerTest.java
+++ backend/src/test/java/com/pi/stepup/domain/music/api/MusicApplyApiControllerTest.java
@@ -3,6 +3,7 @@ package com.pi.stepup.domain.music.api;
 import com.google.gson.Gson;
 import com.pi.stepup.domain.music.domain.MusicApply;
 import com.pi.stepup.domain.music.dto.MusicRequestDto.MusicApplySaveRequestDto;
+import com.pi.stepup.domain.music.dto.MusicResponseDto.MusicApplyFindResponseDto;
 import com.pi.stepup.domain.music.service.MusicApplyService;
 import com.pi.stepup.domain.user.domain.User;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,11 +13,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(MusicApplyApiControllerTest.class)
@@ -30,7 +37,7 @@ class MusicApplyApiControllerTest {
     private Gson gson;
     private MusicApplySaveRequestDto musicApplySaveRequestDto;
     private MusicApply musicApply;
-    private User writer;
+    private User user;
 
     @Test
     @BeforeEach
@@ -40,30 +47,32 @@ class MusicApplyApiControllerTest {
                 .artist("artist")
                 .title("title")
                 .content("content")
-                .writerId("writer")
-                .build();
-
-        writer = User.builder()
-                .id("writer")
+                .writerId("user")
                 .build();
 
         musicApply = MusicApply.builder()
                 .title(musicApplySaveRequestDto.getTitle())
                 .artist(musicApplySaveRequestDto.getArtist())
                 .content(musicApplySaveRequestDto.getContent())
-                .writer(writer)
+                .build();
+
+        user = User.builder()
+                .id("user")
+                .password("password")
                 .build();
     }
 
 
+    // TODO : 404 에러 - security 관련 에러
     @Test
     @DisplayName("노래 신청 등록 테스트")
+    @WithMockUser
     public void MusicApplyCreateControllerTest() throws Exception {
         when(musicApplyService.create(musicApplySaveRequestDto)).thenReturn(musicApply);
 
         String url = "/api/music/apply";
         final ResultActions postAction = mockMvc.perform(
-                MockMvcRequestBuilders.post(url)
+                MockMvcRequestBuilders.post(url).with(csrf())
                         .content(gson.toJson(musicApplySaveRequestDto))
                         .contentType(MediaType.APPLICATION_JSON)
         );
@@ -71,5 +80,33 @@ class MusicApplyApiControllerTest {
         postAction.andExpect(status().isCreated());
     }
 
+
+    @Test
+    @DisplayName("노래 신청 목록 조회 테스트")
+    public void MusicApplyReadAllControllerTest() throws Exception {
+        String keyword = "";
+        when(musicApplyService.readAll(keyword)).thenReturn(makeMusicApply());
+
+        String url = "/api/music/apply?keyword=" + keyword;
+        final ResultActions getAction = mockMvc.perform(
+                MockMvcRequestBuilders.get(url)
+        );
+
+        getAction.andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    private List<MusicApplyFindResponseDto> makeMusicApply() {
+        List<MusicApplyFindResponseDto> musicApplies = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            MusicApply tmp = MusicApply.builder()
+                    .title("title" + i)
+                    .artist("artist" + (i + 1))
+                    .writer(user)
+                    .build();
+            musicApplies.add(new MusicApplyFindResponseDto(tmp));
+        }
+        return musicApplies;
+    }
 
 }
