334e78853aad0c5c314fc456b15be6501f3c029c
이주희 <joo9837@naver.com>
2023-07-24 17:08:56 +0900

    :heavy_plus_sign: Feat(board/notice): 공지 등록 api 구현

--- backend/src/main/java/com/pi/stepup/domain/board/api/NoticeApiController.java
+++ backend/src/main/java/com/pi/stepup/domain/board/api/NoticeApiController.java
@@ -1,7 +1,13 @@
 package com.pi.stepup.domain.board.api;
 
+import com.pi.stepup.domain.board.dto.NoticeRequestDto.NoticeSaveRequestDto;
 import com.pi.stepup.domain.board.service.NoticeService;
+import com.pi.stepup.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +18,19 @@ public class NoticeApiController {
 
     private final NoticeService noticeService;
 
-//    @PostMapping("/notice")
+    @PostMapping("/notice")
+    public ResponseEntity<ResponseDto> create(@RequestBody NoticeSaveRequestDto noticeSaveRequestDto) {
+        noticeService.create(noticeSaveRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseDto.create(
+                "공지 등록 완료",
+                null
+        ));
+    }
+
+
+//    @PutMapping("/notice/{boardId}")
+//    public ResponseEntity<?> updateNotice(@PathVariable Long boardId, @RequestBody NoticeUpdateRequestDto noticeUpdateRequestDto) {
+//        Long updatedBoardId = noticeService.update(boardId, noticeUpdateRequestDto);
+//        return ResponseEntity.ok().body(ResponseDto.create("게시글 수정 완료", updatedBoardId));
+//    }
 }
