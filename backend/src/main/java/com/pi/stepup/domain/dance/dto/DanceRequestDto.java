d0d411d1c4177ae0bbdd317208918a9b57cb2f0d
Seulgi <urina842@gmail.com>
2023-07-24 13:12:20 +0900

    :hammer: Refactor(dance/dto): DanceRequestDto @NoArgsConstructor, @DateTimeFormat 추가

--- backend/src/main/java/com/pi/stepup/domain/dance/dto/DanceRequestDto.java
+++ backend/src/main/java/com/pi/stepup/domain/dance/dto/DanceRequestDto.java
@@ -1,20 +1,26 @@
 package com.pi.stepup.domain.dance.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.pi.stepup.domain.dance.constant.DanceType;
 import com.pi.stepup.domain.dance.domain.RandomDance;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 public class DanceRequestDto {
 
     @Getter
+    @NoArgsConstructor
     public static class DanceSaveRequestDto {
 
         private Long randomDanceId;
         private String title;
         private String content;
+        @DateTimeFormat
         private LocalDateTime startAt;
+        @DateTimeFormat
         private LocalDateTime endAt;
         private DanceType danceType;
         private int maxUser;
