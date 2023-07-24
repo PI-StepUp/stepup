f79a3b26ba874bdef24e94836d0a240b8313146e
이주희 <joo9837@naver.com>
2023-07-24 17:08:14 +0900

    :heavy_plus_sign: Feat(board): 파일 생성 및 어노테이션 추가

--- backend/src/main/java/com/pi/stepup/domain/board/api/MeetingApiController.java
+++ backend/src/main/java/com/pi/stepup/domain/board/api/MeetingApiController.java
@@ -1,4 +1,15 @@
 package com.pi.stepup.domain.board.api;
 
+import com.pi.stepup.domain.board.service.MeetingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/board")
+@RestController
 public class MeetingApiController {
+
+    private final MeetingService meetingService;
+
 }
---
+++ backend/src/main/java/com/pi/stepup/domain/board/service/TalkServiceImpl.java
@@ -0,0 +1,9 @@
+package com.pi.stepup.domain.board.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TalkServiceImpl implements TalkService{
+}
