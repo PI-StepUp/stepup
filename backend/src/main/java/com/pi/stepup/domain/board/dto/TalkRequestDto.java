c5f4fde18d35eb8020202582a9a88bb1ce693fed
이주희 <joo9837@naver.com>
2023-07-24 17:06:51 +0900

    :hammer: Refactor(board/talk): dto 코드 수정

--- backend/src/main/java/com/pi/stepup/domain/board/dto/TalkRequestDto.java
+++ backend/src/main/java/com/pi/stepup/domain/board/dto/TalkRequestDto.java
@@ -1,7 +1,5 @@
 package com.pi.stepup.domain.board.dto;
 
-import com.pi.stepup.domain.board.domain.Comment;
-import com.pi.stepup.domain.board.domain.Meeting;
 import com.pi.stepup.domain.board.domain.Talk;
 import com.pi.stepup.domain.user.domain.User;
 import lombok.AllArgsConstructor;
@@ -9,30 +7,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 public class TalkRequestDto {
     @Data
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
     public static class TalkSaveRequestDto {
-        private User writer;
+        private String id;
         private String title;
         private String content;
         private String fileURL;
         private int commentCnt;
 
-        public Talk toEntity(){
-            return Talk.builder()
-                    .writer(writer)
-                    .title(title)
-                    .content(content)
-                    .fileURL(fileURL)
-                    .commentCnt(commentCnt)
-                    .build();
-        }
     }
 
     @Data
