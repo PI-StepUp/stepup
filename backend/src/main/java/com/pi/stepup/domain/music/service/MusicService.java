1e036458bc7e4af5e175e945c21b0be897594c82
이가경 <rabbit1999k@naver.com>
2023-07-25 11:13:47 +0900

    🔨 Refactor(music/readOne): service 리턴 타입 변경
    - Optional -> response dto

--- backend/src/main/java/com/pi/stepup/domain/music/service/MusicService.java
+++ backend/src/main/java/com/pi/stepup/domain/music/service/MusicService.java
@@ -5,12 +5,11 @@ import com.pi.stepup.domain.music.dto.MusicRequestDto.MusicSaveRequestDto;
 import com.pi.stepup.domain.music.dto.MusicResponseDto.MusicFindResponseDto;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface MusicService {
     Music create(MusicSaveRequestDto musicSaveRequestDto);
 
-    Optional<Music> readOne(Long musicId);
+    MusicFindResponseDto readOne(Long musicId);
 
     List<MusicFindResponseDto> readAll(String keyword);
 
