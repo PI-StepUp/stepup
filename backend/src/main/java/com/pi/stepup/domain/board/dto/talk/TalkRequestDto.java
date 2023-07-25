b93a6c27d2336f7c2efbb6487e0f53056c5878aa
joo9837 <joo9837@naver.com>
2023-07-25 23:48:04 +0900

    ➕ Feat(board/talk): 자유게시판 dto 구현 (#483)
    - TalkRequestDto 구현
    - TalkResponseDto 구현

---
+++ backend/src/main/java/com/pi/stepup/domain/board/dto/talk/TalkRequestDto.java
@@ -0,0 +1,31 @@
+package com.pi.stepup.domain.board.dto.talk;
+
+import com.pi.stepup.domain.board.domain.Talk;
+import com.pi.stepup.domain.user.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class TalkRequestDto {
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TalkSaveRequestDto {
+        private String id;
+        private String title;
+        private String content;
+        private String fileURL;
+    }
+
+    public static class TalkUpdateRequestDto {
+        private int boardId;
+        private String title;
+        private String content;
+        private String writerName;
+        private String writerProfileImg;
+        private String fileURL;
+        private String boardType;
+    }
+}
