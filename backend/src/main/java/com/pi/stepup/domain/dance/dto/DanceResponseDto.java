b0212953ad9c49bbaaac2978aaf0a3b0bb3bc821
seul <l.23.53.l2l9@gmail.com>
2023-07-26 00:08:18 +0900

    :heavy_plus_sign: Feat(dance/entity): DanceResponseDto DanceFindResponseDto 추가 (#346)

--- backend/src/main/java/com/pi/stepup/domain/dance/dto/DanceResponseDto.java
+++ backend/src/main/java/com/pi/stepup/domain/dance/dto/DanceResponseDto.java
@@ -4,64 +4,35 @@ import com.pi.stepup.domain.dance.domain.RandomDance;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 public class DanceResponseDto {
 
     @Getter
-    public static class DanceSaveResponseDto {
+    public static class DanceFindResponseDto {
 
         private final Long randomDanceId;
-        //        private final String title;
-//        private final String content;
-//        private final LocalDateTime startAt;
-//        private final LocalDateTime endAt;
-//        private final DanceType danceType;
-//        private final int maxUser;
-//        private final String thumbnail;
+        private final String title;
+        private final String content;
+        private final LocalDateTime startAt;
+        private final LocalDateTime endAt;
+        private final String danceType;
+        private final int maxUser;
+        private final String thumbnail;
         private final String hostName;
-//        private final List<DanceMusicFindResponseDto> danceMusicFindResponseDtoList = new ArrayList<>();
 
-        //        private final List<Reservation> reservation = new ArrayList<>();
         @Builder
-        private DanceSaveResponseDto(RandomDance randomDance) {
+        private DanceFindResponseDto(RandomDance randomDance) {
             this.randomDanceId = randomDance.getRandomDanceId();
+            this.title = randomDance.getTitle();
+            this.content = randomDance.getContent();
+            this.startAt = randomDance.getStartAt();
+            this.endAt = randomDance.getEndAt();
+            this.danceType = String.valueOf(randomDance.getDanceType());
+            this.maxUser = randomDance.getMaxUser();
+            this.thumbnail = randomDance.getThumbnail();
             this.hostName = randomDance.getHost().getNickname();
-
-//            randomDance.getDanceMusicList()
-//                .forEach(oi -> this.danceMusicFindResponseDtoList.add(
-//                    new DanceMusicFindResponseDto(oi)));
         }
     }
-
-//    @Getter
-//    public static class UserFindResponseDto {
-//
-//        private final Long userId;
-//        private final String id;
-//        private final Country country;
-//        private final String nickname;
-//
-//        public UserFindResponseDto(User user) {
-//            this.userId = user.getUserId();
-//            this.id = user.getId();
-//            this.country = user.getCountry();
-//            this.nickname = user.getNickname();
-//        }
-//    }
-//
-//    @Getter
-//    public static class DanceMusicFindResponseDto {
-//
-//        private final Long musicId;
-//        private final String title;
-//        private final String artist;
-////        private final String answer;
-////        private final String URL;
-//
-//        public DanceMusicFindResponseDto(DanceMusic danceMusic) {
-//            this.musicId = danceMusic.getMusic().getMusicId();
-//            this.title = danceMusic.getMusic().getTitle();
-//            this.artist = danceMusic.getMusic().getArtist();
-//        }
-//    }
 
 }
