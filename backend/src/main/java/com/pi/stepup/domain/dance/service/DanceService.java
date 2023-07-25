3295683b297742a655c1e330e8e3072101dfda6a
Seulgi <urina842@gmail.com>
2023-07-25 18:14:45 +0900

    :heavy_plus_sign: Feat(dance/service): DanceService 변경 (#315)
    - 랜플댄 생성 시 user 및 music 추가
    - 노래 목록 조회 구현 중

--- backend/src/main/java/com/pi/stepup/domain/dance/service/DanceService.java
+++ backend/src/main/java/com/pi/stepup/domain/dance/service/DanceService.java
@@ -1,15 +1,20 @@
 package com.pi.stepup.domain.dance.service;
 
+import com.pi.stepup.domain.dance.domain.DanceMusic;
 import com.pi.stepup.domain.dance.domain.RandomDance;
-import com.pi.stepup.domain.dance.dto.DanceRequestDto.DanceSaveRequestDto;
+import com.pi.stepup.domain.dance.dto.DanceRequestDto.DanceCreateRequestDto;
+import com.pi.stepup.domain.dance.dto.DanceRequestDto.DanceUpdateRequestDto;
+import java.util.List;
 
 public interface DanceService {
 
-    RandomDance create(DanceSaveRequestDto danceSaveRequestDto);
+    RandomDance create(DanceCreateRequestDto danceCreateRequestDto);
 
     RandomDance readOne(Long randomDanceId);
 
-    RandomDance update(DanceSaveRequestDto danceSaveRequestDto);
+    RandomDance update(DanceUpdateRequestDto danceUpdateRequestDto);
 
     void delete(Long randomDanceId);
+
+    List<DanceMusic> readAllMusic(Long randomDanceId);
 }
--- backend/src/main/java/com/pi/stepup/domain/dance/service/DanceServiceImpl.java
+++ backend/src/main/java/com/pi/stepup/domain/dance/service/DanceServiceImpl.java
@@ -1,44 +1,90 @@
 package com.pi.stepup.domain.dance.service;
 
 import com.pi.stepup.domain.dance.dao.DanceRepository;
+import com.pi.stepup.domain.dance.domain.DanceMusic;
 import com.pi.stepup.domain.dance.domain.RandomDance;
-import com.pi.stepup.domain.dance.dto.DanceRequestDto.DanceSaveRequestDto;
+import com.pi.stepup.domain.dance.dto.DanceRequestDto.DanceCreateRequestDto;
+import com.pi.stepup.domain.dance.dto.DanceRequestDto.DanceUpdateRequestDto;
+import com.pi.stepup.domain.music.dao.MusicRepository;
+import com.pi.stepup.domain.music.domain.Music;
+import com.pi.stepup.domain.user.dao.UserRepository;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class DanceServiceImpl implements DanceService {
 
     private final DanceRepository danceRepository;
+    private final UserRepository userRepository;
+    private final MusicRepository musicRepository;
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
+        "yyyy-MM-dd HH:mm");
 
     @Override
     @Transactional
-    public RandomDance create(DanceSaveRequestDto danceSaveRequestDto) {
-        RandomDance randomDance = danceSaveRequestDto.toEntity();
-        RandomDance createdDance = danceRepository.insert(randomDance);
+    public RandomDance create(DanceCreateRequestDto danceCreateRequestDto) {
+        RandomDance randomDance
+            = RandomDance.builder()
+            .title(danceCreateRequestDto.getTitle())
+            .content(danceCreateRequestDto.getContent())
+            .startAt(LocalDateTime.parse(danceCreateRequestDto.getStartAt(), formatter))
+            .endAt(LocalDateTime.parse(danceCreateRequestDto.getEndAt(), formatter))
+            .danceType(danceCreateRequestDto.getDanceType())
+            .maxUser(danceCreateRequestDto.getMaxUser())
+            .thumbnail(danceCreateRequestDto.getThumbnail())
+            .host(userRepository.findById(danceCreateRequestDto.getHostId()).orElseThrow())
+            .build();
 
+        List<Long> danceMusicIdList = danceCreateRequestDto.getDanceMusicIdList();
+        for (int i = 0; i < danceMusicIdList.size(); i++) {
+            Music music = musicRepository.findOne(danceMusicIdList.get(i)).orElseThrow();
+//            DanceMusic danceMusic
+//                = DanceMusic.builder()
+////                .randomDance(randomDance)
+//                .music(music)
+//                .build();
+            DanceMusic.createDanceMusic(music, randomDance);
+
+//            randomDance.addDanceMusicAndSetThis(danceMusic);
+//            danceMusic.setRandomDanceAndAddThis(randomDance);
+        }
+
+        RandomDance createdDance = danceRepository.insert(randomDance);
         return createdDance;
     }
 
     @Override
     public RandomDance readOne(Long randomDanceId) {
-        return danceRepository.findOne(randomDanceId);
+        return danceRepository.findOne(randomDanceId).orElseThrow();
     }
 
     @Override
     @Transactional
-    public RandomDance update(DanceSaveRequestDto danceSaveRequestDto) {
-        RandomDance randomDance = danceSaveRequestDto.toEntity();
-        return danceRepository.update(randomDance);
+    public RandomDance update(DanceUpdateRequestDto danceUpdateRequestDto) {
+//        RandomDance randomDance = danceUpdateRequestDto.toEntity();
+//        return danceRepository.update(randomDance);
+        return null;
     }
 
     @Override
     @Transactional
     public void delete(Long randomDanceId) {
         danceRepository.delete(randomDanceId);
+    }
+
+    @Override
+    public List<DanceMusic> readAllMusic(Long randomDanceId) {
+//        RandomDance findDance = danceRepository.findOne(randomDanceId);
+//        danceRepository.findAllMusic(randomDanceId);
+//        return findDance.getDanceMusicList();
+        return null;
     }
 
 }
