6ca44c176425b4e34e69c2e31bada98780e9aef2
joo9837 <joo9837@naver.com>
2023-07-26 00:39:43 +0900

    ➕ Feat(board/service): 자유게시판 service 구현 (#484)
    - create 구현
    - update 구현
    - readAll 구현
    - readOne 구현
    - delete 구현

---
+++ backend/src/main/java/com/pi/stepup/domain/board/service/talk/TalkService.java
@@ -0,0 +1,22 @@
+package com.pi.stepup.domain.board.service.talk;
+
+import com.pi.stepup.domain.board.domain.Talk;
+import com.pi.stepup.domain.board.dto.talk.TalkRequestDto.TalkSaveRequestDto;
+import com.pi.stepup.domain.board.dto.talk.TalkRequestDto.TalkUpdateRequestDto;
+import com.pi.stepup.domain.board.dto.talk.TalkResponseDto.TalkInfoResponseDto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TalkService {
+    Talk create(TalkSaveRequestDto talkSaveRequestDto);
+
+    Talk update(TalkUpdateRequestDto talkUpdateRequestDto);
+
+    List<TalkInfoResponseDto> readAll(String keyword);
+
+    Optional<TalkInfoResponseDto> readOne(Long boardId);
+
+    void delete(Long boardId);
+
+}
