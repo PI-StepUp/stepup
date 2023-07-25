25eeff190c327252cec8b5d113de81b4c52ec7f7
Seulgi <urina842@gmail.com>
2023-07-25 09:50:58 +0900

    :heavy_plus_sign: Feat(dance/dto): DanceRequestDto toEntity 메서드 추가 (#304)

--- backend/src/main/java/com/pi/stepup/domain/dance/dto/DanceRequestDto.java
+++ backend/src/main/java/com/pi/stepup/domain/dance/dto/DanceRequestDto.java
@@ -1,41 +1,47 @@
 package com.pi.stepup.domain.dance.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.pi.stepup.domain.dance.constant.DanceType;
 import com.pi.stepup.domain.dance.domain.RandomDance;
+import com.pi.stepup.domain.user.domain.User;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 public class DanceRequestDto {
 
-    @Getter
+    @Data
+    @Builder
     @NoArgsConstructor
+    @AllArgsConstructor
     public static class DanceSaveRequestDto {
 
         private Long randomDanceId;
         private String title;
         private String content;
-        @DateTimeFormat
-        private LocalDateTime startAt;
-        @DateTimeFormat
-        private LocalDateTime endAt;
+        private String startAt;
+        private String endAt;
         private DanceType danceType;
         private int maxUser;
         private String thumbnail;
+        private User host;
 
-        @Builder
-        private DanceSaveRequestDto(RandomDance randomDance) {
-            this.randomDanceId = randomDance.getRandomDanceId();
-            this.title = randomDance.getTitle();
-            this.content = randomDance.getContent();
-            this.startAt = randomDance.getStartAt();
-            this.endAt = randomDance.getEndAt();
-            this.danceType = randomDance.getDanceType();
-            this.maxUser = randomDance.getMaxUser();
-            this.thumbnail = randomDance.getThumbnail();
+        public RandomDance toEntity() {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+            return RandomDance.builder()
+                .randomDanceId(this.randomDanceId)
+                .title(this.title)
+                .content(this.content)
+                .startAt(LocalDateTime.parse(this.startAt, formatter))
+                .endAt(LocalDateTime.parse(this.endAt, formatter))
+                .danceType(this.danceType)
+                .maxUser(this.maxUser)
+                .thumbnail(this.thumbnail)
+                .host(this.host)
+                .build();
         }
     }
 
