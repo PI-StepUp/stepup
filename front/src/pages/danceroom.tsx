3e8a57d6450527b11758955d8a17692b67abe9d6
livinoid98 <ccy9803@gmail.com>
2023-07-26 17:56:03 +0900

    :heavy_plus_sign: Feat(function/cam): webRTC 1:1 webcam 구현 (#290)
    - webrtc 자신 webcam stream 자기 video 태그에 띄우기
    - offer -> answer -> candidate 과정 통해 시그널링 서버 이용하여 IP 전달
    - 연결 이후 다른 사용자 webcam 띄우기 완료

--- front/src/pages/danceroom.tsx
+++ front/src/pages/danceroom.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useRef, useEffect, KeyboardEvent} from "react";
-import socketIOClient from "socket.io-client";
+import socketIOClient, {Socket} from "socket.io-client";
 
 import SideMenu from "components/SideMenu";
 
@@ -18,49 +18,16 @@ import CameraHoverIcon from "/public/images/icon-hover-camera.svg"
 import MoreDotHoverIcon from "/public/images/icon-hover-more-dot.svg"
 
 const DanceRoom = () => {
-    let myStream : any;
-    let myPeerConnection : any;
-    const [muteState, setMuteState] = useState(false);
-    const [cameraState, setCameraState] = useState(false);
 
+    const myName = "임시 이름";
+    let peerNames : any = [];
+    const [participantNum, setParticipantNum] = useState(1);
+    const socketRef = useRef<Socket>();
+
+    const [peer, setPeer] = useState('');
     const myVideoRef = useRef<HTMLVideoElement>(null);
-    useEffect(() => {
-        getMedia();
-    })
-    const getMedia = async () => {
-        try{
-            myStream = await navigator.mediaDevices.getUserMedia({
-                audio: true,
-                video: true,
-            });
-            if(myVideoRef.current != null){
-                console.log(myStream);
-                myVideoRef.current.srcObject = myStream;
-                myVideoRef.current.play();
-            }
-        }catch(e){
-            console.log(e);
-        }
-    }
-
-    const handleMuteClick = () => {
-        myStream?.getAudioTracks().forEach((track : any) => (track.enabled = !track.enabled));
-        if(!muteState){
-            setMuteState(true);
-        }else{
-            setMuteState(false);
-        }
-    }
-
-    const handleCameraClick = () => {
-        myStream?.getVideoTracks().forEach((track : any) => (track.enabled = !track.enabled));
-        if(!cameraState){
-            setCameraState(false);
-        }else{
-            setCameraState(true);
-        }
-    }
-
+    const otherVideoRef = useRef<HTMLVideoElement>(null);
+    const peerRef = useRef<RTCPeerConnection>();
 
     //socket.io 통신 및 채팅방 구현
     const socket = socketIOClient("localhost:4002");
@@ -71,6 +38,7 @@ const DanceRoom = () => {
     const inputChat = useRef(null);
     const chatContent = useRef(null);
     const sendMessage = () => {
+        console.log(msg);
         socket.emit("send_message", msg);
         if(inputChat.current != null){
             setMsg("");
@@ -91,6 +59,7 @@ const DanceRoom = () => {
             scrollToBottom();
         }
     };
+
     socket.on("message", (message) => {
         setMsgList([...msgList, message]);
     });
@@ -126,22 +95,164 @@ const DanceRoom = () => {
         setMoredot(false);
     }
 
+    const getMedia = async () => {
+        try{
+            const stream = await navigator.mediaDevices.getUserMedia({
+                video: true,
+                audio: true,
+            });
+
+            if(myVideoRef.current){
+                myVideoRef.current.srcObject = stream;
+                myVideoRef.current.play();
+            }
+
+            stream.getTracks().forEach((track) => {
+                if(!peerRef.current){
+                    return;
+                }
+                peerRef.current.addTrack(track, stream);
+            });
+
+            if(peerRef.current){
+                peerRef.current.onicecandidate = (e) => {
+                    if(e.candidate){
+                        if(!socketRef.current){
+                            return;
+                        }
+
+                        console.log("recv candidate");
+                        socketRef.current.emit("candidate", e.candidate, [roomName, myName]);
+                    }
+                }
+            }
+
+            if(peerRef.current){
+                peerRef.current.ontrack = (e) => {
+                    if(otherVideoRef.current){
+                        console.log("e", e);
+                        otherVideoRef.current.srcObject = e.streams[0];
+                        otherVideoRef.current.play();
+                    }
+                }
+            }
+
+            createOffer();
+        }catch(e){
+            console.error(e);
+        }
+    }
+
+    const createOffer = async () => {
+        console.log("create Offer");
+        if (!(peerRef.current && socketRef.current)) {
+          return;
+        }
+        try {
+          // offer 생성
+          const sdp = await peerRef.current.createOffer();
+          // 자신의 sdp로 LocalDescription 설정
+          peerRef.current.setLocalDescription(sdp);
+          console.log("sent the offer");
+          // offer 전달
+          socketRef.current.emit("offer", sdp, roomName);
+        } catch (e) {
+          console.error(e);
+        }
+      };
+      
+      const createAnswer = async (sdp: RTCSessionDescription) => {
+        // sdp : PeerA에게서 전달받은 offer
+      
+        console.log("createAnswer");
+        if (!(peerRef.current && socketRef.current)) {
+          return;
+        }
+    
+        try {
+          // PeerA가 전달해준 offer를 RemoteDescription에 등록해 줍시다.
+          peerRef.current.setRemoteDescription(sdp);
+          
+          // answer생성해주고
+          const answerSdp = await peerRef.current.createAnswer();
+          
+          // answer를 LocalDescription에 등록해 줍니다. (PeerB 기준)
+          peerRef.current.setLocalDescription(answerSdp);
+          console.log("sent the answer");
+          socketRef.current.emit("answer", answerSdp, roomName);
+        } catch (e) {
+          console.error(e);
+        }
+      };
+
     useEffect(() => {
         scrollToBottom();
-        myPeerConnection = new RTCPeerConnection({
+        socketRef.current = socketIOClient("localhost:4002");
+
+        peerRef.current = new RTCPeerConnection({
             iceServers: [
                 {
-                    urls: [
-                        "stun:stun.l.google.com:19302",
-                        "stun:stun1.l.google.com:19302",
-                        "stun:stun2.l.google.com:19302",
-                        "stun:stun3.l.google.com:19302",
-                        "stun:stun4.l.google.com:19302",
-                    ],
+                    urls: "stun:stun.l.google.com:19302",
                 },
             ],
         });
-    })
+
+        socketRef.current.on("all_users", (allUsers: Array<{ id: string }>) => {
+            if (allUsers.length > 0) {
+              createOffer();
+            }
+          });
+          
+          // offer를 전달받은 PeerB만 해당됩니다
+          // offer를 들고 만들어둔 answer 함수 실행
+          socketRef.current.on("getOffer", (sdp: RTCSessionDescription) => {
+            console.log("recv Offer");
+            createAnswer(sdp);
+          });
+          
+          // answer를 전달받을 PeerA만 해당됩니다.
+          // answer를 전달받아 PeerA의 RemoteDescription에 등록
+          socketRef.current.on("getAnswer", (sdp: RTCSessionDescription) => {
+            console.log("recv Answer");
+            if (!peerRef.current) {
+              return;
+            }
+            if(peerRef.current){
+                peerRef.current.setRemoteDescription(sdp);
+            }
+          });
+          
+          // 서로의 candidate를 전달받아 등록
+          socketRef.current.on("getCandidate", async (candidate: RTCIceCandidate, peerName) => {
+            if (!peerRef.current) {
+              return;
+            }
+      
+            await peerRef.current.addIceCandidate(candidate);
+            peerNames.push(peerName);
+            setPeer(peerName);
+            setParticipantNum(participantNum + 1);
+          });
+          
+          // 마운트시 해당 방의 roomName을 서버에 전달
+          socketRef.current.emit("join_room", {
+            room: roomName,
+          });
+      
+          getMedia();
+      
+          return () => {
+            // 언마운트시 socket disconnect
+            if (socketRef.current) {
+              socketRef.current.disconnect();
+            }
+            if (peerRef.current) {
+              peerRef.current.close();
+            }
+          };
+
+    }, []);
+
     return(
         <>
             <div className="practiceroom-wrap">
@@ -159,15 +270,12 @@ const DanceRoom = () => {
 
                     <div className="video-content">
                         <div className="my-video">
-                            <video src="" id="peerFace" playsInline ref={myVideoRef}></video>
+                            <video src="" playsInline ref={myVideoRef}></video>
                         </div>
                         <div className="yours-video">
                             <ul>
                                 <li>
-                                    <video src="" playsInline ></video>
-                                </li>
-                                <li>
-                                    <video src=""></video>
+                                    <video src="" playsInline ref={otherVideoRef}></video>
                                 </li>
                             </ul>
                         </div>
@@ -178,13 +286,13 @@ const DanceRoom = () => {
                                         {reflect ? <Image src={ReflectHoverIcon} alt=""/> : <Image src={ReflectIcon} alt=""/>}
                                     </button>
                                 </li>
-                                <li onMouseEnter = {micHover} onMouseLeave = {micLeave} id="mute" onClick={handleMuteClick}>
+                                <li onMouseEnter = {micHover} onMouseLeave = {micLeave} id="mute">
                                     <button>
                                         {mic ? <Image src={MicHoverIcon} alt=""/> : <Image src={MicIcon} alt=""/>}
                                     </button>
                                 </li>
                                 <li><button className="exit-button">연습 종료하기</button></li>
-                                <li onMouseEnter = {cameraHover} onMouseLeave = {cameraLeave} id="camera" onClick={handleCameraClick}>
+                                <li onMouseEnter = {cameraHover} onMouseLeave = {cameraLeave} id="camera">
                                     <button>
                                         {camera ? <Image src={CameraHoverIcon} alt=""/> : <Image src={CameraIcon} alt=""/>}
                                     </button>
@@ -203,15 +311,22 @@ const DanceRoom = () => {
                     <div className="participant-list">
                         <div className="participant-list-title">
                             <h3>참여자 목록</h3>
-                            <span><Image src={GroupIcon} alt=""/>5명</span>
+                            <span><Image src={GroupIcon} alt=""/>{participantNum}명</span>
                         </div>
                         <div className="participant-list-content">
                             <ul>
                                 <li className="on">
                                         <Image src={ChatDefaultImg} alt=""/>
-                                        <span>ME</span>
+                                        <span>{myName}</span>
                                 </li>
-                                <li>
+                                {
+                                    peer.length > 0 ? <li>
+                                    <Image src={ChatDefaultImg} alt=""/>
+                                    <span>{peer}</span></li> : ""
+                            
+                                }
+                                
+                                {/* <li>
                                         <Image src={ChatDefaultImg} alt=""/>
                                         <span>이싸피</span>
                                 </li>
@@ -226,7 +341,7 @@ const DanceRoom = () => {
                                 <li>
                                         <Image src={ChatDefaultImg} alt=""/>
                                         <span>최싸피</span>
-                                </li>
+                                </li> */}
                             </ul>
                         </div>
                     </div>
--- socket/server.ts
+++ socket/server.ts
@@ -16,18 +16,79 @@ const io = socketIO(server, {
     }
 });
 
+let users = {};
+let socketRoom = {};
+const MAXIMUM = 6;
+
 io.on("connection", (socket) => {
-    console.log("User connected");
-    socket.on("enter_room", (roomName, done) => {
-        socket.join(roomName);
-        socket.on("send_message", (item) => {
-            socket.to(roomName).emit("message", item);
-        })
+    console.log(socket.id, "connection");
+    socket.on("join_room", (data) => {
+      // 방이 기존에 생성되어 있다면
+      if (users[data.room]) {
+        // 현재 입장하려는 방에 있는 인원수
+        const currentRoomLength = users[data.room].length;
+        if (currentRoomLength === MAXIMUM) {
+          // 인원수가 꽉 찼다면 돌아갑니다.
+          socket.to(socket.id).emit("room_full");
+          return;
+        }
+  
+        // 여분의 자리가 있다면 해당 방 배열에 추가해줍니다.
+        users[data.room] = [...users[data.room], { id: socket.id }];
+      } else {
+        // 방이 존재하지 않다면 값을 생성하고 추가해줍시다.
+        users[data.room] = [{ id: socket.id }];
+      }
+      socketRoom[socket.id] = data.room;
+  
+      // 입장
+      socket.join(data.room);
+
+      
+  
+      // 입장하기 전 해당 방의 다른 유저들이 있는지 확인하고
+      // 다른 유저가 있었다면 offer-answer을 위해 알려줍니다.
+      const others = users[data.room].filter((user) => user.id !== socket.id);
+      if (others.length) {
+        io.sockets.to(socket.id).emit("all_users", others);
+      }
+    });
+
+    socket.on("send_message", (data) => {
+      socket.to(data.room).emit("message", data);
     })
-    socket.on("disconnected", () => {
-        console.log("User disconnected");
-    })
-});
+
+  
+    socket.on("offer", (sdp, roomName) => {
+      // offer를 전달받고 다른 유저들에게 전달해 줍니다.
+      socket.to(roomName).emit("getOffer", sdp);
+    });
+  
+    socket.on("answer", (sdp, roomName) => {
+      // answer를 전달받고 방의 다른 유저들에게 전달해 줍니다.
+      socket.to(roomName).emit("getAnswer", sdp);
+    });
+  
+    socket.on("candidate", (candidate, array) => {
+      // candidate를 전달받고 방의 다른 유저들에게 전달해 줍니다.
+      socket.to(array[0]).emit("getCandidate", candidate, array[1]);
+    });
+  
+    socket.on("disconnect", () => {
+      // 방을 나가게 된다면 socketRoom과 users의 정보에서 해당 유저를 지워줍니다.
+      const roomID = socketRoom[socket.id];
+  
+      if (users[roomID]) {
+        users[roomID] = users[roomID].filter((user) => user.id !== socket.id);
+        if (users[roomID].length === 0) {
+          delete users[roomID];
+          return;
+        }
+      }
+      delete socketRoom[socket.id];
+      socket.broadcast.to(users[roomID]).emit("user_exit", { id: socket.id });
+    });
+  });
 
 server.listen(port, () => {
     console.log(`server listening on port ${port}`);
