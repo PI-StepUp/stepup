b87fa9e275087e25ef009f21e8a26a26a86cea15
이가경 <rabbit1999k@naver.com>
2023-07-26 22:15:57 +0900

    ✅ Test(rank/pointHistory): 포인트 적립 내역 service 테스트 코드 구현 (#500)

--- backend/src/test/java/com/pi/stepup/domain/rank/service/PointHistoryServiceTest.java
+++ backend/src/test/java/com/pi/stepup/domain/rank/service/PointHistoryServiceTest.java
@@ -2,19 +2,24 @@ package com.pi.stepup.domain.rank.service;
 
 import com.pi.stepup.domain.dance.constant.DanceType;
 import com.pi.stepup.domain.dance.domain.RandomDance;
+import com.pi.stepup.domain.rank.dao.PointHistoryRepository;
 import com.pi.stepup.domain.rank.dao.PointPolicyRepository;
 import com.pi.stepup.domain.rank.domain.PointHistory;
 import com.pi.stepup.domain.rank.domain.PointPolicy;
 import com.pi.stepup.domain.rank.dto.RankRequestDto.PointUpdateRequestDto;
+import com.pi.stepup.domain.rank.dto.RankResponseDto.PointHistoryFindResponseDto;
 import com.pi.stepup.domain.user.dao.UserRepository;
 import com.pi.stepup.domain.user.domain.User;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static com.pi.stepup.domain.rank.constant.PointType.FIRST_PRIZE;
@@ -33,11 +38,15 @@ class PointHistoryServiceTest {
     @Mock
     PointPolicyRepository pointPolicyRepository;
 
+    @Mock
+    PointHistoryRepository pointHistoryRepository;
+
     private PointHistory pointHistory;
     private User user;
     private PointPolicy pointPolicy;
     private PointUpdateRequestDto pointUpdateRequestDto;
     private RandomDance randomDance;
+    private List<PointHistory> pointHistories;
 
     @Test
     @BeforeEach
@@ -50,6 +59,7 @@ class PointHistoryServiceTest {
     }
 
     @Test
+    @DisplayName("포인트 적립 테스트")
     public void pointUpdateServiceTest() {
         when(userRepository.findById(any())).thenReturn(Optional.ofNullable(user));
         when(pointPolicyRepository.findOne(any())).thenReturn(Optional.ofNullable(pointPolicy));
@@ -57,6 +67,18 @@ class PointHistoryServiceTest {
         pointHistoryService.update(pointUpdateRequestDto);
 
         assertThat(user.getPoint()).isEqualTo(FIRST_PRIZE.getPoint() * 2);
+    }
+
+    @Test
+    @DisplayName("포인트 적립 내역 조회 테스트")
+    public void readAllPointHistoryServiceTest() {
+        makePointHistories();
+        when(pointHistoryRepository.findAll()).thenReturn(pointHistories);
+
+        List<PointHistoryFindResponseDto> pointHistoryFindResponseDtos
+                = pointHistoryService.readAll();
+
+        assertThat(pointHistories.size()).isEqualTo(pointHistoryFindResponseDtos.size());
     }
 
     private void makePointPolicy() {
@@ -93,5 +115,18 @@ class PointHistoryServiceTest {
                 .content("content")
                 .danceType(DanceType.RANKING)
                 .build();
+    }
+
+    private List<PointHistory> makePointHistories() {
+        pointHistories = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            PointHistory tmp = PointHistory.builder()
+                    .user(user)
+                    .randomDance(randomDance)
+                    .count(0)
+                    .build();
+            pointHistories.add(tmp);
+        }
+        return pointHistories;
     }
 }
