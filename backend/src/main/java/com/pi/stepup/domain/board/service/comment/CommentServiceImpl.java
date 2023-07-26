e5572a7e5ed83a84c0d19ed5ff1cec3c1bfa26bb
joo9837 <joo9837@naver.com>
2023-07-27 06:05:08 +0900

    ➕ Feat(board/service): 정모, 자유게시판 댓글 카운팅 로직 추가 (#509)

--- backend/src/main/java/com/pi/stepup/domain/board/service/comment/CommentServiceImpl.java
+++ backend/src/main/java/com/pi/stepup/domain/board/service/comment/CommentServiceImpl.java
@@ -2,8 +2,12 @@ package com.pi.stepup.domain.board.service.comment;
 
 import com.pi.stepup.domain.board.dao.board.BoardRepository;
 import com.pi.stepup.domain.board.dao.comment.CommentRepository;
+import com.pi.stepup.domain.board.dao.meeting.MeetingRepository;
+import com.pi.stepup.domain.board.dao.talk.TalkRepository;
 import com.pi.stepup.domain.board.domain.Board;
 import com.pi.stepup.domain.board.domain.Comment;
+import com.pi.stepup.domain.board.domain.Meeting;
+import com.pi.stepup.domain.board.domain.Talk;
 import com.pi.stepup.domain.board.dto.comment.CommentRequestDto.CommentSaveRequestDto;
 import com.pi.stepup.domain.board.dto.comment.CommentResponseDto.CommentInfoResponseDto;
 import com.pi.stepup.domain.user.dao.UserRepository;
@@ -22,6 +26,8 @@ public class CommentServiceImpl implements CommentService {
     private final CommentRepository commentRepository;
     private final UserRepository userRepository;
     private final BoardRepository boardRepository;
+    private final MeetingRepository meetingRepository;
+    private final TalkRepository talkRepository;
 
     @Transactional
     @Override
@@ -38,7 +44,19 @@ public class CommentServiceImpl implements CommentService {
                 .content(commentSaveRequestDto.getContent())
                 .build();
 
-        return commentRepository.insert(comment);
+        Comment savedComment = commentRepository.insert(comment);
+
+        if (board instanceof Talk) {
+            Talk talk = (Talk) board;
+            talk.updateCommentCnt(); // 댓글 수 업데이트
+            talkRepository.insert(talk);
+        } else if (board instanceof Meeting) {
+            Meeting meeting = (Meeting) board;
+            meeting.updateCommentCnt(); // 댓글 수 업데이트
+            meetingRepository.insert(meeting);
+        }
+
+        return savedComment;
     }
 
 
