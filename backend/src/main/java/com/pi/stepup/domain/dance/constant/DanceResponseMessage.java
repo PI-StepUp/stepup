96496d21018f8f97695b48bb5724b1b530f4dd20
Seulgi <urina842@gmail.com>
2023-07-26 17:35:42 +0900

    :hammer: Refactor(dance/messageName): DanceResponseMessage 메세지명 수정 (#309)

--- backend/src/main/java/com/pi/stepup/domain/dance/constant/DanceResponseMessage.java
+++ backend/src/main/java/com/pi/stepup/domain/dance/constant/DanceResponseMessage.java
@@ -3,21 +3,22 @@ package com.pi.stepup.domain.dance.constant;
 public enum DanceResponseMessage {
 
     CREATE_RANDOM_DANCE("랜덤 플레이 댄스 생성 완료"),
-    UPDATE_CREATED_RANDOM_DANCE("랜덤 플레이 댄스 수정 완료"),
-    DELETE_CREATED_RANDOM_DANCE("랜덤 플레이 댄스 삭제 완료"),
-    SELECT_ALL_MUSIC("노래 목록 조회 완료"),
-    SELECT_ALL_RANDOM_DANCE("랜덤 플레이 댄스 목록 조회 완료"),
-    SELECT_HELD_RANDOM_DANCE("내가 개최한 랜덤 플레이 댄스 목록 조회 완료"),
-    JOINED_RANDOM_DANCE("랜덤 플레이 댄스 예약"),
-    SELECT_JOINED_RANDOM_DANCE("내가 예약한 랜덤 플레이 댄스 목록 조회 완료"),
-    DELETE_JOINED_RANDOM_DANCE("랜덤 플레이 댄스 예약 취소");
+    UPDATE_OPEN_RANDOM_DANCE("랜덤 플레이 댄스 수정 완료"),
+    DELETE_OPEN_RANDOM_DANCE("랜덤 플레이 댄스 삭제 완료"),
+    SELECT_ALL_OPEN_RANDOM_DANCE("내가 개최한 랜덤 플레이 댄스 목록 조회 완료"),
+    SELECT_ALL_DANCE_MUSIC("사용 노래 목록 조회 완료"),
+    SELECT_ALL_RANDOM_DANCE("모든 랜덤 플레이 댄스 목록 조회 완료"),
+    RESERVE_RANDOM_DANCE("랜덤 플레이 댄스 예약 완료"),
+    DELETE_RESERVE_RANDOM_DANCE("랜덤 플레이 댄스 예약 취소 완료"),
+    SELECT_ALL_RESERVE_RANDOM_DANCE("내가 예약한 랜덤 플레이 댄스 목록 조회 완료"),
+    ATTEND_RANDOM_DANCE("랜덤 플레이 댄스 참여 완료"),
+    DELETE_ATTEND_RANDOM_DANCE("랜덤 플레이 댄스 참여 취소 완료"),
+    SELECT_ALL_ATTEND_RANDOM_DANCE("내가 참여한 랜덤 플레이 댄스 목록 조회 완료");
 
     private final String message;
-
     DanceResponseMessage(String message) {
         this.message = message;
     }
-
     public String getMessage() {
         return message;
     }
