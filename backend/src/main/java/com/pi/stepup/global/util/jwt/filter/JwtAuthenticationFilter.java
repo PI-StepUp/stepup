22e43b2d2beac51553545bd9b99d0c1eec86159e
Sehbeom <j3beom@gmail.com>
2023-07-27 05:13:58 +0900

    :sparkles: Style(user): import 제거 및 인증 경로 주석 해제 (#508)
    - JwtAuthenticationFilter 내 불필요 import 제거
    - 토큰 재발급 로직 구현을 위해 인증 경로 설정 주석 해제

--- backend/src/main/java/com/pi/stepup/global/util/jwt/filter/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/pi/stepup/global/util/jwt/filter/JwtAuthenticationFilter.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
 
 @RequiredArgsConstructor
