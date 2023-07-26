cdf4979734281abbe099742a51d3c16d92deec4a
이주희 <joo9837@naver.com>
2023-07-26 17:48:07 +0900

    ➕ Feat(board/api): 댓글 api 구현 (#494)
    - createComment 구현
    - deleteComment 구현

--- backend/src/main/java/com/pi/stepup/domain/board/api/CommentApiController.java
+++ backend/src/main/java/com/pi/stepup/domain/board/api/CommentApiController.java
@@ -1,15 +1,35 @@
 package com.pi.stepup.domain.board.api;
 
-import com.pi.stepup.domain.board.service.comment.CommentServiceImpl;
+import com.pi.stepup.domain.board.constant.BoardResponseMessage;
+import com.pi.stepup.domain.board.dto.comment.CommentRequestDto.CommentSaveRequestDto;
+import com.pi.stepup.domain.board.service.comment.CommentService;
+import com.pi.stepup.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/board")
+@RequestMapping("/api/comment")
 @RestController
 public class CommentApiController {
 
-    private final CommentServiceImpl commentService;
+    private final CommentService commentService;
 
+    @PostMapping("/{boardId}")
+    public ResponseEntity<ResponseDto<?>> createComment(@PathVariable Long boardId, @RequestBody CommentSaveRequestDto commentSaveRequestDto) {
+        commentSaveRequestDto.setBoardId(boardId);
+        commentService.create(commentSaveRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseDto.create(
+                BoardResponseMessage.CREATE_COMMENT.getMessage()
+        ));
+    }
+
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<ResponseDto<?>> deleteComment(@PathVariable Long commentId){
+        commentService.delete(commentId);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(
+                BoardResponseMessage.DELETE_COMMENT.getMessage()
+        ));
+    }
 }
