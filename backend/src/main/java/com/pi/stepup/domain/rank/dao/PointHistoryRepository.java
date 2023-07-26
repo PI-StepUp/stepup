39ae2b7a1f450f4abaa772077c7512d00fc723f3
이가경 <rabbit1999k@naver.com>
2023-07-26 21:59:47 +0900

    ➕ Feat(rank/pointHistory): 포인트 적립 내역 repo 구현 (#499)

---
+++ backend/src/main/java/com/pi/stepup/domain/rank/dao/PointHistoryRepository.java
@@ -0,0 +1,9 @@
+package com.pi.stepup.domain.rank.dao;
+
+import com.pi.stepup.domain.rank.domain.PointHistory;
+
+import java.util.List;
+
+public interface PointHistoryRepository {
+    List<PointHistory> findAll();
+}
---
+++ backend/src/main/java/com/pi/stepup/domain/rank/dao/PointHistoryRepositoryImpl.java
@@ -0,0 +1,22 @@
+package com.pi.stepup.domain.rank.dao;
+
+import com.pi.stepup.domain.rank.domain.PointHistory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class PointHistoryRepositoryImpl implements PointHistoryRepository {
+    private final EntityManager em;
+
+    @Override
+    public List<PointHistory> findAll() {
+        return em.createQuery(
+                        "SELECT ph FROM PointHistory ph", PointHistory.class
+                )
+                .getResultList();
+    }
+}
