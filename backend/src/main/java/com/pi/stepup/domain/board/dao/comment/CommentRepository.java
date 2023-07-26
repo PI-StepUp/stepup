10310ace9955d72840278d63c8be928fc416ec12
이주희 <joo9837@naver.com>
2023-07-26 17:50:04 +0900

    ➕ Feat(board/repo): 댓글 repo 구현 (#491)
    - insert 구현
    - findByBoardId 구현
    - delete 구현

--- backend/src/main/java/com/pi/stepup/domain/board/dao/comment/CommentRepository.java
+++ backend/src/main/java/com/pi/stepup/domain/board/dao/comment/CommentRepository.java
@@ -1,9 +1,12 @@
 package com.pi.stepup.domain.board.dao.comment;
 
 import com.pi.stepup.domain.board.domain.Board;
+import com.pi.stepup.domain.board.domain.Comment;
 
 import java.util.List;
 
 public interface CommentRepository {
-    List<Board> findByBoard(Board board);
+    Comment insert (Comment comment);
+    List<Comment> findByBoardId(Long boardId);
+    void delete(Long commemt);
 }
