d3677c52a1e9ad59184b489d2fcb2c2b1871bcc6
Sehbeom <j3beom@gmail.com>
2023-07-26 14:40:49 +0900

    :heavy_plus_sign: Feat(user/find-id): 아이디찾기 이메일 생성 로직 추가 (#467)
    - 아이디 찾기 이메일 가이드문장 Enum 추가
    - 이메일 내용 클래스 EmailContent 추가
    - 이메일 내용 생성 util EmailMessageMaker 추가

--- backend/src/main/java/com/pi/stepup/domain/user/util/EmailMessageMaker.java
+++ backend/src/main/java/com/pi/stepup/domain/user/util/EmailMessageMaker.java
@@ -1,5 +1,6 @@
 package com.pi.stepup.domain.user.util;
 
+import com.pi.stepup.domain.user.domain.EmailContent;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,23 +8,21 @@ import org.springframework.core.io.ClassPathResource;
 
 public class EmailMessageMaker {
 
-    public static String makeEmailMessage(String emailType, String dataTitle, String data) {
+    public static String makeEmailMessage(EmailContent emailContent) {
         try {
-            System.out.println("이메일 변환 완료!!!1111");
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new ClassPathResource("mail/find-mail.html").getInputStream()));
-            System.out.println("이메일 변환 완료!!!22222");
-            StringBuilder emailContent = new StringBuilder();
-            System.out.println("이메일 변환 완료!!!33333333");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+                new ClassPathResource("mail/find-mail.html").getInputStream()));
+            StringBuilder stringBuilder = new StringBuilder();
+
             while (bufferedReader.ready()) {
-                emailContent.append(bufferedReader.readLine());
+                stringBuilder.append(bufferedReader.readLine());
             }
 
-
-
-            return emailContent.toString()
-                .replace("{emailType}", emailType)
-                .replace("{dataTitle}", dataTitle)
-                .replace("{data}", data);
+            return stringBuilder.toString()
+                .replace("{mailType}", emailContent.getEmailGuideContent().getMailType())
+                .replace("{nickname}", emailContent.getNickname())
+                .replace("{dataTitle}", emailContent.getEmailGuideContent().getDataTitle())
+                .replace("{data}", emailContent.getData());
         } catch (IOException e) {
             e.printStackTrace();
             throw new IllegalArgumentException("이메일 전송 중 예외 발생");
