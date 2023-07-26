7bb8bb26dec26479e408557c297cbc3b982900ff
joo9837 <joo9837@naver.com>
2023-07-27 06:25:31 +0900

    ➕ Feat(board/api): 정모 api 구현 (#511)
    - create 구현
    - update 구현
    - readAll 구현
    - readOne 구현
    - delete 구현

--- backend/src/main/java/com/pi/stepup/domain/board/api/MeetingApiController.java
+++ backend/src/main/java/com/pi/stepup/domain/board/api/MeetingApiController.java
@@ -1,9 +1,14 @@
 package com.pi.stepup.domain.board.api;
 
+import com.pi.stepup.domain.board.constant.BoardResponseMessage;
+import com.pi.stepup.domain.board.dto.meeting.MeetingRequestDto.MeetingSaveRequestDto;
+import com.pi.stepup.domain.board.dto.meeting.MeetingRequestDto.MeetingUpdateRequestDto;
 import com.pi.stepup.domain.board.service.meeting.MeetingService;
+import com.pi.stepup.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/board")
@@ -12,4 +17,53 @@ public class MeetingApiController {
 
     private final MeetingService meetingService;
 
+    @PostMapping("/meeting")
+    public ResponseEntity<ResponseDto<?>> createMeeting(@RequestBody MeetingSaveRequestDto meetingSaveRequestDto) {
+
+        meetingService.create(meetingSaveRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseDto.create(
+                BoardResponseMessage.CREATE_MEETING.getMessage()
+        ));
+    }
+
+
+    @PutMapping("/meeting")
+    public ResponseEntity<ResponseDto<?>> updateMeeting(@RequestBody MeetingUpdateRequestDto meetingUpdateRequestDto) {
+
+        meetingService.update(meetingUpdateRequestDto).getBoardId();
+
+        return ResponseEntity.ok().body(ResponseDto.create(
+                BoardResponseMessage.UPDATE_MEETING.getMessage()
+        ));
+    }
+
+    @GetMapping("/meeting/{boardId}")
+    public ResponseEntity<ResponseDto<?>> readOneMeeting(@PathVariable Long boardId) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+                ResponseDto.create(
+                        BoardResponseMessage.READ_ONE_MEETING.getMessage(),
+                        meetingService.readOne(boardId)
+                )
+        );
+    }
+
+
+    @GetMapping("/meeting")
+    public ResponseEntity<ResponseDto<?>> readAllMeeting(@RequestParam(required = false) String keyword) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+                ResponseDto.create(
+                        BoardResponseMessage.READ_ALL_MEETING.getMessage(),
+                        meetingService.readAll(keyword)
+                )
+        );
+    }
+
+
+    @DeleteMapping("meeting/{boardId}")
+    public ResponseEntity<ResponseDto<?>> deleteMeeting(@PathVariable("boardId") Long boardId) {
+        meetingService.delete(boardId);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(
+                BoardResponseMessage.DELETE_MEETING.getMessage()
+        ));
+    }
 }
