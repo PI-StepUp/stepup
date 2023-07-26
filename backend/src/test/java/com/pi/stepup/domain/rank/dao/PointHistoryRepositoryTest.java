eefe767bf7f08b143e214be343adcd7c5a996386
이가경 <rabbit1999k@naver.com>
2023-07-26 21:50:35 +0900

    ✅ Test(rank/pointHistory): 포인트 적립 내역 repo 테스트 코드 구현 (#499)

--- backend/src/test/java/com/pi/stepup/domain/rank/dao/PointHistoryRepositoryTest.java
+++ backend/src/test/java/com/pi/stepup/domain/rank/dao/PointHistoryRepositoryTest.java
@@ -3,13 +3,17 @@ package com.pi.stepup.domain.rank.dao;
 import com.pi.stepup.domain.rank.domain.PointHistory;
 import com.pi.stepup.domain.rank.domain.PointPolicy;
 import com.pi.stepup.domain.user.domain.User;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.pi.stepup.domain.rank.constant.PointType.FIRST_PRIZE;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,6 +31,7 @@ class PointHistoryRepositoryTest {
     private PointHistory pointHistory;
     private User user;
     private PointPolicy pointPolicy;
+    private List<PointHistory> pointHistories;
 
     @Test
     @BeforeEach
@@ -37,28 +42,49 @@ class PointHistoryRepositoryTest {
     }
 
     @Test
-    public void insertPointRepoTest() {
-        User result = pointHistoryRepository.update(pointPolicy.getPoint());
+    @DisplayName("포인트 적립 내역 조회")
+    public void findAllPointHistoryRepositoryTest() {
+        List<PointHistory> made = makePointHistories();
+        insertPointHistories(made);
 
-        assertThat(result.getPoint()).isEqualTo(100);
+        List<PointHistory> result = pointHistoryRepository.findAll();
+
+        assertThat(result.size()).isEqualTo(made.size());
+    }
+
+    private void insertPointHistories(List<PointHistory> made) {
+        made.forEach(em::persist);
+    }
+
+    private List<PointHistory> makePointHistories() {
+        pointHistories = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            pointHistories.add(pointHistory);
+        }
+        return pointHistories;
     }
 
     private void makePointPolicy() {
         pointPolicy = PointPolicy.builder()
-            .pointType(FIRST_PRIZE)
-            .point(100)
-            .build();
+                .pointType(FIRST_PRIZE)
+                .point(100)
+                .build();
     }
 
     private void makeUser() {
         user = User.builder()
-            .id("user")
-            .password("pass")
-            .build();
+                .id("user")
+                .password("pass")
+                .point(0)
+                .build();
     }
 
     private void makePointHistory() {
-        pointHistory = PointHistory.builder().build();
+        pointHistory = PointHistory.builder()
+                .pointPolicy(pointPolicy)
+                .user(user)
+                .count(0)
+                .build();
     }
 
 }
