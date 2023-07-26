3c1fc87e01f117f3556a9d78af06ec5598eeb154
Seulgi <urina842@gmail.com>
2023-07-26 17:29:45 +0900

    :heavy_plus_sign: Feat(dance/reserve): DanceApiController 예약 및 예약 취소 메서드 구현 (#317)

--- backend/src/main/java/com/pi/stepup/domain/dance/api/DanceApiController.java
+++ backend/src/main/java/com/pi/stepup/domain/dance/api/DanceApiController.java
@@ -1,20 +1,40 @@
 package com.pi.stepup.domain.dance.api;
 
-import com.pi.stepup.domain.dance.constant.DanceResponseMessage;
-import com.pi.stepup.domain.dance.domain.RandomDance;
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.CREATE_RANDOM_DANCE;
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.DELETE_RESERVE_RANDOM_DANCE;
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.UPDATE_OPEN_RANDOM_DANCE;
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.DELETE_OPEN_RANDOM_DANCE;
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.SELECT_ALL_DANCE_MUSIC;
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.SELECT_ALL_OPEN_RANDOM_DANCE;
+//import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.SELECT_ALL_DANCE_MUSIC;
+//import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.SELECT_ALL_RANDOM_DANCE;
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.RESERVE_RANDOM_DANCE;
+//import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.DELETE_RESERVE_RANDOM_DANCE;
+//import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.SELECT_ALL_RESERVE_RANDOM_DANCE;
+//import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.ATTEND_RANDOM_DANCE;
+//import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.DELETE_ATTEND_RANDOM_DANCE;
+//import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.SELECT_ALL_ATTEND_RANDOM_DANCE;
+
 import com.pi.stepup.domain.dance.dto.DanceRequestDto.DanceCreateRequestDto;
+import com.pi.stepup.domain.dance.dto.DanceRequestDto.DanceReserveRequestDto;
 import com.pi.stepup.domain.dance.dto.DanceRequestDto.DanceUpdateRequestDto;
 import com.pi.stepup.domain.dance.dto.DanceResponseDto.DanceFindResponseDto;
 import com.pi.stepup.domain.dance.service.DanceService;
-import com.pi.stepup.domain.music.domain.Music;
+import com.pi.stepup.domain.music.dto.MusicResponseDto.MusicFindResponseDto;
 import com.pi.stepup.global.dto.ResponseDto;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/dance")
@@ -25,61 +45,73 @@ public class DanceApiController {
 
     @PostMapping("")
     public ResponseEntity<ResponseDto<?>> createDance
-            (@RequestBody DanceCreateRequestDto danceCreateRequestDto) {
-        RandomDance createDance = danceService.createDance(danceCreateRequestDto);
+        (@RequestBody DanceCreateRequestDto danceCreateRequestDto) {
+        danceService.create(danceCreateRequestDto);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(ResponseDto.create(
-                DanceResponseMessage.CREATE_RANDOM_DANCE.getMessage(),
-                danceService.readDance(createDance.getRandomDanceId())
+            CREATE_RANDOM_DANCE.getMessage()
         ));
     }
 
     @PutMapping("/my")
     public ResponseEntity<ResponseDto<?>> updateDance
-            (@RequestBody DanceUpdateRequestDto danceUpdateRequestDto) {
-        RandomDance updateDance = danceService.updateDance(danceUpdateRequestDto);
+        (@RequestBody DanceUpdateRequestDto danceUpdateRequestDto) {
+        danceService.update(danceUpdateRequestDto);
 
         return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(
-                DanceResponseMessage.UPDATE_CREATED_RANDOM_DANCE.getMessage(),
-                danceService.readDance(updateDance.getRandomDanceId())
+            UPDATE_OPEN_RANDOM_DANCE.getMessage()
         ));
     }
 
     @DeleteMapping("/my/{randomDanceId}")
     public ResponseEntity<ResponseDto<?>> deleteDance
-            (@PathVariable("randomDanceId") Long randomDanceId) {
-        danceService.deleteDance(randomDanceId);
+        (@PathVariable("randomDanceId") Long randomDanceId) {
+        danceService.delete(randomDanceId);
 
         return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(
-                DanceResponseMessage.DELETE_CREATED_RANDOM_DANCE.getMessage()
+            DELETE_OPEN_RANDOM_DANCE.getMessage()
         ));
     }
 
     @GetMapping("/playlist/{randomDanceId}")
     public ResponseEntity<ResponseDto<?>> readAllDanceMusic
-            (@PathVariable("randomDanceId") Long randomDanceId) {
-        List<Music> allMusic = danceService.readAllDanceMusic(randomDanceId);
+        (@PathVariable("randomDanceId") Long randomDanceId) {
+        List<MusicFindResponseDto> allDanceMusic = danceService.readAllDanceMusic(randomDanceId);
 
         return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(
-                DanceResponseMessage.SELECT_ALL_MUSIC.getMessage(),
-                allMusic
+            SELECT_ALL_DANCE_MUSIC.getMessage(),
+            allDanceMusic
         ));
     }
 
-    @GetMapping("/my/opened/{id}")
-    public ResponseEntity<ResponseDto<?>> readAllMyHeldDance
-            (@PathVariable("id") String id) {
-        List<RandomDance> allHeldDance = danceService.readAllMyHeldDance(id);
-
-        List<DanceFindResponseDto> danceFindResponseDtoList = new ArrayList<>();
-        for (int i = 0; i < allHeldDance.size(); i++) {
-            DanceFindResponseDto danceFindResponseDto
-                    = DanceFindResponseDto.builder().randomDance(allHeldDance.get(i)).build();
-            danceFindResponseDtoList.add(danceFindResponseDto);
-        }
+    // TODO: 쿼리스트링으로! ?id={id}
+    @GetMapping("/my/open/{id}")
+    public ResponseEntity<ResponseDto<?>> readAllMyOpenDance
+    (@PathVariable("id") String id) {
+        List<DanceFindResponseDto> allMyOpenDance = danceService.readAllMyOpenDance(id);
         return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(
-                DanceResponseMessage.SELECT_HELD_RANDOM_DANCE.getMessage(),
-                danceFindResponseDtoList
+            SELECT_ALL_OPEN_RANDOM_DANCE.getMessage(),
+            allMyOpenDance
+        ));
+    }
+
+    @PostMapping("/reserve")
+    public ResponseEntity<ResponseDto<?>> reserveDance
+        (@RequestBody DanceReserveRequestDto danceReserveRequestDto) {
+        danceService.createReservation(danceReserveRequestDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseDto.create(
+            RESERVE_RANDOM_DANCE.getMessage()
+        ));
+    }
+
+    @DeleteMapping("/reserve/my")
+    public ResponseEntity<ResponseDto<?>> unreserveDance
+        (@RequestParam("randomDanceId") Long randomDanceId, @RequestParam("id") String id) {
+        danceService.deleteReservation(randomDanceId, id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(
+            DELETE_RESERVE_RANDOM_DANCE.getMessage()
         ));
     }
 
