294a45b9299a4efb6488a6a2aab4ea1143d2fd30
lkk <rabbit1999k@naver.com>
2023-07-27 10:43:12 +0900

    :package:Rewrite(rank/userRank) : userRank 삭제

--- backend/src/test/java/com/pi/stepup/domain/rank/dao/RankRepositoryTest.java
+++ backend/src/test/java/com/pi/stepup/domain/rank/dao/RankRepositoryTest.java
@@ -1,30 +1,26 @@
 package com.pi.stepup.domain.rank.dao;
 
+import static com.pi.stepup.domain.rank.constant.PointType.FIRST_PRIZE;
+
 import com.pi.stepup.domain.dance.constant.DanceType;
 import com.pi.stepup.domain.dance.domain.RandomDance;
 import com.pi.stepup.domain.rank.constant.RankName;
 import com.pi.stepup.domain.rank.domain.PointHistory;
 import com.pi.stepup.domain.rank.domain.PointPolicy;
 import com.pi.stepup.domain.rank.domain.Rank;
-import com.pi.stepup.domain.rank.domain.UserRank;
 import com.pi.stepup.domain.user.domain.User;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.Optional;
-
-import static com.pi.stepup.domain.rank.constant.PointType.FIRST_PRIZE;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 @SpringBootTest
 @Transactional
 class RankRepositoryTest {
+
     @PersistenceContext
     EntityManager em;
 
@@ -32,7 +28,6 @@ class RankRepositoryTest {
     RankRepository rankRepository;
 
     private User user;
-    private UserRank userRank;
     private PointPolicy pointPolicy;
     private PointHistory pointHistory;
     private RandomDance randomDance;
@@ -44,7 +39,6 @@ class RankRepositoryTest {
         makePointPolicy();
         makeUser();
         makeRandomDance();
-        makeUserRank();
         makeRank();
     }
 
@@ -54,63 +48,41 @@ class RankRepositoryTest {
      *  2. 포인트 범위에 따라 랭크 조정 <- 이 부분을 포인트 업데이트 할 때마다 바꿔줘야 할 듯
      *  3. 랭크 조회
      */
-    @Test
-    @DisplayName("등급 조회 테스트")
-    public void findRankRepositoryTest() {
-        Optional<UserRank> result = rankRepository.findByUserId(user.getId());
-
-        UserRank resultUserRank = null;
-        if (result.isPresent()) {
-            resultUserRank = result.get();
-        }
-        System.out.println(user);
-        assertThat(resultUserRank).isEqualTo(user.getUserRank());
-    }
 
 
     private void makeRank() {
         rank = Rank.builder()
-                .name(RankName.BRONZE)
-                .startPoint(0)
-                .endPoint(99)
-                .build();
+            .name(RankName.BRONZE)
+            .startPoint(0)
+            .endPoint(99)
+            .build();
         em.persist(rank);
-    }
-
-    private void makeUserRank() {
-        userRank = UserRank.builder()
-                .rank(rank)
-                .user(user)
-                .build();
-        userRank.setUserAndSetThis(user);
-        em.persist(userRank);
     }
 
     private void makePointPolicy() {
         pointPolicy = PointPolicy.builder()
-                .pointType(FIRST_PRIZE)
-                .point(FIRST_PRIZE.getPoint())
-                .build();
+            .pointType(FIRST_PRIZE)
+            .point(FIRST_PRIZE.getPoint())
+            .build();
         em.persist(pointPolicy);
     }
 
     private void makeUser() {
         user = User.builder()
-                .id("user")
-                .password("pass")
-                .point(0)
-                .userRank(userRank)
-                .build();
+            .id("user")
+            .password("pass")
+            .point(0)
+            .build();
         em.persist(user);
     }
 
     private void makeRandomDance() {
         randomDance = RandomDance.builder()
-                .id(1L)
-                .title("title")
-                .content("content")
-                .danceType(DanceType.RANKING)
-                .build();
+            .id(1L)
+            .title("title")
+            .content("content")
+            .danceType(DanceType.RANKING)
+            .build();
 //        em.persist(randomDance);
     }
 }
