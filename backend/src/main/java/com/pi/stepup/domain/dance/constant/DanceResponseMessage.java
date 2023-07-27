dc2581e2b85dff0902913946749ceec81aecda1e
Seulgi <urina842@gmail.com>
2023-07-27 13:31:13 +0900

    :sparkles: Style(dance/messageName): 메시지명 변경 (#378)

--- backend/src/main/java/com/pi/stepup/domain/dance/constant/DanceResponseMessage.java
+++ backend/src/main/java/com/pi/stepup/domain/dance/constant/DanceResponseMessage.java
@@ -6,7 +6,7 @@ public enum DanceResponseMessage {
     UPDATE_OPEN_RANDOM_DANCE("랜덤 플레이 댄스 수정 완료"),
     DELETE_OPEN_RANDOM_DANCE("랜덤 플레이 댄스 삭제 완료"),
     SELECT_ALL_OPEN_RANDOM_DANCE("내가 개최한 랜덤 플레이 댄스 목록 조회 완료"),
-    SELECT_ALL_DANCE_MUSIC("사용 노래 목록 조회 완료"),
+    SELECT_ALL_DANCE_MUSIC("랜덤 플레이 댄스 노래 목록 조회 완료"),
     SELECT_ALL_RANDOM_DANCE("참여 가능한 랜덤 플레이 댄스 목록 조회 완료"),
     SELECT_IN_PROGRESS_RANDOM_DANCE("진행 중인 랜덤 플레이 댄스 목록 조회 완료"),
     SELECT_SCHEDULED_RANDOM_DANCE("진행 예정된 랜덤 플레이 댄스 목록 조회 완료"),
@@ -14,7 +14,6 @@ public enum DanceResponseMessage {
     DELETE_RESERVE_RANDOM_DANCE("랜덤 플레이 댄스 예약 취소 완료"),
     SELECT_ALL_RESERVE_RANDOM_DANCE("내가 예약한 랜덤 플레이 댄스 목록 조회 완료"),
     ATTEND_RANDOM_DANCE("랜덤 플레이 댄스 참여 완료"),
-    DELETE_ATTEND_RANDOM_DANCE("랜덤 플레이 댄스 참여 취소 완료"),
     SELECT_ALL_ATTEND_RANDOM_DANCE("내가 참여한 랜덤 플레이 댄스 목록 조회 완료");
 
     private final String message;
