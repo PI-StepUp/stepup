47b74ddf55c91ec61d76bbc4f6cd85f6b9279483
lkk <rabbit1999k@naver.com>
2023-07-28 11:24:51 +0900

    🔨 Refactor(rank/findRank): 필요 없는 코드 삭제
    - case when sql 제거

--- backend/src/main/java/com/pi/stepup/domain/rank/dao/RankRepositoryImpl.java
+++ backend/src/main/java/com/pi/stepup/domain/rank/dao/RankRepositoryImpl.java
@@ -18,19 +18,6 @@ public class RankRepositoryImpl implements RankRepository {
     @Override
     public Optional<Rank> findOneByPoint(Integer point) {
         Optional<Rank> rank;
-        // TODO : 포인트 범위 안에 있는거 꺼내오기
-
-        // 방법 1 : 사용자 포인트 기준으로 랭크 이름 바로 뽑기
-        String sql = "SELECT ID, POINT,\n" +
-            "CASE WHEN POINT BETWEEN 0 AND 99 THEN 'BRONZE'\n" +
-            "WHEN POINT BETWEEN 100 AND 299 THEN 'SILVER'\n" +
-            "WHEN POINT BETWEEN 300 AND 999 THEN 'GOLD'\n" +
-            "ELSE 'PLATINUM' \n" +
-            "END AS RANK_NAME\n" +
-            "FROM USERS";
-
-        // 하고싶은 방법 : Rank 테이블 조회하면서 유저 포인트가 StartPoint ~ EndPoint 안에 들어가면 해당하는 Rank PK 뽑아오기
-
         try {
             rank = Optional.ofNullable(em.createQuery(
                     "SELECT r FROM Rank r "
