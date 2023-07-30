301131cd1aaf4e3a4a8e9287e27128483799d2df
이가경 <rabbit1999k@naver.com>
2023-07-30 16:09:34 +0900

    ✅ Test(music/readMusicApply): 로그인 안한 사용자가 노래 신청 조회 예외 api 테스트 코드 구현 (#544)

--- backend/src/test/java/com/pi/stepup/domain/music/api/MusicApplyApiControllerTest.java
+++ backend/src/test/java/com/pi/stepup/domain/music/api/MusicApplyApiControllerTest.java
@@ -1,12 +1,21 @@
 package com.pi.stepup.domain.music.api;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.google.gson.Gson;
 import com.pi.stepup.domain.music.domain.MusicApply;
 import com.pi.stepup.domain.music.dto.MusicRequestDto.MusicApplySaveRequestDto;
 import com.pi.stepup.domain.music.dto.MusicResponseDto.AllMusicApplyFindResponseDto;
+import com.pi.stepup.domain.music.dto.MusicResponseDto.MusicApplyFindResponseDto;
 import com.pi.stepup.domain.music.service.MusicApplyService;
 import com.pi.stepup.domain.user.domain.User;
 import com.pi.stepup.global.util.jwt.JwtTokenProvider;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,20 +23,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@WebMvcTest(MusicApplyApiControllerTest.class)
+@WebMvcTest(controllers = MusicApplyApiControllerTest.class)
 class MusicApplyApiControllerTest {
 
     @Autowired
@@ -39,8 +41,10 @@ class MusicApplyApiControllerTest {
     @MockBean
     private JwtTokenProvider jwtTokenProvider;
 
+
     private Gson gson;
     private MusicApplySaveRequestDto musicApplySaveRequestDto;
+    private MusicApplyFindResponseDto musicApplyFindResponseDto;
     private MusicApply musicApply;
     private User user;
 
@@ -49,9 +53,10 @@ class MusicApplyApiControllerTest {
     public void init() {
         gson = new Gson();
         makeMusicApplySaveRequestDto();
+        makeUser();
         makeMusicApply();
         makeMusicApplies();
-        makeUser();
+        makeMusicApplyFindResponseDto();
     }
 
     // TODO : 왜 403 forbidden..?
@@ -62,9 +67,9 @@ class MusicApplyApiControllerTest {
         String url = "/api/music/apply";
 
         final ResultActions postAction = mockMvc.perform(
-                MockMvcRequestBuilders.post(url)
-                        .content(gson.toJson(musicApplySaveRequestDto))
-                        .contentType(MediaType.APPLICATION_JSON)
+            MockMvcRequestBuilders.post(url)
+                .content(gson.toJson(musicApplySaveRequestDto))
+                .contentType(MediaType.APPLICATION_JSON)
         );
 
         postAction.andExpect(status().isCreated());
@@ -75,9 +80,9 @@ class MusicApplyApiControllerTest {
     public void createMusicApplyNotLoginUserControllerTest() throws Exception {
         String url = "/api/music/apply";
         final ResultActions postAction = mockMvc.perform(
-                MockMvcRequestBuilders.post(url).with(csrf())
-                        .content(gson.toJson(musicApplySaveRequestDto))
-                        .contentType(MediaType.APPLICATION_JSON)
+            MockMvcRequestBuilders.post(url).with(csrf())
+                .content(gson.toJson(musicApplySaveRequestDto))
+                .contentType(MediaType.APPLICATION_JSON)
         );
 
         postAction.andExpect(status().isUnauthorized());
@@ -85,27 +90,71 @@ class MusicApplyApiControllerTest {
 
     @Test
     @DisplayName("노래 신청 목록 조회 테스트")
+    @WithMockUser
+    // TODO : 404 해결 - user 때문인듯
     public void MusicApplyReadAllControllerTest() throws Exception {
         String keyword = "";
         when(musicApplyService.readAllByKeyword(keyword)).thenReturn(makeMusicApplies());
 
         String url = "/api/music/apply?keyword=" + keyword;
         final ResultActions getAction = mockMvc.perform(
-                MockMvcRequestBuilders.get(url)
+            MockMvcRequestBuilders.get(url)
         );
 
         getAction.andExpect(status().isOk())
-                .andDo(print());
+            .andDo(print());
+    }
+
+    @Test
+    @DisplayName("노래 신청 상세 조회 테스트")
+    // TODO : 404 해결 해야 됨 - user 때문인듯
+    public void readOneMusicControllerTest() throws Exception {
+        when(musicApplyService.readOne(any(), any())).thenReturn(musicApplyFindResponseDto);
+
+        String url = "/api/music/apply/detail?id=" + user.getId()
+            + "&musicApplyId=" + musicApply.getMusicApplyId();
+        final ResultActions getAction = mockMvc.perform(
+            MockMvcRequestBuilders.get(url)
+        );
+
+        getAction.andExpect(status().isOk()).andDo(print());
+    }
+
+    @Test
+    @DisplayName("없는 노래 신청 상세 조회 예외 테스트")
+    @WithMockUser
+    // TODO : 404
+    public void readOneNotExistMusicApplyControllerTest() throws Exception {
+        String url = "/api/music/apply/detail?id=" + user.getId()
+            + "&musicApplyId=" + musicApply.getMusicApplyId();
+        final ResultActions getAction = mockMvc.perform(
+            MockMvcRequestBuilders.get(url)
+        );
+
+        getAction.andExpect(status().isNoContent());
+    }
+
+    @Test
+    @DisplayName("로그인 안한 사용자가 노래 신청 상세 조회 예외 테스트")
+    @WithAnonymousUser
+    public void readOneMusicApplyNotLoginUserControllerTest() throws Exception {
+        String url = "/api/music/apply/detail?id=" + user.getId()
+            + "&musicApplyId=" + musicApply.getMusicApplyId();
+        final ResultActions getAction = mockMvc.perform(
+            MockMvcRequestBuilders.get(url)
+        );
+
+        getAction.andExpect(status().isUnauthorized());
     }
 
     private List<AllMusicApplyFindResponseDto> makeMusicApplies() {
         List<AllMusicApplyFindResponseDto> musicApplies = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             MusicApply tmp = MusicApply.builder()
-                    .title("title" + i)
-                    .artist("artist" + (i + 1))
-                    .writer(user)
-                    .build();
+                .title("title" + i)
+                .artist("artist" + (i + 1))
+                .writer(user)
+                .build();
             musicApplies.add(new AllMusicApplyFindResponseDto(tmp));
         }
         return musicApplies;
@@ -113,25 +162,34 @@ class MusicApplyApiControllerTest {
 
     private void makeMusicApply() {
         musicApply = MusicApply.builder()
-                .title(musicApplySaveRequestDto.getTitle())
-                .artist(musicApplySaveRequestDto.getArtist())
-                .content(musicApplySaveRequestDto.getContent())
-                .build();
+            .musicApplyId(1L)
+            .title(musicApplySaveRequestDto.getTitle())
+            .artist(musicApplySaveRequestDto.getArtist())
+            .content(musicApplySaveRequestDto.getContent())
+            .writer(user)
+            .build();
     }
 
     private void makeUser() {
         user = User.builder()
-                .id("user")
-                .password("password")
-                .build();
+            .id("j3beom")
+            .password("password")
+            .build();
     }
 
     private void makeMusicApplySaveRequestDto() {
         musicApplySaveRequestDto = MusicApplySaveRequestDto.builder()
-                .artist("artist")
-                .title("title")
-                .content("content")
-                .writerId("user")
-                .build();
+            .artist("artist")
+            .title("title")
+            .content("content")
+            .writerId("user")
+            .build();
+    }
+
+    private void makeMusicApplyFindResponseDto() {
+        musicApplyFindResponseDto = MusicApplyFindResponseDto.builder()
+            .musicApply(musicApply)
+            .canHeart(1)
+            .build();
     }
 }
