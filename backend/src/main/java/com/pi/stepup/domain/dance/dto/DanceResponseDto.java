e7a7ccf88ce5bdc84bf22fc02ef104088557dd0d
seul <l.23.53.l2l9@gmail.com>
2023-07-30 19:40:52 +0900

    :heavy_plus_sign: Feat(dance/dto): DanceResponseDto DanceSearchResponseDto 추가

--- backend/src/main/java/com/pi/stepup/domain/dance/dto/DanceResponseDto.java
+++ backend/src/main/java/com/pi/stepup/domain/dance/dto/DanceResponseDto.java
@@ -1,5 +1,6 @@
 package com.pi.stepup.domain.dance.dto;
 
+import com.pi.stepup.domain.dance.constant.ProgressType;
 import com.pi.stepup.domain.dance.domain.RandomDance;
 import java.time.LocalDateTime;
 import lombok.Builder;
@@ -33,4 +34,38 @@ public class DanceResponseDto {
             this.hostNickname = randomDance.getHost().getNickname();
         }
     }
+
+    @Getter
+    public static class DanceSearchResponseDto {
+
+        private final Long randomDanceId;
+        private final String title;
+        private final String content;
+        private final LocalDateTime startAt;
+        private final LocalDateTime endAt;
+        private final String danceType;
+        private final int maxUser;
+        private final String thumbnail;
+        private final String hostNickname;
+        private String progressType;
+
+        @Builder
+        private DanceSearchResponseDto(RandomDance randomDance) {
+            this.randomDanceId = randomDance.getRandomDanceId();
+            this.title = randomDance.getTitle();
+            this.content = randomDance.getContent();
+            this.startAt = randomDance.getStartAt();
+            this.endAt = randomDance.getEndAt();
+            this.danceType = String.valueOf(randomDance.getDanceType());
+            this.maxUser = randomDance.getMaxUser();
+            this.thumbnail = randomDance.getThumbnail();
+            this.hostNickname = randomDance.getHost().getNickname();
+            this.progressType = "";
+        }
+
+        public void setProgressType(String progressType) {
+            this.progressType = progressType;
+        }
+    }
+
 }
