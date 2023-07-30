47a5a0874f437768c49e85b7cd2a17a97b44c3b8
seul <l.23.53.l2l9@gmail.com>
2023-07-30 19:40:08 +0900

    :hammer: Refactor(dance/delete): 사용하지 않는 코드 삭제

--- backend/src/main/java/com/pi/stepup/domain/dance/dto/DanceRequestDto.java
+++ backend/src/main/java/com/pi/stepup/domain/dance/dto/DanceRequestDto.java
@@ -3,10 +3,12 @@ package com.pi.stepup.domain.dance.dto;
 import com.pi.stepup.domain.dance.constant.DanceType;
 import com.pi.stepup.domain.dance.domain.RandomDance;
 import com.pi.stepup.domain.user.domain.User;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,7 +18,7 @@ import lombok.NoArgsConstructor;
 public class DanceRequestDto {
 
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
-        "yyyy-MM-dd HH:mm");
+            "yyyy-MM-dd HH:mm");
 
     @Data
     @Builder
@@ -35,15 +37,15 @@ public class DanceRequestDto {
 
         public RandomDance toEntity(User host) {
             return RandomDance.builder()
-                .title(this.title)
-                .content(this.content)
-                .startAt(LocalDateTime.parse(this.startAt, formatter))
-                .endAt(LocalDateTime.parse(this.endAt, formatter))
-                .danceType(this.danceType)
-                .maxUser(this.maxUser)
-                .thumbnail(this.thumbnail)
-                .host(host)
-                .build();
+                    .title(this.title)
+                    .content(this.content)
+                    .startAt(LocalDateTime.parse(this.startAt, formatter))
+                    .endAt(LocalDateTime.parse(this.endAt, formatter))
+                    .danceType(this.danceType)
+                    .maxUser(this.maxUser)
+                    .thumbnail(this.thumbnail)
+                    .host(host)
+                    .build();
         }
     }
 
@@ -62,20 +64,17 @@ public class DanceRequestDto {
         private String thumbnail;
         private String hostId;
         private List<Long> danceMusicIdList = new ArrayList<>();
-
-        public RandomDance toEntity(User host) {
-            return RandomDance.builder()
-                .title(this.title)
-                .content(this.content)
-                .startAt(LocalDateTime.parse(this.startAt, formatter))
-                .endAt(LocalDateTime.parse(this.endAt, formatter))
-                .danceType(this.danceType)
-                .maxUser(this.maxUser)
-                .thumbnail(this.thumbnail)
-                .host(host)
-                .build();
-        }
+        private String id;
     }
+
+//    @Data
+//    @Builder
+//    @NoArgsConstructor
+//    @AllArgsConstructor
+//    public static class DanceDeleteRequestDto {
+//        private String id;
+//        private Long randomDanceId;
+//    }
 
     @Data
     @Builder
