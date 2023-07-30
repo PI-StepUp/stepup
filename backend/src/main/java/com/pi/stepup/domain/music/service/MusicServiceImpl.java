ada44a987b80642f93b75cbfa27ee0f73df4b91e
이가경 <rabbit1999k@naver.com>
2023-07-30 16:31:00 +0900

    ✨Style(music): 코드 포매팅

--- backend/src/main/java/com/pi/stepup/domain/music/service/MusicServiceImpl.java
+++ backend/src/main/java/com/pi/stepup/domain/music/service/MusicServiceImpl.java
@@ -1,21 +1,20 @@
 package com.pi.stepup.domain.music.service;
 
+import static com.pi.stepup.domain.music.constant.MusicExceptionMessage.MUSIC_DUPLICATED;
+import static com.pi.stepup.domain.music.constant.MusicExceptionMessage.MUSIC_NOT_FOUND;
+
 import com.pi.stepup.domain.music.dao.MusicRepository;
 import com.pi.stepup.domain.music.domain.Music;
 import com.pi.stepup.domain.music.dto.MusicRequestDto.MusicSaveRequestDto;
 import com.pi.stepup.domain.music.dto.MusicResponseDto.MusicFindResponseDto;
 import com.pi.stepup.domain.music.exception.MusicDuplicatedException;
 import com.pi.stepup.domain.music.exception.MusicNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.pi.stepup.domain.music.constant.MusicExceptionMessage.MUSIC_DUPLICATED;
-import static com.pi.stepup.domain.music.constant.MusicExceptionMessage.MUSIC_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -40,22 +39,24 @@ public class MusicServiceImpl implements MusicService {
     @Override
     public MusicFindResponseDto readOne(Long musicId) {
         return MusicFindResponseDto.builder()
-                .music(musicRepository.findOne(musicId).orElseThrow(() -> new MusicNotFoundException(MUSIC_NOT_FOUND.getMessage())))
-                .build();
+            .music(musicRepository.findOne(musicId)
+                .orElseThrow(() -> new MusicNotFoundException(MUSIC_NOT_FOUND.getMessage())))
+            .build();
     }
 
     public List<MusicFindResponseDto> readAll(String keyword) {
         return musicRepository.findAll(keyword).stream()
-                .map(music -> MusicFindResponseDto.builder()
-                        .music(music)
-                        .build())
-                .collect(Collectors.toList());
+            .map(music -> MusicFindResponseDto.builder()
+                .music(music)
+                .build())
+            .collect(Collectors.toList());
     }
 
     @Override
     @Transactional
     public void delete(Long musicId) {
-        musicRepository.findOne(musicId).orElseThrow(() -> new MusicNotFoundException(MUSIC_NOT_FOUND.getMessage()));
+        musicRepository.findOne(musicId)
+            .orElseThrow(() -> new MusicNotFoundException(MUSIC_NOT_FOUND.getMessage()));
         musicRepository.delete(musicId);
     }
 }
