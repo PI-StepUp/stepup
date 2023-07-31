27032253607fb0f6d7e705c849c8b6ae4f109c1f
이주희 <joo9837@naver.com>
2023-07-31 18:07:00 +0900

    ➕ Feat(board/exception): Comment NotNull 예외 처리 (#703)

--- backend/src/main/java/com/pi/stepup/domain/board/dto/comment/CommentRequestDto.java
+++ backend/src/main/java/com/pi/stepup/domain/board/dto/comment/CommentRequestDto.java
@@ -5,14 +5,20 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class CommentRequestDto {
     @Data
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
     public static class CommentSaveRequestDto{
+        @NotNull
         private Long boardId;
+        @NotBlank
         private String id;
+        @NotBlank
         private String content;
     }
 }
