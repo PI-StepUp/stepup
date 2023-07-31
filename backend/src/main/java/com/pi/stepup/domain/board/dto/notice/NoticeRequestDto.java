a71eb4a35fc2a3215c0fe9416f31f7254c613736
이주희 <joo9837@naver.com>
2023-07-31 18:02:13 +0900

    ➕ Feat(board/exception): Notice NotNull 예외 처리 (#701)

--- backend/src/main/java/com/pi/stepup/domain/board/dto/notice/NoticeRequestDto.java
+++ backend/src/main/java/com/pi/stepup/domain/board/dto/notice/NoticeRequestDto.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class NoticeRequestDto {
 
     @Data
@@ -12,8 +15,11 @@ public class NoticeRequestDto {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class NoticeSaveRequestDto {
+        @NotBlank
         private String id;
+        @NotBlank
         private String title;
+        @NotBlank
         private String content;
         private String fileURL;
         private Long randomDanceId;
@@ -24,12 +30,17 @@ public class NoticeRequestDto {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class NoticeUpdateRequestDto {
+        @NotNull
         private Long boardId;
+        @NotBlank
         private String title;
+        @NotBlank
         private String content;
+        @NotBlank
         private String writerName;
         private String writerProfileImg;
         private String fileURL;
+        @NotBlank
         private String boardType;
         private Long randomDanceId;
     }
