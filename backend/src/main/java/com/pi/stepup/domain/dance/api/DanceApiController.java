895d3b162f0d0f9634bdc1f4987ea985c5eb2953
Seulgi <urina842@gmail.com>
2023-07-31 17:41:00 +0900

    :speech_balloon: Comment(dance/comment): 코드리뷰 TODO 추가 (#579)

--- backend/src/main/java/com/pi/stepup/domain/dance/api/DanceApiController.java
+++ backend/src/main/java/com/pi/stepup/domain/dance/api/DanceApiController.java
@@ -1,7 +1,25 @@
 package com.pi.stepup.domain.dance.api;
 
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.ATTEND_RANDOM_DANCE;
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.CREATE_RANDOM_DANCE;
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.DELETE_OPEN_RANDOM_DANCE;
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.DELETE_RESERVE_RANDOM_DANCE;
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.RESERVE_RANDOM_DANCE;
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.SELECT_ALL_ATTEND_RANDOM_DANCE;
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.SELECT_ALL_DANCE_MUSIC;
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.SELECT_ALL_OPEN_RANDOM_DANCE;
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.SELECT_ALL_RANDOM_DANCE;
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.SELECT_ALL_RESERVE_RANDOM_DANCE;
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.SELECT_IN_PROGRESS_RANDOM_DANCE;
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.SELECT_SCHEDULED_RANDOM_DANCE;
+import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.UPDATE_OPEN_RANDOM_DANCE;
+
 import com.pi.stepup.domain.dance.constant.ProgressType;
-import com.pi.stepup.domain.dance.dto.DanceRequestDto.*;
+import com.pi.stepup.domain.dance.dto.DanceRequestDto.DanceAttendRequestDto;
+import com.pi.stepup.domain.dance.dto.DanceRequestDto.DanceCreateRequestDto;
+import com.pi.stepup.domain.dance.dto.DanceRequestDto.DanceReserveRequestDto;
+import com.pi.stepup.domain.dance.dto.DanceRequestDto.DanceSearchRequestDto;
+import com.pi.stepup.domain.dance.dto.DanceRequestDto.DanceUpdateRequestDto;
 import com.pi.stepup.domain.dance.dto.DanceResponseDto.DanceFindResponseDto;
 import com.pi.stepup.domain.dance.dto.DanceResponseDto.DanceSearchResponseDto;
 import com.pi.stepup.domain.dance.service.DanceService;
@@ -10,14 +28,20 @@ import com.pi.stepup.global.dto.ResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-import static com.pi.stepup.domain.dance.constant.DanceResponseMessage.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "dance", description = "dance domain apis")
 @RestController
@@ -27,33 +51,35 @@ public class DanceApiController {
 
     private final DanceService danceService;
 
+    //TODO: id 쿼리스트링 빼기
+
     @Operation(summary = "랜덤 플레이 댄스 개최", description = "회원들이 참여할 랜덤 플레이 댄스를 개최한다. 유형은 서바이벌, 랭킹, 자율 중에서 고른다.")
     @ApiResponse(responseCode = "201", description = "랜덤 플레이 댄스 생성 완료")
     @PostMapping("")
     public ResponseEntity<ResponseDto<?>> createDance
-            (@RequestBody DanceCreateRequestDto danceCreateRequestDto) {
+        (@RequestBody @Valid DanceCreateRequestDto danceCreateRequestDto) {
         danceService.create(danceCreateRequestDto);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(ResponseDto.create(
-                CREATE_RANDOM_DANCE.getMessage()
+            CREATE_RANDOM_DANCE.getMessage()
         ));
     }
 
     @Operation(summary = "개최 랜덤 플레이 댄스 수정",
-            description = "내가 개최한 랜덤 플레이 댄스 제목, 내용, 시작 및 끝 시간, 유형, 최대 참가 인원, 썸네일 이미지를 수정한다.")
+        description = "내가 개최한 랜덤 플레이 댄스 제목, 내용, 시작 및 끝 시간, 유형, 최대 참가 인원, 썸네일 이미지를 수정한다.")
     @ApiResponse(responseCode = "200", description = "랜덤 플레이 댄스 수정 완료")
     @PutMapping("/my")
     public ResponseEntity<ResponseDto<?>> updateDance
-            (@RequestBody DanceUpdateRequestDto danceUpdateRequestDto) {
+        (@RequestBody @Valid DanceUpdateRequestDto danceUpdateRequestDto) {
         danceService.update(danceUpdateRequestDto);
 
         return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(
-                UPDATE_OPEN_RANDOM_DANCE.getMessage()
+            UPDATE_OPEN_RANDOM_DANCE.getMessage()
         ));
     }
 
     @Operation(summary = "개최 랜덤 플레이 댄스 삭제",
-            description = "내가 개최한 랜덤 플레이 댄스를 삭제한다. 예약자가 있더라도 삭제할 수 있다. 종료된 랜덤 플레이 댄스는 삭제할 수 없다.")
+        description = "내가 개최한 랜덤 플레이 댄스를 삭제한다. 예약자가 있더라도 삭제할 수 있다. 종료된 랜덤 플레이 댄스는 삭제할 수 없다.")
     @ApiResponse(responseCode = "200", description = "랜덤 플레이 댄스 삭제 완료")
     @DeleteMapping("/my/{randomDanceId}")
     public ResponseEntity<ResponseDto<?>> deleteDance
@@ -62,135 +88,135 @@ public class DanceApiController {
         danceService.delete(randomDanceId);
 
         return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(
-                DELETE_OPEN_RANDOM_DANCE.getMessage()
+            DELETE_OPEN_RANDOM_DANCE.getMessage()
         ));
     }
 
     @Operation(summary = "개최 랜덤 플레이 댄스 사용 노래 목록 조회",
-            description = "내가 개최한 랜덤 플레이 댄스에 사용한 노래 목록을 조회한다.")
+        description = "내가 개최한 랜덤 플레이 댄스에 사용한 노래 목록을 조회한다.")
     @ApiResponse(responseCode = "200", description = "랜덤 플레이 댄스 노래 목록 조회 완료")
     @GetMapping("/playlist/{randomDanceId}")
     public ResponseEntity<ResponseDto<?>> readAllDanceMusic
-            (@PathVariable("randomDanceId") Long randomDanceId) {
+        (@PathVariable("randomDanceId") Long randomDanceId) {
         List<MusicFindResponseDto> allDanceMusic = danceService.readAllDanceMusic(randomDanceId);
 
         return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(
-                SELECT_ALL_DANCE_MUSIC.getMessage(),
-                allDanceMusic
+            SELECT_ALL_DANCE_MUSIC.getMessage(),
+            allDanceMusic
         ));
     }
 
     @Operation(summary = "개최 랜덤 플레이 댄스 목록 조회",
-            description = "내가 개최한 랜덤 플레이 댄스 목록을 조회한다.")
+        description = "내가 개최한 랜덤 플레이 댄스 목록을 조회한다.")
     @ApiResponse(responseCode = "200", description = "내가 개최한 랜덤 플레이 댄스 목록 조회 완료")
     @GetMapping("/my/open")
     public ResponseEntity<ResponseDto<?>> readAllMyOpenDance
-            (@RequestParam("id") String id) {
+        (@RequestParam("id") String id) {
         List<DanceFindResponseDto> allMyOpenDance = danceService.readAllMyOpenDance(id);
 
         return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(
-                SELECT_ALL_OPEN_RANDOM_DANCE.getMessage(),
-                allMyOpenDance
+            SELECT_ALL_OPEN_RANDOM_DANCE.getMessage(),
+            allMyOpenDance
         ));
     }
 
     @Operation(summary = "참여 가능한 랜덤 플레이 댄스 목록 조회",
-            description = "현재 시간과 비교해 참여 가능한 랜덤 플레이 댄스 목록을 조회한다. 인기순(예약자 및 참여자 많은 순), 시작 시간이 현재 시간과 가까운 순으로 정렬한다.")
+        description = "현재 시간과 비교해 참여 가능한 랜덤 플레이 댄스 목록을 조회한다. 인기순(예약자 및 참여자 많은 순), 시작 시간이 현재 시간과 가까운 순으로 정렬한다.")
     @ApiResponse(responseCode = "200", description = "참여 가능한 랜덤 플레이 댄스 목록 조회 완료")
     @GetMapping("")
     public ResponseEntity<ResponseDto<?>> readAllRandomDance
-            (DanceSearchRequestDto danceSearchRequestDto) {
-
-        //해당 되는 목록 조회
+        (DanceSearchRequestDto danceSearchRequestDto) {
         List<DanceSearchResponseDto> allDance
-                = danceService.readAllRandomDance(danceSearchRequestDto);
+            = danceService.readAllRandomDance(danceSearchRequestDto);
 
-        if (allDance.get(0).getProgressType().equals(ProgressType.SCHEDULED.toString())) {
+        //그냥 메시지 다른 거 전달하지 말까...
+        if (danceSearchRequestDto.getProgressType().equals(ProgressType.SCHEDULED.toString())) {
             return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(
-                    SELECT_SCHEDULED_RANDOM_DANCE.getMessage(),
-                    allDance
+                SELECT_SCHEDULED_RANDOM_DANCE.getMessage(),
+                allDance
             ));
-        } else if (allDance.get(0).getProgressType().equals(ProgressType.IN_PROGRESS.toString())) {
+        } else if (danceSearchRequestDto.getProgressType()
+            .equals(ProgressType.IN_PROGRESS.toString())) {
             return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(
-                    SELECT_IN_PROGRESS_RANDOM_DANCE.getMessage(),
-                    allDance
+                SELECT_IN_PROGRESS_RANDOM_DANCE.getMessage(),
+                allDance
             ));
         } else {
             return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(
-                    SELECT_ALL_RANDOM_DANCE.getMessage(),
-                    allDance
+                SELECT_ALL_RANDOM_DANCE.getMessage(),
+                allDance
             ));
         }
     }
 
     @Operation(summary = "랜덤 플레이 댄스 예약",
-            description = "참여하고 싶은 랜덤 플레이 댄스를 예약한다.")
+        description = "참여하고 싶은 랜덤 플레이 댄스를 예약한다.")
     @ApiResponse(responseCode = "201", description = "랜덤 플레이 댄스 예약 완료")
     @PostMapping("/reserve")
     public ResponseEntity<ResponseDto<?>> createReservation
-            (@RequestBody DanceReserveRequestDto danceReserveRequestDto) {
+        (@RequestBody DanceReserveRequestDto danceReserveRequestDto) {
         danceService.createReservation(danceReserveRequestDto);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(ResponseDto.create(
-                RESERVE_RANDOM_DANCE.getMessage()
+            RESERVE_RANDOM_DANCE.getMessage()
         ));
     }
 
     @Operation(summary = "랜덤 플레이 댄스 예약 취소",
-            description = "내가 예약한 랜덤 플레이 댄스의 예약을 취소한다.")
+        description = "내가 예약한 랜덤 플레이 댄스의 예약을 취소한다.")
     @ApiResponse(responseCode = "200", description = "랜덤 플레이 댄스 예약 취소 완료")
     @DeleteMapping("/my/reserve")
     public ResponseEntity<ResponseDto<?>> deleteReservation
-            (@RequestParam("randomDanceId") Long randomDanceId, @RequestParam("id") String id) {
+        (@RequestParam("randomDanceId") Long randomDanceId, @RequestParam("id") String id) {
         danceService.deleteReservation(randomDanceId, id);
 
         return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(
-                DELETE_RESERVE_RANDOM_DANCE.getMessage()
+            DELETE_RESERVE_RANDOM_DANCE.getMessage()
         ));
     }
 
     @Operation(summary = "예약 랜덤 플레이 댄스 목록 조회",
-            description = "내가 예약한 랜덤 플레이 댄스 목록을 조회한다.")
+        description = "내가 예약한 랜덤 플레이 댄스 목록을 조회한다.")
     @ApiResponse(responseCode = "200", description = "내가 예약한 랜덤 플레이 댄스 목록 조회 완료")
     @GetMapping("/my/reserve")
     public ResponseEntity<ResponseDto<?>> readAllMyReserveDance
-            (@RequestParam("id") String id) {
+        (@RequestParam("id") String id) {
 
         List<DanceFindResponseDto> allMyRandomDance
-                = danceService.readAllMyReserveDance(id);
+            = danceService.readAllMyReserveDance(id);
 
         return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(
-                SELECT_ALL_RESERVE_RANDOM_DANCE.getMessage(),
-                allMyRandomDance
+            SELECT_ALL_RESERVE_RANDOM_DANCE.getMessage(),
+            allMyRandomDance
         ));
     }
 
     @Operation(summary = "랜덤 플레이 댄스 참여",
-            description = "참여하고 싶은 랜덤 플레이 댄스에 참여한다.")
+        description = "참여하고 싶은 랜덤 플레이 댄스에 참여한다.")
     @ApiResponse(responseCode = "201", description = "랜덤 플레이 댄스 참여 완료")
     @PostMapping("/attend")
     public ResponseEntity<ResponseDto<?>> createAttend
-            (@RequestBody DanceAttendRequestDto danceAttendRequestDto) {
+        (@RequestBody DanceAttendRequestDto danceAttendRequestDto) {
         danceService.createAttend(danceAttendRequestDto);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(ResponseDto.create(
-                ATTEND_RANDOM_DANCE.getMessage()
+            ATTEND_RANDOM_DANCE.getMessage()
         ));
     }
 
     @Operation(summary = "참여 랜덤 플레이 댄스 목록 조회",
-            description = "내가 참여한 랜덤 플레이 댄스 목록을 조회한다.")
+        description = "내가 참여한 랜덤 플레이 댄스 목록을 조회한다.")
     @ApiResponse(responseCode = "200", description = "내가 참여한 랜덤 플레이 댄스 목록 조회 완료")
     @GetMapping("/my/attend")
     public ResponseEntity<ResponseDto<?>> readAllMyAttendDance
-            (@RequestParam("id") String id) {
+        (@RequestParam("id") String id) {
 
         List<DanceFindResponseDto> allMyRandomDance
-                = danceService.readAllMyAttendDance(id);
+            = danceService.readAllMyAttendDance(id);
 
         return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(
-                SELECT_ALL_ATTEND_RANDOM_DANCE.getMessage(),
-                allMyRandomDance
+            SELECT_ALL_ATTEND_RANDOM_DANCE.getMessage(),
+            allMyRandomDance
         ));
     }
 
