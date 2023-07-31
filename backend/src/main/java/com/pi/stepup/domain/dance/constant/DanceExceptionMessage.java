f14cea481c57d3e36b421ef66bad51806bcc8aba
Seulgi <urina842@gmail.com>
2023-07-31 17:52:31 +0900

    :hammer: Refactor(dance/message): exception message 추가 (#579)

--- backend/src/main/java/com/pi/stepup/domain/dance/constant/DanceExceptionMessage.java
+++ backend/src/main/java/com/pi/stepup/domain/dance/constant/DanceExceptionMessage.java
@@ -5,17 +5,13 @@ public enum DanceExceptionMessage {
 
     DANCE_NOT_FOUND("랜덤 플레이 댄스 조회 실패"),
     DANCE_UPDATE_FORBIDDEN("랜덤 플레이 댄스 수정 권한 없음"),
-    DANCE_DELETE_FORBIDDEN("랜덤 플레이 댄스 수정 권한 없음"),
-    DANCEMUSICLIST_IS_NEEDED("노래 목록 등록 요청"),
-    DANCEMUSICLIST_NOT_FOUND("노래 목록 조회 실패"),
-    RESERVATION_DUPLICATED("예약 중복"),
-    RESERVATION_IMPOSSIBLE("예약 불가(본인이 개최한 랜덤 플래이 댄스)"),
-    RESERVATION_NOT_FOUND("랜덤 플레이 댄스 예약 조회 실패"),
-    ATTEND_DUPLICATED("참여 중복"),
-    ATTEND_NOT_FOUND("랜덤 플레이 댄스 예약 조회 실패");
-
-    //예약 취소 관련 검증은...
-    //이미 로그인한 아이디 정보로 예약 정보 가져와서...
+    DANCE_DELETE_FORBIDDEN("랜덤 플레이 댄스 삭제 권한 없음"),
+    DANCE_INVALID_TIME("유효하지 않은 시간"),
+    DANCE_INVALID_MUSIC("유효하지 않은 노래 수"),
+    RESERVATION_DUPLICATED("랜덤 플레이 댄스 예약 중복"),
+    RESERVATION_IMPOSSIBLE("랜덤 플레이 댄스 예약 불가(본인이 개최한 랜덤 플래이 댄스)"),
+    RESERVATION_DELETE_FORBIDDEN("랜덤 플레이 댄스 예약 취소 권한 없음"),
+    ATTEND_DUPLICATED("랜덤 플레이 댄스 참여 중복(이미 DB에 삽입된 데이터)");
 
     private final String message;
 
