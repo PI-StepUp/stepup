4739fb22b649d1b0e9c319c6ee5f2d88b28fba31
lkk <rabbit1999k@naver.com>
2023-08-01 11:02:08 +0900

    🔨 Refactor(music): 노래 service 테스트 코드 user authentication mock 주입 (#246)
    - 좋아요 상태 체크 테스트 코드 작성

--- backend/src/test/java/com/pi/stepup/domain/music/service/MusicApplyServiceTest.java
+++ backend/src/test/java/com/pi/stepup/domain/music/service/MusicApplyServiceTest.java
@@ -1,5 +1,6 @@
 package com.pi.stepup.domain.music.service;
 
+import static com.pi.stepup.domain.music.constant.MusicExceptionMessage.MUSIC_APPLY_DELETE_FAIL;
 import static com.pi.stepup.domain.music.constant.MusicExceptionMessage.MUSIC_APPLY_NOT_FOUND;
 import static com.pi.stepup.domain.music.constant.MusicExceptionMessage.UNAUTHORIZED_USER_ACCESS;
 import static com.pi.stepup.domain.user.constant.UserExceptionMessage.USER_NOT_FOUND;
@@ -7,6 +8,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.only;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -22,6 +24,7 @@ import com.pi.stepup.domain.music.exception.UnauthorizedUserAccessException;
 import com.pi.stepup.domain.user.dao.UserRepository;
 import com.pi.stepup.domain.user.domain.User;
 import com.pi.stepup.domain.user.exception.UserNotFoundException;
+import com.pi.stepup.global.config.security.SecurityUtils;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -31,6 +34,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -64,175 +68,240 @@ class MusicApplyServiceTest {
     @Test
     @DisplayName("노래 신청 등록 테스트")
     public void createMusicApplyServiceTest() {
-        when(musicApplyRepository.insert(any(MusicApply.class))).thenReturn(musicApply);
-        when(userRepository.findById(any(String.class))).thenReturn(Optional.of(user));
+        try (MockedStatic<SecurityUtils> securityUtilsMockedStatic = mockStatic(
+            SecurityUtils.class)) {
+            securityUtilsMockedStatic.when(SecurityUtils::getLoggedInUserId)
+                .thenReturn(user.getId());
+            when(musicApplyRepository.insert(any(MusicApply.class))).thenReturn(musicApply);
+            when(userRepository.findById(any(String.class))).thenReturn(Optional.of(user));
 
-        musicApplyService.create(musicApplySaveRequestDto);
-        verify(musicApplyRepository, only()).insert(any(MusicApply.class));
+            musicApplyService.create(musicApplySaveRequestDto);
+            verify(musicApplyRepository, only()).insert(any(MusicApply.class));
+        }
     }
 
     @Test
     @DisplayName("노래 신청 등록 유저 정보 없는 예외 테스트")
     public void noUserCreateMusicApplyServiceTest() {
-        when(userRepository.findById(any(String.class))).thenReturn(Optional.empty());
+        try (MockedStatic<SecurityUtils> securityUtilsMockedStatic = mockStatic(
+            SecurityUtils.class)) {
+            securityUtilsMockedStatic.when(SecurityUtils::getLoggedInUserId)
+                .thenReturn(user.getId());
+            when(userRepository.findById(any(String.class))).thenReturn(Optional.empty());
 
-        assertThatThrownBy(
-            () -> musicApplyService.create(musicApplySaveRequestDto)
-        )
-            .isInstanceOf(UserNotFoundException.class)
-            .hasMessageContaining(USER_NOT_FOUND.getMessage());
+            assertThatThrownBy(
+                () -> musicApplyService.create(musicApplySaveRequestDto)
+            )
+                .isInstanceOf(UserNotFoundException.class)
+                .hasMessageContaining(USER_NOT_FOUND.getMessage());
+        }
     }
 
+    // TODO : 좋아요 연관관계 수정 후 다시 할 것
     @Test
     @DisplayName("노래 신청 목록 조회 테스트")
     public void readAllMusicApplyServiceTest() {
-        String keyword = "";
-        List<MusicApply> makedMusicApply = makeMusicApplies();
-        doReturn(makedMusicApply)
-            .when(musicApplyRepository).findAll(keyword, user.getId());
+        try (MockedStatic<SecurityUtils> securityUtilsMockedStatic = mockStatic(
+            SecurityUtils.class)) {
+            securityUtilsMockedStatic.when(SecurityUtils::getLoggedInUserId)
+                .thenReturn(user.getId());
+            String keyword = "";
+            List<MusicApply> makedMusicApply = makeMusicApplies();
+            doReturn(makedMusicApply)
+                .when(musicApplyRepository).findAll(keyword, user.getId());
 
-        List<MusicApplyFindResponseDto> musicApplies = musicApplyService.readAllByKeyword(
-            keyword);
-        assertThat(musicApplies.size()).isEqualTo(makedMusicApply.size());
+            List<MusicApplyFindResponseDto> musicApplies = musicApplyService.readAllByKeyword(
+                keyword);
+            assertThat(musicApplies.size()).isEqualTo(makedMusicApply.size());
+        }
     }
 
     @Test
     @DisplayName("노래 신청 목록 키워드 조회 테스트")
     public void readAllMusicApplyByKeywordServiceTest() {
-        String keyword = "1";
-        List<MusicApply> madeMusicApply = makeMusicApplies();
-        List<MusicApply> keywordMusicApply = makeMusicApplyByKeyword(madeMusicApply, keyword);
+        try (MockedStatic<SecurityUtils> securityUtilsMockedStatic = mockStatic(
+            SecurityUtils.class)) {
+            securityUtilsMockedStatic.when(SecurityUtils::getLoggedInUserId)
+                .thenReturn(user.getId());
+            String keyword = "1";
+            List<MusicApply> madeMusicApply = makeMusicApplies();
+            List<MusicApply> keywordMusicApply = makeMusicApplyByKeyword(madeMusicApply, keyword);
 
-        doReturn(keywordMusicApply)
-            .when(musicApplyRepository).findAll(keyword, user.getId());
+            doReturn(keywordMusicApply)
+                .when(musicApplyRepository).findAll(keyword, user.getId());
 
-        List<MusicApplyFindResponseDto> musicApplies = musicApplyService.readAllByKeyword(
-            keyword);
-        assertThat(musicApplies.size()).isEqualTo(keywordMusicApply.size());
+            List<MusicApplyFindResponseDto> musicApplies = musicApplyService.readAllByKeyword(
+                keyword);
+            assertThat(musicApplies.size()).isEqualTo(keywordMusicApply.size());
+        }
+
     }
 
     @Test
     @DisplayName("내 노래 신청 목록 조회 테스트")
     public void readAllMusicApplyByUserServiceTest() {
-        List<MusicApply> madeMusicApply = makeMusicApplies();
-        List<MusicApply> writerMusicApply = makeMusicApplyByUser(madeMusicApply, user);
+        try (MockedStatic<SecurityUtils> securityUtilsMockedStatic = mockStatic(
+            SecurityUtils.class)) {
+            securityUtilsMockedStatic.when(SecurityUtils::getLoggedInUserId)
+                .thenReturn(user.getId());
+            List<MusicApply> madeMusicApply = makeMusicApplies();
+            List<MusicApply> writerMusicApply = makeMusicApplyByUser(madeMusicApply, user);
 
-        doReturn(writerMusicApply)
-            .when(musicApplyRepository).findById(user.getId());
+            doReturn(writerMusicApply)
+                .when(musicApplyRepository).findById(user.getId());
 
-        List<MusicApplyFindResponseDto> musicApplies = musicApplyService.readAllById();
-        assertThat(musicApplies.size()).isEqualTo(writerMusicApply.size());
+            List<MusicApplyFindResponseDto> musicApplies = musicApplyService.readAllById();
+            assertThat(musicApplies.size()).isEqualTo(writerMusicApply.size());
+        }
     }
 
 
     @Test
     @DisplayName("노래 신청 상세 조회 테스트")
     public void readOneMusicApplyServiceTest() {
-        when(musicApplyRepository.findOne(any())).thenReturn(Optional.of(musicApply));
+        try (MockedStatic<SecurityUtils> securityUtilsMockedStatic = mockStatic(
+            SecurityUtils.class)) {
+            securityUtilsMockedStatic.when(SecurityUtils::getLoggedInUserId)
+                .thenReturn(user.getId());
+            when(musicApplyRepository.findOne(any())).thenReturn(Optional.of(musicApply));
 
-        MusicApplyFindResponseDto result = musicApplyService.readOne(user.getId(),
-            musicApply.getMusicApplyId());
+            MusicApplyFindResponseDto result = musicApplyService.readOne(user.getId(),
+                musicApply.getMusicApplyId());
 
-        assertThat(result.getTitle()).isEqualTo(musicApply.getTitle());
+            assertThat(result.getTitle()).isEqualTo(musicApply.getTitle());
+        }
     }
 
     @Test
     @DisplayName("없는 노래 신청 상세 조회 예외 테스트")
     public void readOneNotExistMusicApplyServiceTest() {
-        when(musicApplyRepository.findOne(any())).thenReturn(Optional.empty());
+        try (MockedStatic<SecurityUtils> securityUtilsMockedStatic = mockStatic(
+            SecurityUtils.class)) {
+            securityUtilsMockedStatic.when(SecurityUtils::getLoggedInUserId)
+                .thenReturn(user.getId());
+            when(musicApplyRepository.findOne(any())).thenReturn(Optional.empty());
 
-        assertThatThrownBy(
-            () -> musicApplyService.readOne(user.getId(), musicApply.getMusicApplyId()))
-            .isInstanceOf(MusicApplyNotFoundException.class)
-            .hasMessageContaining(MUSIC_APPLY_NOT_FOUND.getMessage());
+            assertThatThrownBy(
+                () -> musicApplyService.readOne(user.getId(), musicApply.getMusicApplyId()))
+                .isInstanceOf(MusicApplyNotFoundException.class)
+                .hasMessageContaining(MUSIC_APPLY_NOT_FOUND.getMessage());
+        }
     }
 
     @Test
     @DisplayName("노래 신청 삭제 테스트")
     public void deleteMusicApplyServiceTest() {
-        Long musicId = 1L;
+        try (MockedStatic<SecurityUtils> securityUtilsMockedStatic = mockStatic(
+            SecurityUtils.class)) {
+            securityUtilsMockedStatic.when(SecurityUtils::getLoggedInUserId)
+                .thenReturn(user.getId());
+            Long musicApplyId = 1L;
+            when(musicApplyRepository.findOne(any())).thenReturn(Optional.ofNullable(musicApply));
+            musicApplyService.delete(musicApplyId);
 
-        musicApplyService.delete(musicId);
-
-        verify(musicApplyRepository, only()).delete(musicId);
+            verify(musicApplyRepository, only()).delete(musicApplyId);
+        }
     }
 
     @Test
     @DisplayName("노래 신청한 사용자가 아닌 다른 사용자가 삭제 예외 테스트")
     public void deleteMusicApplyNotAuthorizedUserServiceTest() {
-        // TODO : claim 에서 사용자 정보 가져와서 체크
-        assertThatThrownBy(() -> musicApplyService.delete(musicApply.getMusicApplyId()))
-            .isInstanceOf(UnauthorizedUserAccessException.class)
-            .hasMessageContaining(UNAUTHORIZED_USER_ACCESS.getMessage());
+        try (MockedStatic<SecurityUtils> securityUtilsMockedStatic = mockStatic(
+            SecurityUtils.class)) {
+            securityUtilsMockedStatic.when(SecurityUtils::getLoggedInUserId)
+                .thenReturn(user.getId());
+
+            assertThatThrownBy(() -> musicApplyService.delete(musicApply.getMusicApplyId()))
+                .isInstanceOf(UnauthorizedUserAccessException.class)
+                .hasMessageContaining(UNAUTHORIZED_USER_ACCESS.getMessage());
+        }
     }
 
     @Test
     @DisplayName("없는 노래 신청 삭제 예외 테스트")
     public void deleteNotExistMusicApplyServiceTest() {
-        when(musicApplyRepository.findOne(any())).thenReturn(Optional.empty());
+        try (MockedStatic<SecurityUtils> securityUtilsMockedStatic = mockStatic(
+            SecurityUtils.class)) {
+            securityUtilsMockedStatic.when(SecurityUtils::getLoggedInUserId)
+                .thenReturn(user.getId());
+            when(musicApplyRepository.findOne(any())).thenReturn(Optional.empty());
 
-        assertThatThrownBy(() -> musicApplyService.delete(musicApply.getMusicApplyId()))
-            .isInstanceOf(MusicApplyNotFoundException.class)
-            .hasMessageContaining(MUSIC_APPLY_NOT_FOUND.getMessage());
-    }
-
-    private List<MusicApply> makeMusicApplyByKeyword(List<MusicApply> musicApplies,
-        String keyword) {
-        List<MusicApply> result = new ArrayList<>();
-
-        for (MusicApply m : musicApplies) {
-            if (m.getTitle().contains(keyword) || m.getArtist().contains(keyword)) {
-                result.add(m);
-            }
+            assertThatThrownBy(() -> musicApplyService.delete(musicApply.getMusicApplyId()))
+                .isInstanceOf(MusicApplyNotFoundException.class)
+                .hasMessageContaining(MUSIC_APPLY_DELETE_FAIL.getMessage());
         }
-        return result;
-    }
-
-    private List<MusicApply> makeMusicApplyByUser(List<MusicApply> musicApplies, User user) {
-        List<MusicApply> result = new ArrayList<>();
-
-        for (MusicApply m : musicApplies) {
-            if (m.getWriter().getId().equals(user.getId())) {
-                result.add(m);
-            }
-        }
-        return result;
     }
 
     @Test
     @DisplayName("노래 신청 좋아요 테스트")
     public void musicApplyHeartServiceTest() {
-        when(musicApplyRepository.insert(any(Heart.class))).thenReturn(heart);
+        try (MockedStatic<SecurityUtils> securityUtilsMockedStatic = mockStatic(
+            SecurityUtils.class)) {
+            securityUtilsMockedStatic.when(SecurityUtils::getLoggedInUserId)
+                .thenReturn(user.getId());
+            when(userRepository.findById(any(String.class))).thenReturn(Optional.ofNullable(user));
+            when(musicApplyRepository.findOne(any())).thenReturn(Optional.ofNullable(musicApply));
+            when(musicApplyRepository.insert(any(Heart.class))).thenReturn(heart);
 
-        // TODO : heartCnt default value null pointer 예외 해결 할 것 (DB에는 잘 들어감)
+            // TODO : heartCnt default value null pointer 예외 해결 할 것 (DB에는 잘 들어감)
 
-        musicApplyService.createHeart(heartSaveRequestDto);
-        assertThat(musicApply.getHeartCnt()).isEqualTo(1);
+            musicApplyService.createHeart(heartSaveRequestDto);
+
+            assertThat(musicApply.getHeartCnt()).isEqualTo(1);
+        }
     }
 
     @Test
     @DisplayName("노래 신청 좋아요 취소 테스트")
     public void musicApplyHeartCancelServiceTest() {
-        Long heartId = heart.getHeartId();
-        // TODO : user, musicApply 더미 데이터 넣기
-        when(musicApplyRepository.findHeart(any(), any())).thenReturn(Optional.of(heart));
+        try (MockedStatic<SecurityUtils> securityUtilsMockedStatic = mockStatic(
+            SecurityUtils.class)) {
+            securityUtilsMockedStatic.when(SecurityUtils::getLoggedInUserId)
+                .thenReturn(user.getId());
+            Long heartId = heart.getHeartId();
 
-        musicApplyService.deleteHeart(musicApply.getMusicApplyId());
+            when(userRepository.findById(any(String.class))).thenReturn(Optional.ofNullable(user));
+            when(musicApplyRepository.findOne(any())).thenReturn(Optional.ofNullable(musicApply));
+            when(musicApplyRepository.findHeart(any(), any())).thenReturn(Optional.of(heart));
 
-        verify(musicApplyRepository, only()).deleteHeart(heartId);
+            musicApplyService.deleteHeart(musicApply.getMusicApplyId());
+
+            verify(musicApplyRepository, only()).deleteHeart(heartId);
+        }
     }
 
     @Test
-    @DisplayName("노래 신청 좋아요 상태 체크 테스트")
-    public void musicApplyHeartStatusTest() {
-        // TODO : Heart 테이블에 해당하는 user && musicApply 있는지 확인
-        when(musicApplyRepository.findHeart(any(), any())).thenReturn(Optional.of(heart));
+    @DisplayName("노래 신청 좋아요 상태 체크 테스트 - 좋아요를 누를 수 있다.")
+    public void musicApplyHeartStatusCanHeartTest() {
+        try (MockedStatic<SecurityUtils> securityUtilsMockedStatic = mockStatic(
+            SecurityUtils.class)) {
+            securityUtilsMockedStatic.when(SecurityUtils::getLoggedInUserId)
+                .thenReturn(user.getId());
+            when(musicApplyRepository.findHeart(any(), any())).thenReturn(Optional.empty());
 
-        Integer canHeart = musicApplyService.findHeartStatus(musicApply.getMusicApplyId());
+            Integer canHeart = musicApplyService.findHeartStatus(musicApply.getMusicApplyId());
 
-        // TODO : 좋아요 안눌렸으면 1, 눌려있으면 0
-        final Integer CAN_HEART = 1, CANNOT_HEART = 0;
-        assertThat(canHeart).isEqualTo(CAN_HEART);
+            // TODO : 좋아요 안눌렸으면 1, 눌려있으면 0
+            final Integer CAN_HEART = 1, CANNOT_HEART = 0;
+            assertThat(canHeart).isEqualTo(CAN_HEART);
+        }
+    }
+
+    @Test
+    @DisplayName("노래 신청 좋아요 상태 체크 테스트 - 좋아요를 누를 수 없다.")
+    public void musicApplyHeartStatusCannotHeartTest() {
+        try (MockedStatic<SecurityUtils> securityUtilsMockedStatic = mockStatic(
+            SecurityUtils.class)) {
+            securityUtilsMockedStatic.when(SecurityUtils::getLoggedInUserId)
+                .thenReturn(user.getId());
+            when(musicApplyRepository.findHeart(any(), any())).thenReturn(Optional.of(heart));
+
+            Integer canHeart = musicApplyService.findHeartStatus(musicApply.getMusicApplyId());
+
+            // TODO : 좋아요 안눌렸으면 1, 눌려있으면 0
+            final Integer CAN_HEART = 1, CANNOT_HEART = 0;
+            assertThat(canHeart).isEqualTo(CANNOT_HEART);
+        }
     }
 
     private MusicApply updateMusicApply() {
@@ -297,5 +366,29 @@ class MusicApplyServiceTest {
             .writer(tmp)
             .build());
         return musicApplies;
+    }
+
+
+    private List<MusicApply> makeMusicApplyByKeyword(List<MusicApply> musicApplies,
+        String keyword) {
+        List<MusicApply> result = new ArrayList<>();
+
+        for (MusicApply m : musicApplies) {
+            if (m.getTitle().contains(keyword) || m.getArtist().contains(keyword)) {
+                result.add(m);
+            }
+        }
+        return result;
+    }
+
+    private List<MusicApply> makeMusicApplyByUser(List<MusicApply> musicApplies, User user) {
+        List<MusicApply> result = new ArrayList<>();
+
+        for (MusicApply m : musicApplies) {
+            if (m.getWriter().getId().equals(user.getId())) {
+                result.add(m);
+            }
+        }
+        return result;
     }
 }
