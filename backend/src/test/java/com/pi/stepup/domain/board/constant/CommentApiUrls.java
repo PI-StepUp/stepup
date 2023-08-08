f7ec6555e9e790c0993fd51f7a9aca8a116a7c4f
이주희 <joo9837@naver.com>
2023-08-08 15:28:56 +0900

    ✅ Test(board/api): Comment 테스트 코드 url 리팩토링 (#819)

--- backend/src/test/java/com/pi/stepup/domain/board/constant/CommentApiUrls.java
+++ backend/src/test/java/com/pi/stepup/domain/board/constant/CommentApiUrls.java
@@ -1,7 +1,7 @@
 package com.pi.stepup.domain.board.constant;
 
 public enum CommentApiUrls {
-    CREATE_COMMENT_URL(""),
+    CREATE_COMMENT_URL("/"),
     DELETE_COMMENT_URL("/");
 
     private final String url;
