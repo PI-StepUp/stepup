2f36de9f51327b715335e9a409cafc95bc9dc196
Sehbeom <j3beom@gmail.com>
2023-08-10 01:46:50 +0900

    :hammer: Refactor(user/refresh): refresh token redis repo 이름 변경 (#843)
    - refresh token redis repository 이름 수정

--- backend/src/main/java/com/pi/stepup/domain/user/dao/RefreshTokenRedisRepository.java
+++ backend/src/main/java/com/pi/stepup/domain/user/dao/RefreshTokenRedisRepository.java
@@ -3,6 +3,6 @@ package com.pi.stepup.domain.user.dao;
 import com.pi.stepup.domain.user.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRedisRepository extends CrudRepository<RefreshToken, String> {
+public interface RefreshTokenRedisRepository extends CrudRepository<RefreshToken, String> {
 
 }
