cc27d41e22d71e06e45284767440e5d3e75cfd57
lkk <rabbit1999k@naver.com>
2023-08-17 00:28:24 +0900

    :heavy_plus_sign: Feat(rank): 사용자 등급, 포인트 redis 적용 중
    - rank enum 버그 수정해야됨
    
    
    Former-commit-id: 5892cd3a9c9f9fac0bf66b77480a99a0584a1961

--- backend/src/main/java/com/pi/stepup/domain/rank/service/PointRedisServiceImpl.java
+++ backend/src/main/java/com/pi/stepup/domain/rank/service/PointRedisServiceImpl.java
@@ -1,9 +1,12 @@
 package com.pi.stepup.domain.rank.service;
 
+import static com.pi.stepup.domain.user.constant.UserExceptionMessage.USER_NOT_FOUND;
+
 import com.pi.stepup.domain.rank.constant.RankName;
-import com.pi.stepup.domain.rank.dao.PointHistoryRepository;
-import com.pi.stepup.domain.user.service.UserRedisService;
-import com.pi.stepup.domain.user.service.UserRedisServiceImpl;
+import com.pi.stepup.domain.user.dao.UserRepository;
+import com.pi.stepup.domain.user.domain.User;
+import com.pi.stepup.domain.user.exception.UserNotFoundException;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,25 +20,45 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class PointRedisServiceImpl implements PointRedisService {
 
-    private final PointHistoryRepository pointHistoryRepository;
+    @Value("${spring.redis.ttls.point}")
+    private long POINT_EXPIRE_IN;
     private final RedisTemplate<String, Object> redisTemplate;
-    private final UserRedisService userRedisService;
+    private final UserRepository userRepository;
+
 
     @Override
     public void updatePoint(String id, Integer pointToAdd) {
-        Integer userPoint = Integer.valueOf(
-            String.valueOf(userRedisService.getUserInfo(id).getPoint()));
-        userPoint = userPoint + pointToAdd;
+        String userInfoKey = "user_info:" + id;
+        String pointKey = "user:" + id + ":point";
+        int userPoint;
 
+        if (redisTemplate.hasKey(pointKey) == null || !redisTemplate.hasKey(pointKey)) {
+            User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND.getMessage()));
+            userPoint = user.getPoint();
+        } else {
+            userPoint = (int) redisTemplate.opsForValue().get(pointKey);
+        }
+        userPoint = userPoint + pointToAdd;
         log.debug("[DEBUG] {} 사용자 포인트 : {}", id, userPoint);
 
-        String userInfoKey = "user_info:" + id;
-        redisTemplate.opsForHash().put(userInfoKey, "point", String.valueOf(userPoint));
+        if (redisTemplate.hasKey(userInfoKey) != null && redisTemplate.hasKey(userInfoKey)) {
+            redisTemplate.opsForHash().put(userInfoKey, "point", userPoint);
+        }
+
+        redisTemplate.opsForValue().getAndSet(pointKey, userPoint);
+        redisTemplate.expire(pointKey, POINT_EXPIRE_IN, TimeUnit.MILLISECONDS);
     }
 
     @Override
     public void updateRank(String id, RankName rankName) {
         String userInfoKey = "user_info:" + id;
-        redisTemplate.opsForHash().put(userInfoKey, "rankName", String.valueOf(rankName));
+        String rankKey = "user:" + id + ":rank";
+
+        if (redisTemplate.hasKey(userInfoKey) != null && redisTemplate.hasKey(userInfoKey)) {
+            redisTemplate.opsForHash().put(userInfoKey, "rankName", rankName.name());
+        }
+        redisTemplate.opsForValue().getAndSet(rankKey, rankName.name());
+        redisTemplate.expire(rankKey, POINT_EXPIRE_IN, TimeUnit.MILLISECONDS);
     }
 }
