0335a8fb7141076b07b0c9c74cb1f5d67b4012dd
Sehbeom <j3beom@gmail.com>
2023-08-16 16:39:58 +0900

    :construction_worker: Setting(redis, mongo): redis, mongodb 배포용 yml 추가
    - redis, mongodb 배포용 yml 추가

--- backend/src/main/java/com/pi/stepup/domain/board/domain/Board.java
+++ backend/src/main/java/com/pi/stepup/domain/board/domain/Board.java
@@ -47,7 +47,7 @@ public abstract class Board extends BaseEntity {
     @Column(name = "BOARD_TYPE", insertable = false, updatable = false)
     String boardType;
 
-    @Column(name = "VIEW_CNT", nullable = false)
+    @Column(name = "VIEW_CNT")
     Long viewCnt;
 
     public void increaseViewCnt() {
