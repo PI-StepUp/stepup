d7c0641636cdc5eb632e2943050db2f283715ced
Seulgi <urina842@gmail.com>
2023-08-31 10:16:39 +0900

    :hammer: Refactor(dance/redis): SyncScheduler 1시간 마다 실행되도록 변경
    Former-commit-id: 53b085ffb04295019f7dcef8f637160cc2065982

--- backend/src/main/java/com/pi/stepup/domain/dance/service/SyncScheduler.java
+++ backend/src/main/java/com/pi/stepup/domain/dance/service/SyncScheduler.java
@@ -7,6 +7,7 @@ import com.pi.stepup.domain.dance.dao.DanceRepository;
 import com.pi.stepup.domain.dance.domain.Reservation;
 import com.pi.stepup.domain.dance.exception.DanceBadRequestException;
 import com.pi.stepup.domain.user.dao.UserRepository;
+import com.pi.stepup.domain.user.domain.User;
 import com.pi.stepup.domain.user.exception.UserNotFoundException;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -30,58 +31,47 @@ public class SyncScheduler {
     private final DanceRepository danceRepository;
     private final UserRepository userRepository;
 
-    //    private final long SCHEDULED_TIME = 3600000;
-    private final long SCHEDULED_TIME = 20000;
-    private final long CHECK_TTL_TIME = 10000;
-
+    private final long SCHEDULED_TIME = 3600000;
     @Scheduled(fixedDelay = SCHEDULED_TIME)
     @Transactional
     public void syncExpiredDataToDB() {
         Set<String> reservationRedisKeys = redisTemplate.keys("reservation:*");
 
         for (String reservationRedisKey : reservationRedisKeys) {
-            if (redisTemplate.getExpire(reservationRedisKey) <= CHECK_TTL_TIME) {
-                log.info("reservationRedisKey: {}", reservationRedisKey);
+            Set<Object> set = redisTemplate.opsForSet().members(reservationRedisKey);
 
-                Set<Object> set = redisTemplate.opsForSet().members(reservationRedisKey);
+            String userId = getUserId(reservationRedisKey);
+            User user = userRepository.findById(userId).orElseThrow(()
+                -> new UserNotFoundException(USER_NOT_FOUND.getMessage()));
+            Long userPk = user.getUserId();
 
-                String userId = getUserId(reservationRedisKey);
-
-                Long userPk = userRepository.findById(userId).orElseThrow(()
-                    -> new UserNotFoundException(USER_NOT_FOUND.getMessage())).getUserId();
-
-                Set<Long> randomDanceIdSet = new HashSet<>();
-                for (Object value : set) {
-                    Long randomDanceId = Long.valueOf(String.valueOf(value));
-                    randomDanceIdSet.add(randomDanceId);
-                }
-
-                //이미 DB에 존재하는 리스트 가져와서 있는 거 제외하기
-                List<Reservation> reservationList = danceRepository.findAllMyReservation(userPk);
-                for (int i = 0; i < reservationList.size(); i++) {
-                    Long randomDanceId = reservationList.get(i).getRandomDance().getRandomDanceId();
-
-                    if (randomDanceIdSet.contains(randomDanceId)) {
-                        randomDanceIdSet.remove(randomDanceId);
-                    }
-                }
-
-                List<Reservation> reservationInsertList = new ArrayList<>();
-                for (Long randomDanceId : randomDanceIdSet) {
-                    log.info("[insert] randomDanceId: {}", randomDanceId);
-
-                    Reservation reservation = Reservation.builder()
-                        .user(userRepository.findById(userId).orElseThrow(()
-                            -> new UserNotFoundException(USER_NOT_FOUND.getMessage())))
-                        .randomDance(danceRepository.findOne(randomDanceId).orElseThrow(()
-                            -> new DanceBadRequestException(DANCE_NOT_FOUND.getMessage())))
-                        .build();
-
-                    reservationInsertList.add(reservation);
-                }
-
-                danceRepository.insertReservationList(reservationInsertList);
+            Set<Long> randomDanceIdSet = new HashSet<>();
+            for (Object value : set) {
+                Long randomDanceId = Long.valueOf(String.valueOf(value));
+                randomDanceIdSet.add(randomDanceId);
             }
+
+            List<Reservation> reservationList = danceRepository.findAllMyReservation(userPk);
+            for (int i = 0; i < reservationList.size(); i++) {
+                Long randomDanceId = reservationList.get(i).getRandomDance().getRandomDanceId();
+
+                if (randomDanceIdSet.contains(randomDanceId)) {
+                    randomDanceIdSet.remove(randomDanceId);
+                }
+            }
+
+            List<Reservation> reservationInsertList = new ArrayList<>();
+            for (Long randomDanceId : randomDanceIdSet) {
+                Reservation reservation = Reservation.builder()
+                    .user(user)
+                    .randomDance(danceRepository.findOne(randomDanceId).orElseThrow(()
+                        -> new DanceBadRequestException(DANCE_NOT_FOUND.getMessage())))
+                    .build();
+
+                reservationInsertList.add(reservation);
+            }
+
+            danceRepository.insertReservationList(reservationInsertList);
         }
     }
 
