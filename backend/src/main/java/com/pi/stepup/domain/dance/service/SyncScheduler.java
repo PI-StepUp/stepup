90bf87d5f1ba6bd1aba6de65b547e388cd55ddf6
Seulgi <urina842@gmail.com>
2023-09-01 11:29:20 +0900

    :hammer: Refactor(dance/scheduler): DB 삽입 시 이미 시작한 랜플댄 예약은 제외하고 삽입되도록 변경
    Former-commit-id: cdbc7178521a13937f6dcaeb32a355a21575151b

--- backend/src/main/java/com/pi/stepup/domain/dance/service/SyncScheduler.java
+++ backend/src/main/java/com/pi/stepup/domain/dance/service/SyncScheduler.java
@@ -4,11 +4,13 @@ import static com.pi.stepup.domain.dance.constant.DanceExceptionMessage.DANCE_NO
 import static com.pi.stepup.domain.user.constant.UserExceptionMessage.USER_NOT_FOUND;
 
 import com.pi.stepup.domain.dance.dao.DanceRepository;
+import com.pi.stepup.domain.dance.domain.RandomDance;
 import com.pi.stepup.domain.dance.domain.Reservation;
 import com.pi.stepup.domain.dance.exception.DanceBadRequestException;
 import com.pi.stepup.domain.user.dao.UserRepository;
 import com.pi.stepup.domain.user.domain.User;
 import com.pi.stepup.domain.user.exception.UserNotFoundException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -32,6 +34,7 @@ public class SyncScheduler {
     private final UserRepository userRepository;
 
     private final long SCHEDULED_TIME = 3600000;
+
     @Scheduled(fixedDelay = SCHEDULED_TIME)
     @Transactional
     public void syncExpiredDataToDB() {
@@ -61,14 +64,21 @@ public class SyncScheduler {
             }
 
             List<Reservation> reservationInsertList = new ArrayList<>();
+            LocalDateTime now = LocalDateTime.now();
             for (Long randomDanceId : randomDanceIdSet) {
-                Reservation reservation = Reservation.builder()
-                    .user(user)
-                    .randomDance(danceRepository.findOne(randomDanceId).orElseThrow(()
-                        -> new DanceBadRequestException(DANCE_NOT_FOUND.getMessage())))
-                    .build();
+                RandomDance randomDance = danceRepository.findOne(randomDanceId).orElseThrow(()
+                    -> new DanceBadRequestException(DANCE_NOT_FOUND.getMessage()));
 
-                reservationInsertList.add(reservation);
+                if (randomDance.getStartAt().isAfter(now)) {
+                    Reservation reservation = Reservation.builder()
+                        .user(user)
+                        .randomDance(randomDance)
+                        .build();
+
+                    reservationInsertList.add(reservation);
+                } else if (randomDance.getStartAt().isBefore(now)) {
+                    redisTemplate.opsForSet().remove(reservationRedisKey, randomDanceId);
+                }
             }
 
             danceRepository.insertReservationList(reservationInsertList);
