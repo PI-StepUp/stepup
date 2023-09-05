75222a6b4d763331bafd6d42d6b192b66ca746b7
이주희 <joo9837@naver.com>
2023-09-05 17:03:10 +0900

    Update StepupApplication.java
    Former-commit-id: 2e56f585a9c38ca86c7b18982a605908c94713c3

--- backend/src/main/java/com/pi/stepup/StepupApplication.java
+++ backend/src/main/java/com/pi/stepup/StepupApplication.java
@@ -8,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableScheduling
 @SpringBootApplication
-@EnableScheduling
 public class StepupApplication {
 
 	public static void main(String[] args) {
