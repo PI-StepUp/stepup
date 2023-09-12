222486659e81f0275695e1df4c70328cf8cf196c
Sehbeom <j3beom@gmail.com>
2023-09-12 09:17:27 +0900

    :construction_worker: Setting(secret): secret 파일 제거

--- exec/Play Scenario.md
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:18591010cdf6bd78d38f1da9dc0c388a293f65b999e207dfedfe04f26dd93135
-size 371
--- exec/stepup_mariaDB_dump.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e42f4ab39dd5685f64a2ff0abe242087de411c087fcd3de2de3d79175a3b265b
-size 130237
